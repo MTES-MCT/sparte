22e9be5c196ed0de33f604dd240e12ed8a78cd89
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-07-06 17:54:55 -0500

    admin for cutom user

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
 
-#admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['email', ]
+
+
+admin.site.register(User, CustomUserAdmin)
