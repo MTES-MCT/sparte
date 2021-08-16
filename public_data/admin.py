f24278e0f6af3f61588c49df5c9eb5decaa93329
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-08-16 16:31:59 -0500

    add public_data to admin

--- public_data/admin.py
+++ public_data/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+
+from .models import (
+    Artificialisee2015to2018,
+    Artificielle2018,
+    CommunesSybarval,
+    EnveloppeUrbaine2018,
+    Renaturee2018to2015,
+    Sybarval,
+    Voirie2018,
+    ZonesBaties2018,
+)
+
+
+class DefaultAdmin(admin.GeoModelAdmin):
+    pass
+
+
+@admin.register(CommunesSybarval)
+class CommunesSybarvalAdmin(admin.GeoModelAdmin):
+    model = CommunesSybarval
+    list_display = (
+        "nom",
+        "code_insee",
+        "statut",
+        "_nom_epci",
+        "_siren_epc",
+    )
+    list_filter = ("_nom_epci",)
+    search_fields = (
+        "nom",
+        "code_insee",
+    )
+    ordering = ("nom",)
+
+
+admin.site.register(Artificialisee2015to2018, DefaultAdmin)
+admin.site.register(Artificielle2018, DefaultAdmin)
+admin.site.register(EnveloppeUrbaine2018, DefaultAdmin)
+admin.site.register(Renaturee2018to2015, DefaultAdmin)
+admin.site.register(Sybarval, DefaultAdmin)
+admin.site.register(Voirie2018, DefaultAdmin)
+admin.site.register(ZonesBaties2018, DefaultAdmin)
