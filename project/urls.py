baf363ff936584a6188af0cba1e8d8b320c7e761
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-08-24 17:47:37 -0500

    add api endpoint to get project's geojson

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
+from rest_framework import routers
 
 from . import views
+from .api_views import EmpriseViewSet
 
 
 app_name = "project"
+
+
+router = routers.DefaultRouter()
+router.register(r"geojson", EmpriseViewSet)
+
 
 urlpatterns = [
     path("", views.ProjectListView.as_view(), name="list"),
     path("add/", views.ProjectCreateView.as_view(), name="add"),
     path("<int:pk>/", views.ProjectUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", views.ProjectDeleteView.as_view(), name="delete"),
-]
+] + router.urls
