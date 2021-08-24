5f0951dce2dd9f35eb23816e129f272efd2de652
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-08-24 17:45:54 -0500

    admin to see project and emprise data

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+from .models import Project, Emprise
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.GeoModelAdmin):
+    model = Project
+    list_display = (
+        "name",
+        "user",
+        "analyse_start_date",
+        "analyse_end_date",
+        "import_status",
+        "import_date",
+    )
+    list_filter = ("import_status",)
+    search_fields = (
+        "name",
+        "import_error",
+        "user",
+    )
+    ordering = ("name",)
+
+
+@admin.register(Emprise)
+class EmpriseAdmin(admin.GeoModelAdmin):
+    model = Emprise
+    list_display = (
+        "id",
+        "project",
+    )
+    search_fields = ("project",)
