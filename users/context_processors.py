80373a497ec2896be664222602ee0b027eb5c16a
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-08-27 08:41:59 -0500

    add admin menu

--- users/context_processors.py
+++ users/context_processors.py
@@ -2,6 +2,7 @@ def add_connected_user_to_context(request):
     if request.user:
         return {
             "user": request.user,
+            "is_staff": request.user.is_staff,
         }
     else:
         return dict()
