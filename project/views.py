a109ce12d45d2bd4a1fa2a356ecc353b83f2da80
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-09-13 06:33:54 -0500

    remove unused import

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Project
