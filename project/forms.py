9b235dae7b17720e2ce78479a5adbe9826722766
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-09-13 16:52:05 -0500

    complete project flow

--- project/forms.py
+++ project/forms.py
@@ -3,22 +3,30 @@ from django.db.models import Q
 
 from public_data.models import ArtifCommune, CommunesSybarval
 
+from .models import Project
+from .tasks import import_shp
 
-class SelectCitiesForm(forms.Form):
+
+class SetEmpriseForm(forms.Form):
+    def save(self, project):
+        project.cities.clear()
+        for city in self.cleaned_data["cities"]:
+            project.cities.add(city)
+        project.import_status = Project.Status.SUCCESS
+        project.save()
+
+
+class SelectCitiesForm(SetEmpriseForm):
     cities = forms.ModelMultipleChoiceField(
         queryset=ArtifCommune.objects.all().order_by("name"),
     )
 
 
-class SelectPluForm(forms.Form):
-    cities = forms.ModelMultipleChoiceField(
-        queryset=ArtifCommune.objects.all().order_by("name"),
-        required=False,
-        # widget=forms.HiddenInput()
-    )
+class SelectPluForm(SetEmpriseForm):
     plu = forms.ChoiceField()
 
     def __init__(self, *args, **kwargs):
+        """set choices dynamicaly"""
         super().__init__(*args, **kwargs)
         qs_epci = CommunesSybarval.objects.values("_nom_epci").distinct()
         qs_scot = CommunesSybarval.objects.values("_nom_scot").distinct()
@@ -27,6 +35,7 @@ class SelectPluForm(forms.Form):
         self.fields["plu"].choices = [(x, x) for x in choices_plu]
 
     def clean(self):
+        """Add cleaned_data["cities"]"""
         super().clean()
         if "plu" in self.cleaned_data:
             name = self.cleaned_data["plu"]
@@ -40,3 +49,9 @@ class SelectPluForm(forms.Form):
 
 class UploadShpForm(forms.Form):
     shape_zip = forms.FileField()
+
+    def save(self, project):
+        project.shape_file = self.cleaned_data["shape_zip"]
+        project.import_status = Project.Status.PENDING
+        project.save()
+        import_shp.delay(project.id)
