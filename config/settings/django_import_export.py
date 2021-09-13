9456572c59bfa48eab35a37ec2b2a06f9a89d085
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-09-13 06:33:54 -0500

    refactor settings

--- config/settings/django_import_export.py
+++
@@ -1,9 +0,0 @@
-"""
-Parameter for django-import-export
-"""
-from .base import INSTALLED_APPS
-
-# Add crispy to installed app
-INSTALLED_APPS += ["import_export"]
-
-IMPORT_EXPORT_USE_TRANSACTIONS = True
