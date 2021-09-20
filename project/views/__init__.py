0852aa5f420181b155df1833a828701979125d0d
Swann <swann.bouvier-muller-ext@johnpaul.com>
2021-09-20 16:01:32 -0500

    split file views.py into folder

---
+++ project/views/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+from .project import (
+    ProjectCreateView,
+    ProjectDeleteView,
+    ProjectDetailView,
+    ProjectFailedView,
+    ProjectListView,
+    ProjectMapView,
+    ProjectNoShpView,
+    ProjectPendingView,
+    ProjectReinitView,
+    ProjectReportArtifView,
+    ProjectReportView,
+    ProjectSuccessView,
+    ProjectUpdateView,
+)
