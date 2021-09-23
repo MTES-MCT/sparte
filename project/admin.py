95fb8224ed84d5c7d3ced657613086020f8f8345
Swann <swann.bouviermuller@gmail.com>
2021-09-23 18:05:27 -0500

    refactor celery tasks

--- project/admin.py
+++ project/admin.py
@@ -2,7 +2,7 @@ from django.contrib.gis import admin
 from django.http import HttpResponseRedirect
 
 from .models import Project, Emprise
-from .tasks import import_shp
+from .tasks import import_project_shp
 
 
 @admin.register(Project)
@@ -29,7 +29,7 @@ class ProjectAdmin(admin.GeoModelAdmin):
     def response_change(self, request, obj):
         if "_reload-emprise-action" in request.POST:
             # Trigger asynch task to reload emprise file
-            import_shp.delay(obj.id)
+            import_project_shp.delay(obj.id)
             return HttpResponseRedirect(".")  # stay on the same detail page
         return super().response_change(request, obj)
 
