3dbd6ae2709f70ba984433ddc0bf79b2644697ab
Swann <swann.bouviermuller@gmail.com>
2021-11-18 16:23:28 +0000

    add unsubscribe

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("signin/", views.SigninView.as_view(), name="signin"),
     path("profile/", views.ProfilFormView.as_view(), name="profile"),
     path("password/", views.UpdatePwFormView.as_view(), name="password"),
+    path("unsubscribe/", views.UserDeleteView.as_view(), name="unsubscribe"),
 ]
