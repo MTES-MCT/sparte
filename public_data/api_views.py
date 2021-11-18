e3ff4e6d61210ffc5bbbdae72f99b32dd303a143
Swann <swann.bouviermuller@gmail.com>
2021-11-18 16:23:28 +0000

    use color in property to display map

--- public_data/api_views.py
+++ public_data/api_views.py
@@ -2,6 +2,7 @@
 import json
 
 from django.db import connection
+from django.http import HttpResponse
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -133,6 +134,66 @@ class OcsgeViewSet(DataViewSet):
         if year:
             qs = qs.filter(year=year)
         return qs
+
+    def get_data(self, bbox=None, year=2015, color="usage"):
+        if color == "usage":
+            table_name = UsageSol._meta.db_table
+            field = "usage"
+        else:
+            table_name = CouvertureSol._meta.db_table
+            field = "couverture"
+        query = (
+            "SELECT o.id, o.couverture_label, o.usage_label, o.millesime, t.map_color, "
+            "o.year, st_AsGeoJSON(o.mpoly, 4) AS geojson "
+            f"FROM {Ocsge._meta.db_table} o "
+            f"INNER JOIN {table_name} t ON t.code_prefix = o.{field} "
+            "WHERE o. mpoly && ST_MakeEnvelope(%s, %s, %s, %s, 4326) "
+            "AND o.year = %s"
+        )
+        params = bbox + [year]
+        with connection.cursor() as cursor:
+            cursor.execute(query, params)
+            rows = cursor.fetchall()
+        return rows
+
+    @action(detail=False)
+    def optimized(self, request):
+        bbox = request.query_params.get("in_bbox").split(",")
+        bbox = list(map(float, bbox))
+        data = self.get_data(
+            bbox=bbox,
+            year=int(request.query_params.get("year")),
+            color=request.query_params.get("color"),
+        )
+        geojson = json.dumps(
+            {
+                "type": "FeatureCollection",
+                "crs": {"type": "name", "properties": {"name": "EPSG:4326"}},
+                "features": "-features-",
+            }
+        )
+        features = []
+        for row in data:
+            feature = json.dumps(
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "id": row[0],
+                        "couverture_label": row[1],
+                        "usage_label": row[2],
+                        "millesime": row[3].strftime("%Y-%m-%d"),
+                        "map_color": row[4],
+                        "year": row[5],
+                    },
+                    "geometry": "-geometry-",
+                }
+            )
+            feature = feature.replace('"-geometry-"', row[6])
+            features.append(feature)
+        features = f" [{', '.join(features)}]"
+        geojson = geojson.replace('"-features-"', features)
+        # return Response(geojson)
+        return HttpResponse(geojson, content_type="application/json")
 
 
 class Renaturee2018to2015ViewSet(DataViewSet):
