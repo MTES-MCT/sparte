e16998afc6ec01dcb0e7d149b5495274b170e943
Swann <swann.bouviermuller@gmail.com>
2021-12-21 17:03:15 +0000

    ajout du model FrequentlyAskedQuestion

---
+++ home/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import FrequentlyAskedQuestion
+
+
+@admin.register(FrequentlyAskedQuestion)
+class FrequentlyAskedQuestionAdmin(admin.ModelAdmin):
+    model = FrequentlyAskedQuestion
+    list_display = ("title",)
+    search_fields = (
+        "title",
+        "menu_entry",
+        "md_content",
+        "slug",
+    )
+    ordering = ("title",)
+    readonly_fields = ("slug",)
