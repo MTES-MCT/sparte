b0c61d46f18f2e0bbd7af3baf4d9990107484189
Swann <swann.bouviermuller@gmail.com>
2021-12-24 16:02:19 +0000

    remove useless urls

--- carto/urls.py
+++ carto/urls.py
@@ -6,9 +6,10 @@ from . import views
 app_name = "carto"
 
 urlpatterns = [
-    path("", views.HomeConnected.as_view(), name="home_connected"),
-    path("ortho", views.OrthoMapView.as_view(), name="ortho"),
-    path("arcachon/", views.arcachon, name="arcachon"),
-    path("ocsge/2015/", views.Ocsge2015MapView.as_view(), name="ocsge-2015"),
-    path("ocsge/", views.OcsgeMapViewSet.as_view(), name="ocsge"),
+    # path("", views.HomeConnected.as_view(), name="home_connected"),
+    # path("ortho", views.OrthoMapView.as_view(), name="ortho"),
+    # path("arcachon/", views.arcachon, name="arcachon"),
+    # path("ocsge/2015/", views.Ocsge2015MapView.as_view(), name="ocsge-2015"),
+    # path("ocsge/", views.OcsgeMapViewSet.as_view(), name="ocsge"),
+    path("select/", views.SelectTerritory.as_view(), name="select"),
 ]
