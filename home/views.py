579bbbd46a01fe33091ab100da2df1678871a77a
Swann <swann.bouviermuller@gmail.com>
2022-01-05 23:53:21 +0000

    update breadcrumbs building for all pages

--- home/views.py
+++ home/views.py
@@ -1,12 +1,24 @@
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, DetailView
+
+from utils.views_mixins import BreadCrumbMixin, GetObjectMixin
+
 from .models import FrequentlyAskedQuestion
 
 
-class Home(TemplateView):
+class Home(BreadCrumbMixin, TemplateView):
     template_name = "home/home.html"
 
 
-class FrequentlyAskedQuestionDetail(DetailView):
+class FrequentlyAskedQuestionDetail(GetObjectMixin, BreadCrumbMixin, DetailView):
     template_name = "home/faq_detail.html"
     context_object_name = "faq"
     queryset = FrequentlyAskedQuestion.objects.all()
+
+    def get_context_breadcrumbs(self):
+        breadcrumbs = super().get_context_breadcrumbs()
+        faq = self.get_object()
+        breadcrumbs += [
+            {"href": reverse_lazy("home:faq-detail"), "title": faq.menu_entry},
+        ]
+        return breadcrumbs
