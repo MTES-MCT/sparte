602bdf0cd6ba054fec6045f8c055b4942156f3c3
Swann <swann.bouviermuller@gmail.com>
2022-01-09 00:54:59 +0000

    improve report

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,30 +1,4 @@
 # flake8: noqa
-from .project import (
-    ProjectCreateView,
-    ProjectDeleteView,
-    ProjectDetailView,
-    ProjectFailedView,
-    ProjectListView,
-    ProjectMapView,
-    ProjectNoShpView,
-    ProjectPendingView,
-    ProjectReinitView,
-    ProjectReportCouvertureView,
-    ProjectReportSynthesisView,
-    ProjectReportUsageView,
-    ProjectReportView,
-    ProjectSuccessView,
-    ProjectUpdateView,
-)
-
-from .plan import (
-    PlanCreateView,
-    PlanDeleteView,
-    PlanDetailView,
-    PlanListView,
-    PlanUpdateView,
-)
-
-from .public import (
-    SelectPublicProjects,
-)
+from .project import *
+from .plan import *
+from .public import *
