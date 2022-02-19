7462e27da0a7256bf547fed6456afdcf6117015d
Swann <swann.bouviermuller@gmail.com>
2022-02-19 23:27:09 +0000

    update app parameter

--- app_parameter/admin.py
+++ app_parameter/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Parameter
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    model = Parameter
+    list_display = (
+        "name",
+        "slug",
+        "value",
+        "value_type",
+    )
+    list_filter = ("value_type",)
+    readonly_fields = ("slug",)
+    search_fields = (
+        "name",
+        "slug",
+        "description",
+        "value",
+    )
