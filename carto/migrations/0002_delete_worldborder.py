3e44c6b1f1d65834838f17a400218378d05c4d13
Swann <swann.bouviermuller@gmail.com>
2022-02-24 18:10:34 -0500

    black formating

--- carto/migrations/0002_delete_worldborder.py
+++ carto/migrations/0002_delete_worldborder.py
@@ -6,11 +6,11 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('carto', '0001_initial'),
+        ("carto", "0001_initial"),
     ]
 
     operations = [
         migrations.DeleteModel(
-            name='WorldBorder',
+            name="WorldBorder",
         ),
     ]
--- public_data/migrations/0017_auto_20210924_2219.py
+++ public_data/migrations/0017_auto_20210924_2219.py
@@ -6,18 +6,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('public_data', '0016_auto_20210924_2137'),
+        ("public_data", "0016_auto_20210924_2137"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='artificielle2018',
-            name='couverture_label',
-            field=models.CharField(blank=True, max_length=254, null=True, verbose_name='Libellé couverture du sol'),
+            model_name="artificielle2018",
+            name="couverture_label",
+            field=models.CharField(
+                blank=True,
+                max_length=254,
+                null=True,
+                verbose_name="Libellé couverture du sol",
+            ),
         ),
         migrations.AddField(
-            model_name='artificielle2018',
-            name='usage_label',
-            field=models.CharField(blank=True, max_length=254, null=True, verbose_name='Libellé usage du sol'),
+            model_name="artificielle2018",
+            name="usage_label",
+            field=models.CharField(
+                blank=True,
+                max_length=254,
+                null=True,
+                verbose_name="Libellé usage du sol",
+            ),
         ),
     ]
