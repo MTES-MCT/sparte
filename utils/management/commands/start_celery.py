8500c96f3dd15c5101c4785f02f6998bcf902842
Swann <swann.bouviermuller@gmail.com>
2022-02-25 11:41:36 -0500

    check with bandit

--- utils/management/commands/start_celery.py
+++ utils/management/commands/start_celery.py
@@ -1,15 +1,16 @@
 import shlex
-import subprocess
+import subprocess  # nosec
 
 from django.core.management.base import BaseCommand
 from django.utils import autoreload
 
 
 def restart_celery():
+    # below subprocess calls can't be
     cmd = "pkill celery"
-    subprocess.call(shlex.split(cmd))
+    subprocess.call(shlex.split(cmd))  # nosec
     cmd = "celery -A config.celery worker --loglevel=info"
-    subprocess.call(shlex.split(cmd))
+    subprocess.call(shlex.split(cmd))  # nosec
 
 
 class Command(BaseCommand):
