01a05e119b60709ea431d4f857f273d5d561baec
Swann <swann.bouviermuller@gmail.com>
2022-02-25 08:50:22 -0500

    remove dbbackup

--- dbbackup/storages.py
+++
@@ -1,9 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-from django.conf import settings
-
-
-class DatabaseStorage(S3Boto3Storage):
-    """Enable access to data folder at root of the bucket."""
-
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-    location = "dbbackup"
