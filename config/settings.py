f903714dc0f92453d5504e811fa536b088867089
Swann <swann.bouviermuller@gmail.com>
2022-02-28 16:58:37 -0500

    upgrade sentry sdk and add redis integration

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import pkg_resources
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib.messages import constants as messages
@@ -360,6 +361,7 @@ if ENVIRONMENT != "local":
                 # function_name - formats based on the view function name
                 transaction_style="url",
             ),
+            RedisIntegration(),
         ],
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
