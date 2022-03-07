d4f3e77c3b8ffdc32ea4ea36c9d92cd6132b167d
Swann <swann.bouviermuller@gmail.com>
2022-03-07 07:39:42 -0500

    move chart definition to view

--- highcharts/templatetags/highcharts_tags.py
+++ highcharts/templatetags/highcharts_tags.py
@@ -1,8 +1,19 @@
 from django import template
 
+
 register = template.Library()
 
 
-@register.inclusion_tag("highcharts/translation.html")
-def translate_chart():
+@register.inclusion_tag("highcharts/french_translation.html")
+def french_translation():
     return dict()
+
+
+@register.inclusion_tag("highcharts/display_chart.html")
+def display_chart(div_id, chart):
+    return {
+        "div_id": div_id,
+        "chart_name": chart.get_name(),
+        "json_options": chart.dumps(),
+        "js_name": chart.get_js_name(),
+    }
