11ac6844077ad21ce8f4a960c7eaf4ce14a5b907
Swann <swann.bouviermuller@gmail.com>
2022-05-04 11:17:18 +0200

    add docx template to menu and adapt look and feel

---
+++ project/management/commands/get_image.py
@@ -0,0 +1,21 @@
+import base64
+
+from django.core.management.base import BaseCommand
+
+from project.models import Project
+from project.charts import ConsoCommuneChart, DeterminantPieChart
+
+
+class Command(BaseCommand):
+    help = "Test get image from highchart"
+
+    def handle(self, *args, **options):
+        project = Project.objects.get(pk=8)
+        chart = DeterminantPieChart(project)
+        b64_content = chart.request_b64_image_from_server()
+        with open("test_image.png", "wb") as f:
+            f.write(base64.decodebytes(b64_content))
+        # fd, img_path = tempfile.mkstemp(suffix=".png", text=False)
+        # os.write(fd, base64.decodebytes(b64_content))
+        # os.close(fd)
+        # return img_path
