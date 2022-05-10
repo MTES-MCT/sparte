3686599eb667276d1b19a6904fc43d2bd724b061
Swann <swann.bouviermuller@gmail.com>
2022-05-10 08:25:15 +0200

    Add is_artificial and foreignkey to matrix

--- public_data/models/ocsge.py
+++ public_data/models/ocsge.py
@@ -23,13 +23,14 @@ problème de performance
 from django.contrib.gis.db import models
 from django.contrib.gis.db.models.functions import Intersection, Area, Transform
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import connection
-from django.db.models import Sum
+
+# from django.db import connection
+from django.db.models import Sum, OuterRef, Subquery
 from django.db.models.functions import Cast
 
 from .mixins import AutoLoadMixin, DataColorationMixin
 
-from .couverture_usage import CouvertureSol, UsageSol
+from .couverture_usage import CouvertureSol, UsageSol, CouvertureUsageMatrix
 
 
 class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
@@ -37,7 +38,6 @@ class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
         "Couverture du sol", max_length=254, blank=True, null=True
     )
     usage = models.CharField("Usage du sol", max_length=254, blank=True, null=True)
-    # TODO is_artificial = models.IntegerField("Sureface artificialisée", null=True)
     millesime = models.DateField("Millésime", blank=True, null=True)
     source = models.CharField("Source", max_length=254, blank=True, null=True)
     origine = models.CharField("Origine", max_length=254, blank=True, null=True)
@@ -56,19 +56,23 @@ class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
     )
 
     # calculated fields
+    matrix = models.ForeignKey(
+        CouvertureUsageMatrix, on_delete=models.PROTECT, null=True, blank=True
+    )
     couverture_label = models.CharField(
         "Libellé couverture du sol", max_length=254, blank=True, null=True
     )
     usage_label = models.CharField(
         "Libellé usage du sol", max_length=254, blank=True, null=True
     )
-    map_color = models.CharField("Couleur", max_length=8, blank=True, null=True)
+    is_artificial = models.BooleanField("Est artificiel", null=True, blank=True)
+    surface = models.DecimalField(
+        "surface", max_digits=15, decimal_places=4, blank=True, null=True
+    )
 
     mpoly = models.MultiPolygonField()
 
     default_property = "id"
-    couverture_field = "couverture"
-    usage_field = "usage"
     mapping = {
         "couverture": "couverture",
         "usage": "usage",
@@ -119,6 +123,37 @@ class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
     def clean_data(cls):
         raise NotImplementedError("Need to be overrided to return a year")
 
+    @classmethod
+    def calculate_fields(cls):
+        """Override if you need to calculate some fields after loading data.
+        By default, it will calculate label for couverture and usage if couverture_field
+        and usage_field are set with the name of the field containing code (cs.2.1.3)
+        """
+        # cls.set_label(CouvertureSol, "couverture", "couverture_label")
+        # cls.set_label(UsageSol, "usage", "usage_label")
+        cls.objects.all().filter(surface__isnull=True).update(
+            surface=Cast(
+                Area(Transform("mpoly", 2154)),
+                models.DecimalField(max_digits=15, decimal_places=4),
+            )
+        )
+
+    @classmethod
+    def set_label(cls, klass, field_code, field_label):
+        """Set label field using CouvertureSol or UsageSol référentiel.
+
+        Parameters:
+        ===========
+        * klass: CouvertureSol or UsageSol
+        * field_code: name of the field containing the code (eg. us1.1.1)
+        * field_label: name of the field where to save the label
+        """
+        label = klass.objects.filter(code_prefix=OuterRef(field_code))
+        label = label.values("label")[:1]
+        update_kwargs = {field_label: Subquery(label)}
+        filter_kwargs = {f"{field_label}__isnull": True}
+        cls.objects.all().filter(**filter_kwargs).update(**update_kwargs)
+
 
 class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
     year_old = models.IntegerField(
@@ -161,6 +196,20 @@ class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
     new_is_artif = models.BooleanField(blank=True, null=True)
     is_new_artif = models.BooleanField(blank=True, null=True)
     is_new_naf = models.BooleanField(blank=True, null=True)
+    old_matrix = models.ForeignKey(
+        CouvertureUsageMatrix,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="ocsge_dif_old",
+    )
+    new_matrix = models.ForeignKey(
+        CouvertureUsageMatrix,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="ocsge_difnew",
+    )
 
     # shape_file_path = "gers_diff_2016_2019.zip"  # "media/gers/DIFF_2016_2019.zip"
     default_property = "surface"
@@ -185,57 +234,57 @@ class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
                 models.DecimalField(max_digits=15, decimal_places=4),
             )
         )
-        query = """
-        with t as (
-        select
-            o.id,
-            cs_old.label as cs_old_label,
-            us_old.label as us_old_label,
-            cs_new.label as cs_new_label,
-            us_new.label as us_new_label,
-            m_old.is_artificial as old_is_artif,
-            m_new.is_artificial as new_is_artif,
-            m_new.is_artificial = true
-            and m_old.is_natural = true as is_new_artif,
-            m_old.is_artificial = true
-            and m_new.is_natural = true as is_new_naf
-        from
-            public_data_ocsgediff o
-        left join public_data_couverturesol cs_old on
-            o.cs_old = cs_old.code_prefix
-        left join public_data_usagesol us_old on
-            o.us_old = us_old.code_prefix
-        inner join public_data_couvertureusagematrix m_old
-                on
-            m_old.couverture_id = cs_old.id
-            and m_old.usage_id = us_old.id
-        left join public_data_couverturesol cs_new on
-            o.cs_new = cs_new.code_prefix
-        left join public_data_usagesol us_new on
-            o.us_new = us_new.code_prefix
-        inner join public_data_couvertureusagematrix m_new
-                on
-            m_new.couverture_id = cs_new.id
-            and m_new.usage_id = us_new.id
-        )
-        update
-            public_data_ocsgediff o
-        set
-            cs_old_label = t.cs_old_label,
-            us_old_label = t.us_old_label,
-            cs_new_label = t.cs_new_label,
-            us_new_label = t.us_new_label,
-            old_is_artif = t.old_is_artif,
-            new_is_artif = t.new_is_artif,
-            is_new_artif = t.is_new_artif,
-            is_new_naf = t.is_new_naf
-        from
-            t
-        where
-            o.id = t.id;
-        """
-        with connection.cursor() as cursor:
-            cursor.execute(query)
+        # query = """
+        # with t as (
+        # select
+        #     o.id,
+        #     cs_old.label as cs_old_label,
+        #     us_old.label as us_old_label,
+        #     cs_new.label as cs_new_label,
+        #     us_new.label as us_new_label,
+        #     m_old.is_artificial as old_is_artif,
+        #     m_new.is_artificial as new_is_artif,
+        #     m_new.is_artificial = true
+        #     and m_old.is_natural = true as is_new_artif,
+        #     m_old.is_artificial = true
+        #     and m_new.is_natural = true as is_new_naf
+        # from
+        #     public_data_ocsgediff o
+        # left join public_data_couverturesol cs_old on
+        #     o.cs_old = cs_old.code_prefix
+        # left join public_data_usagesol us_old on
+        #     o.us_old = us_old.code_prefix
+        # inner join public_data_couvertureusagematrix m_old
+        #         on
+        #     m_old.couverture_id = cs_old.id
+        #     and m_old.usage_id = us_old.id
+        # left join public_data_couverturesol cs_new on
+        #     o.cs_new = cs_new.code_prefix
+        # left join public_data_usagesol us_new on
+        #     o.us_new = us_new.code_prefix
+        # inner join public_data_couvertureusagematrix m_new
+        #         on
+        #     m_new.couverture_id = cs_new.id
+        #     and m_new.usage_id = us_new.id
+        # )
+        # update
+        #     public_data_ocsgediff o
+        # set
+        #     cs_old_label = t.cs_old_label,
+        #     us_old_label = t.us_old_label,
+        #     cs_new_label = t.cs_new_label,
+        #     us_new_label = t.us_new_label,
+        #     old_is_artif = t.old_is_artif,
+        #     new_is_artif = t.new_is_artif,
+        #     is_new_artif = t.is_new_artif,
+        #     is_new_naf = t.is_new_naf
+        # from
+        #     t
+        # where
+        #     o.id = t.id;
+        # """
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
 
     @classmethod
     def set_labels(cls):
