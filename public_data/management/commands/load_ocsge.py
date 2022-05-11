dd02794cf8e2bc212608a53f2eb9218562d204da
Swann <swann.bouviermuller@gmail.com>
2022-05-11 14:02:35 +0200

    remove unused fields & generalize new way to load ocsge

---
+++ public_data/management/commands/load_ocsge.py
@@ -0,0 +1,388 @@
+import logging
+
+from django.contrib.gis.db.models.functions import Area, Transform
+from django.core.management.base import BaseCommand
+from django.db.models import DecimalField
+from django.db.models.functions import Cast
+
+from public_data.models import (
+    AutoLoadMixin,
+    CouvertureSol,
+    CouvertureUsageMatrix,
+    Departement,
+    Ocsge,
+    OcsgeDiff,
+    UsageSol,
+    ZoneConstruite,
+)
+
+
+logger = logging.getLogger("management.commands")
+
+
+USAGE_LIST = {usage.code_prefix: usage for usage in UsageSol.objects.all()}
+COUVERTURE_LIST = {
+    couverture.code_prefix: couverture for couverture in CouvertureSol.objects.all()
+}
+MATRIX_LIST = {
+    (item.couverture.code_prefix, item.usage.code_prefix): item
+    for item in CouvertureUsageMatrix.objects.all().select_related(
+        "usage", "couverture"
+    )
+}
+
+
+# #######
+# ADD FUNCTION TO LOAD DATA
+# #######
+
+
+class AutoOcsgeDiff(AutoLoadMixin, OcsgeDiff):
+    class Meta:
+        proxy = True
+
+    def before_save(self):
+        self.year_new = self.__class__._year_new
+        self.year_old = self.__class__._year_old
+
+        key = (self.cs_new, self.us_new)
+        if key in MATRIX_LIST:
+            self.new_matrix = MATRIX_LIST[key]
+            self.cs_new_label = self.new_matrix.couverture.label
+            self.us_new_label = self.new_matrix.usage.label
+            self.new_is_artif = bool(self.new_matrix.is_artificial)
+        else:
+            if self.cs_new in COUVERTURE_LIST:
+                self.cs_new_label = COUVERTURE_LIST[self.cs_new].label
+            if self.us_new in USAGE_LIST:
+                self.us_new_label = USAGE_LIST[self.us_new].label
+            self.new_is_artif = False
+
+        key = (self.cs_old, self.us_old)
+        if key in MATRIX_LIST:
+            self.old_matrix = MATRIX_LIST[key]
+            self.cs_old_label = self.old_matrix.couverture.label
+            self.us_old_label = self.old_matrix.usage.label
+            self.old_is_artif = bool(self.old_matrix.is_artificial)
+        else:
+            if self.cs_old in COUVERTURE_LIST:
+                self.cs_old_label = COUVERTURE_LIST[self.cs_old].label
+            if self.us_old in USAGE_LIST:
+                self.us_old_label = USAGE_LIST[self.us_old].label
+            self.old_is_artif = False
+
+        self.is_new_artif = not self.old_is_artif and self.new_is_artif
+        self.is_new_natural = self.old_is_artif and not self.new_is_artif
+
+    @classmethod
+    def calculate_fields(cls):
+        """override to hook specific label setting."""
+        # update surface field
+        cls.objects.all().filter(surface__isnull=True).update(
+            surface=Cast(
+                Area(Transform("mpoly", 2154)),
+                DecimalField(max_digits=15, decimal_places=4),
+            )
+        )
+
+
+# ##############
+#   ARCACHON
+# ##############
+
+
+class ArcachonOcsge2015(AutoLoadMixin, Ocsge):
+    """
+    Données de l'OCSGE pour l'année 2015
+    Données fournies par Philippe 09/2021
+    python manage.py load_data --class public_data.models.Ocsge2015
+    """
+
+    shape_file_path = "OCSGE_2015.zip"
+    _year = 2015
+    mapping = {
+        "couverture": "couverture",
+        "usage": "usage",
+        "mpoly": "MULTIPOLYGON",
+    }
+
+    @classmethod
+    def clean_data(cls):
+        """Delete only data with year=2015"""
+        gironde = Departement.objects.get(id=32)
+        # select only data covered by Gironde
+        qs = cls.objects.filter(mpoly__intersects=gironde.mpoly)
+        # only current millesime
+        qs = qs.filter(year=cls._year)
+        qs.delete()
+
+    def save(self, *args, **kwargs):
+        self.year = self.__class__._year
+        key = (self.couverture, self.usage)
+        try:
+            self.matrix = MATRIX_LIST[key]
+            self.is_artificial = bool(self.matrix.is_artificial)
+            self.couverture_label = self.matrix.couverture.label
+            self.usage_label = self.matrix.usage.label
+        except KeyError:
+            self.is_artificial = False
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        proxy = True
+
+
+class ArcachonOcsge2018(ArcachonOcsge2015):
+    hape_file_path = "OCSGE_2018.zip"
+    _year = 2018
+
+    class Meta:
+        proxy = True
+
+
+class ArcachonArtif(AutoOcsgeDiff):
+    """
+    A_B_2015_2018 : la surface (en hectares) artificialisée entre 2015 et 2018
+    Données construites par Philippe
+    """
+
+    class Meta:
+        proxy = True
+
+    shape_file_path = "a_b_2015_2018.zip"
+    _year_new = 2018
+    _year_old = 2015
+    mapping = {
+        "surface": "Surface",
+        "cs_new": "cs_2018",
+        "us_new": "us_2018",
+        "cs_old": "cs_2015",
+        "us_old": "us_2015",
+        "mpoly": "MULTIPOLYGON",
+    }
+
+    def save(self, *args, **kwargs):
+        self.before_save()
+
+        self.new_is_artif = True
+        self.old_is_artif = False
+        self.is_new_artif = True
+        self.is_new_natural = False
+
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def clean_data(cls, clean_queryset=None):
+        gironde = Departement.objects.get(id=32)
+        # select only data covered by Gironde
+        qs = cls.objects.filter(mpoly__intersects=gironde.mpoly)
+        # only current millesime
+        qs = qs.filter(year_new=cls._year_new, year_old=cls._year_old)
+        qs = qs.filter(is_new_natural=False, is_new_artif=True)
+        qs.delete()
+
+
+class ArcachonRenat(ArcachonArtif):
+    class Meta:
+        proxy = True
+
+    shape_file_path = "a_b_2015_2018.zip"
+
+    def save(self, *args, **kwargs):
+        self.before_save()
+
+        self.new_is_artif = False
+        self.old_is_artif = True
+        self.is_new_artif = False
+        self.is_new_natural = True
+
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def clean_data(cls, clean_queryset=None):
+        gironde = Departement.objects.get(id=32)
+        # select only data covered by Gironde
+        qs = cls.objects.filter(mpoly__intersects=gironde.mpoly)
+        # only current millesime
+        qs = qs.filter(year_new=cls._year_new, year_old=cls._year_old)
+        qs = qs.filter(is_new_natural=True, is_new_artif=False)
+        qs.delete()
+
+
+# ##########
+#   GERS
+# ##########
+
+
+class GersOcsge(AutoLoadMixin, Ocsge):
+    class Meta:
+        proxy = True
+
+    mapping = {
+        "id_source": "ID",
+        "couverture": "CODE_CS",
+        "usage": "CODE_US",
+        "millesime_source": "MILLESIME",
+        "id_origine": "ID_ORIGINE",
+        "mpoly": "MULTIPOLYGON",
+    }
+
+    def save(self, *args, **kwargs):
+        key = (self.couverture, self.usage)
+        self.matrix = MATRIX_LIST[key]
+        self.is_artificial = bool(self.matrix.is_artificial)
+        self.couverture_label = self.matrix.couverture.label
+        self.usage_label = self.matrix.usage.label
+        self.year = self.__class__.year
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def clean_data(cls, clean_queryset=None):
+        """Delete only data with year=2015"""
+        gers = Departement.objects.get(id=33)
+        # select only data covered by Gers
+        qs = cls.objects.filter(mpoly__intersects=gers.mpoly)
+        # only current millesime
+        qs = qs.filter(year=cls.year)
+        qs.delete()
+
+
+class GersOcsge2016(GersOcsge):
+    class Meta:
+        proxy = True
+
+    shape_file_path = "gers_ocsge_2016.zip"
+    year = 2016
+
+
+class GersOcsge2019(GersOcsge):
+    class Meta:
+        proxy = True
+
+    shape_file_path = "gers_ocsge_2019.zip"
+    year = 2019
+
+
+class GersOcsgeDiff(AutoOcsgeDiff):
+    class Meta:
+        proxy = True
+
+    _year_new = 2019
+    _year_old = 2016
+
+    shape_file_path = "gers_diff_2016_2019.zip"
+    # mapping cible
+    # mapping = {
+    #     "cs_new": "CS_nouveau",
+    #     "us_new": "US_nouveau",
+    #     "cs_old": "CS_ancien",
+    #     "us_old": "US_ancien",
+    #     "mpoly": "MULTIPOLYGON",
+    # }
+    # mapping provisoir avec les données erronées
+    mapping = {
+        "cs_old": "CS_nouveau",
+        "us_new": "US_nouveau",
+        "cs_new": "CS_ancien",
+        "us_old": "US_ancien",
+        "mpoly": "MULTIPOLYGON",
+    }
+
+    @classmethod
+    def clean_data(cls, clean_queryset=None):
+        gers = Departement.objects.get(id=33)
+        # select only data covered by Gers
+        qs = cls.objects.filter(mpoly__intersects=gers.mpoly)
+        # only current millesime
+        qs = qs.filter(year_new=cls._year_new, year_old=cls._year_old)
+        qs.delete()
+
+
+class GersZoneConstruite2016(AutoLoadMixin, ZoneConstruite):
+    class Meta:
+        proxy = True
+
+    _year = 2016
+    shape_file_path = "gers_zone_construite_2016.zip"
+    mapping = {
+        "id_source": "ID",
+        "millesime": "MILLESIME",
+        "mpoly": "MULTIPOLYGON",
+    }
+
+    def save(self, *args, **kwargs):
+        self.year = self._year
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def clean_data(cls, clean_queryset=None):
+        gers = Departement.objects.get(id=33)
+        # select only data covered by Gers
+        qs = cls.objects.filter(mpoly__intersects=gers.mpoly)
+        # only current millesime
+        qs = qs.filter(year=cls._year)
+        qs.delete()
+
+
+class GersZoneConstruite2019(GersZoneConstruite2016):
+    _year = 2019
+    shape_file_path = "gers_zone_construite_2019.zip"
+
+    class Meta:
+        proxy = True
+
+
+class Command(BaseCommand):
+    help = "Load all data from OCS GE"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--item",
+            type=str,
+            help="item that you want to load ex: GersOcsge2016, ZoneConstruite2019...",
+        )
+        parser.add_argument(
+            "--truncate",
+            action="store_true",
+            help="if you want to completly restart tables including id, not compatible with --item",
+        )
+        parser.add_argument(
+            "--no-verbose",
+            action="store_true",
+            help="reduce output",
+        )
+
+    def handle(self, *args, **options):
+        logger.info("Load OCSGE")
+        self.verbose = not options["no_verbose"]
+        item_list = [
+            ArcachonOcsge2018,
+            ArcachonOcsge2015,
+            ArcachonArtif,
+            ArcachonRenat,
+            GersOcsge2016,
+            GersOcsge2019,
+            GersOcsgeDiff,
+            GersZoneConstruite2016,
+            GersZoneConstruite2019,
+        ]
+        if options["item"]:
+            self.load([i for i in item_list if i.__name__ == options["item"]])
+        else:
+            logger.info("Full load")
+            if options["truncate"]:
+                self.truncate()
+            self.load(item_list)
+        logger.info("End loading OCSGE")
+
+    def truncate(self):
+        logger.info("Truncate Ocsge, OcsgeDiff and ZoneConstruite")
+        Ocsge.truncate()
+        OcsgeDiff.truncate()
+        ZoneConstruite.truncate()
+
+    def load(self, item_list):
+        logger.info("Items to load: %d", len(item_list))
+        for item in item_list:
+            logger.info("Load data for: %s", item.__name__)
+            item.load(verbose=self.verbose)
--- public_data/models/ocsge.py
+++ public_data/models/ocsge.py
@@ -28,34 +28,29 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models import Sum, OuterRef, Subquery
 from django.db.models.functions import Cast
 
-from .mixins import AutoLoadMixin, DataColorationMixin
+from .mixins import DataColorationMixin, TruncateTableMixin
 
-from .couverture_usage import CouvertureSol, UsageSol, CouvertureUsageMatrix
+from .couverture_usage import CouvertureUsageMatrix
 
 
-class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
+class Ocsge(TruncateTableMixin, DataColorationMixin, models.Model):
     couverture = models.CharField(
         "Couverture du sol", max_length=254, blank=True, null=True
     )
     usage = models.CharField("Usage du sol", max_length=254, blank=True, null=True)
-    millesime = models.DateField("Millésime", blank=True, null=True)
-    source = models.CharField("Source", max_length=254, blank=True, null=True)
-    origine = models.CharField("Origine", max_length=254, blank=True, null=True)
-    origine2 = models.CharField("Origine1", max_length=254, blank=True, null=True)
-    ossature = models.IntegerField("Ossature", blank=True, null=True)
-    commentaire = models.CharField("Commentaire", max_length=254, blank=True, null=True)
-    year = models.IntegerField(
-        "Année", validators=[MinValueValidator(2000), MaxValueValidator(2050)]
-    )
     # Nouveaux champs pour le Gers
     id_source = models.CharField("ID source", max_length=200, blank=True, null=True)
     id_origine = models.CharField("ID origine", max_length=200, blank=True, null=True)
-    code_or = models.CharField("Code OR", max_length=200, blank=True, null=True)
     millesime_source = models.CharField(
         "Source du millesime", max_length=200, blank=True, null=True
     )
+    # deprecated. to keep for Arcachon
+    millesime = models.DateField("Millésime", blank=True, null=True)
 
     # calculated fields
+    year = models.IntegerField(
+        "Année", validators=[MinValueValidator(2000), MaxValueValidator(2050)]
+    )
     matrix = models.ForeignKey(
         CouvertureUsageMatrix, on_delete=models.PROTECT, null=True, blank=True
     )
@@ -155,7 +150,7 @@ class Ocsge(AutoLoadMixin, DataColorationMixin, models.Model):
         cls.objects.all().filter(**filter_kwargs).update(**update_kwargs)
 
 
-class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
+class OcsgeDiff(TruncateTableMixin, DataColorationMixin, models.Model):
     year_old = models.IntegerField(
         "Ancienne année", validators=[MinValueValidator(2000), MaxValueValidator(2050)]
     )
@@ -172,8 +167,6 @@ class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
         "Code nouveau usage", max_length=12, blank=True, null=True
     )
     us_old = models.CharField("Code ancien usage", max_length=12, blank=True, null=True)
-    oss_old = models.CharField("Ancien OSS", max_length=200, blank=True, null=True)
-    oss_new = models.CharField("Nouveau OSS", max_length=200, blank=True, null=True)
     mpoly = models.MultiPolygonField()
 
     # calculated fields
@@ -211,92 +204,11 @@ class OcsgeDiff(AutoLoadMixin, DataColorationMixin, models.Model):
         related_name="ocsge_difnew",
     )
 
-    # shape_file_path = "gers_diff_2016_2019.zip"  # "media/gers/DIFF_2016_2019.zip"
     default_property = "surface"
     default_color = "Red"
-    mapping = {
-        "cs_new": "CS_nouveau",
-        "us_new": "US_nouveau",
-        "oss_new": "OSS_nouvea",
-        "cs_old": "CS_ancien",
-        "us_old": "US_ancien",
-        "oss_old": "OSS_ancien",
-        "mpoly": "MULTIPOLYGON",
-    }
-
-    @classmethod
-    def calculate_fields(cls):
-        """override to hook specific label setting."""
-        # update surface field
-        cls.objects.all().filter(surface__isnull=True).update(
-            surface=Cast(
-                Area(Transform("mpoly", 2154)),
-                models.DecimalField(max_digits=15, decimal_places=4),
-            )
-        )
-        # query = """
-        # with t as (
-        # select
-        #     o.id,
-        #     cs_old.label as cs_old_label,
-        #     us_old.label as us_old_label,
-        #     cs_new.label as cs_new_label,
-        #     us_new.label as us_new_label,
-        #     m_old.is_artificial as old_is_artif,
-        #     m_new.is_artificial as new_is_artif,
-        #     m_new.is_artificial = true
-        #     and m_old.is_natural = true as is_new_artif,
-        #     m_old.is_artificial = true
-        #     and m_new.is_natural = true as is_new_naf
-        # from
-        #     public_data_ocsgediff o
-        # left join public_data_couverturesol cs_old on
-        #     o.cs_old = cs_old.code_prefix
-        # left join public_data_usagesol us_old on
-        #     o.us_old = us_old.code_prefix
-        # inner join public_data_couvertureusagematrix m_old
-        #         on
-        #     m_old.couverture_id = cs_old.id
-        #     and m_old.usage_id = us_old.id
-        # left join public_data_couverturesol cs_new on
-        #     o.cs_new = cs_new.code_prefix
-        # left join public_data_usagesol us_new on
-        #     o.us_new = us_new.code_prefix
-        # inner join public_data_couvertureusagematrix m_new
-        #         on
-        #     m_new.couverture_id = cs_new.id
-        #     and m_new.usage_id = us_new.id
-        # )
-        # update
-        #     public_data_ocsgediff o
-        # set
-        #     cs_old_label = t.cs_old_label,
-        #     us_old_label = t.us_old_label,
-        #     cs_new_label = t.cs_new_label,
-        #     us_new_label = t.us_new_label,
-        #     old_is_artif = t.old_is_artif,
-        #     new_is_artif = t.new_is_artif,
-        #     is_new_artif = t.is_new_artif,
-        #     is_new_naf = t.is_new_naf
-        # from
-        #     t
-        # where
-        #     o.id = t.id;
-        # """
-        # with connection.cursor() as cursor:
-        #     cursor.execute(query)
-
-    @classmethod
-    def set_labels(cls):
-        """Set label for cs and us fields."""
-        for fieldname in ["cs_old", "us_old", "cs_new", "us_new"]:
-            if fieldname.startswith("cs"):
-                cls.set_label(CouvertureSol, fieldname, f"{fieldname}_label")
-            else:
-                cls.set_label(UsageSol, fieldname, f"{fieldname}_label")
 
 
-class ZoneConstruite(AutoLoadMixin, DataColorationMixin, models.Model):
+class ZoneConstruite(TruncateTableMixin, DataColorationMixin, models.Model):
     id_source = models.CharField("ID Source", max_length=200)
     millesime = models.CharField("Millesime", max_length=200)
     mpoly = models.MultiPolygonField()
@@ -310,12 +222,6 @@ class ZoneConstruite(AutoLoadMixin, DataColorationMixin, models.Model):
     surface = models.DecimalField(
         "surface", max_digits=15, decimal_places=4, blank=True, null=True
     )
-
-    mapping = {
-        "id_source": "ID",
-        "millesime": "MILLESIME",
-        "mpoly": "MULTIPOLYGON",
-    }
 
     @classmethod
     def calculate_fields(cls):
