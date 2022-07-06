90bf1477e8aae80aa8df52055b4433a238275b84
Swann <swann.bouviermuller@gmail.com>
2022-07-06 18:13:02 +0200

    remove migrate from start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Execute migrations
-python manage.py migrate users
-python manage.py migrate
 
-# load new parameters if any
-python manage.py load_param --no-update --file required_parameters.json
 
 # launch webserver
 gunicorn config.wsgi \
