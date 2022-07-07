c9825d895cb7bdc11bd57892116d050e7dda08da
Swann <swann.bouviermuller@gmail.com>
2022-07-07 17:55:48 +0200

    finish CSP configuration

--- config/settings.py
+++ config/settings.py
@@ -283,6 +283,23 @@ if DEBUG:
         pass
 
 
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "djt_csp.panel.SecurityPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+]
+
+
 # DJANGO DOCX TEMPLATES
 DJANGO_DOCX_TEMPLATES = {
     "data_sources": [
@@ -415,9 +432,10 @@ CSP_SCRIPT_SRC = [
     "https://code.highcharts.com",
 ]
 CSP_STYLE_SRC = ["'self'"]
-CSP_IMG_SRC = ["'self'", "http://www.w3.org"]
+CSP_IMG_SRC = ["'self'", "https://wxs.ign.fr", "data:"]
 CSP_UPGRADE_INSECURE_REQUESTS = not DEBUG
 CSP_INCLUDE_NONCE_IN = ["script-src", "style-src"]
+CSP_CONNECT_SRC = ["https://stats.data.gouv.fr", "'self'"]
 
 # LOGGING SETTINGS
 
