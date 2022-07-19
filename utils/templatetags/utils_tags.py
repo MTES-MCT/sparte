3f9b521baca8f82e3f683bf24f1a11f6561b64cd
Swann <swann.bouviermuller@gmail.com>
2022-07-19 15:32:48 +0200

    start of matomo push event integration

--- utils/templatetags/utils_tags.py
+++ utils/templatetags/utils_tags.py
@@ -10,3 +10,13 @@ def tracking_code(context):
         "CSP_NONCE": context["CSP_NONCE"],
         "DISPLAY": not settings.DEBUG,
     }
+
+
+@register.inclusion_tag("utils/matomo_event.html", takes_context=True)
+def push_event(context, category, action, name):
+    return {
+        "CSP_NONCE": context["CSP_NONCE"],
+        "CATEGORY": category,
+        "ACTION": action,
+        "NAME": name,
+    }
