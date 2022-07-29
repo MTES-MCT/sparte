90bcda21fb496cc1220496daec3a1b7d2b8671fb
Swann <swann.bouviermuller@gmail.com>
2022-07-29 14:39:14 +0200

    display cover on diagnostic's list

---
+++ config/storages.py
@@ -0,0 +1,9 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    """Enable access to data folder at root of the bucket."""
+
+    location = "media"
+    default_acl = "public-read"
+    file_overwrite = False
