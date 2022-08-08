4945a28470046d6272bf97a052e1fe8ffd5a6678
Swann <swann.bouviermuller@gmail.com>
2022-08-08 15:28:29 +0200

    Store contact form into db

---
+++ home/models.py
@@ -0,0 +1,32 @@
+import traceback
+from django.db import models
+from django.utils import timezone
+
+
+class ContactForm(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING", "Awaiting celery to process the task"
+        SUCCESS = "SUCCESS", "Task done with success"
+        FAILED = "FAILED", "Task failed"
+
+    email = models.EmailField("Votre courriel")
+    content = models.TextField("Votre message")
+
+    status = models.CharField(
+        max_length=10, choices=StatusChoices.choices, default=StatusChoices.PENDING
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    processed_date = models.DateTimeField(null=True, blank=True)
+    error = models.TextField(null=True, blank=True)
+
+    def success(self):
+        self.processed_date = timezone.now()
+        self.status = self.StatusChoices.SUCCESS
+        self.error = None
+        self.save()
+
+    def handle_exception(self):
+        self.processed_date = timezone.now()
+        self.status = self.StatusChoices.FAILED
+        self.error = traceback.format_exc()
+        self.save()
