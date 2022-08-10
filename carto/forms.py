a99495f19d07c2ba3123dfd0e37021f1aab1f23e
Swann <swann.bouviermuller@gmail.com>
2022-08-10 10:21:46 +0200

    remove unused files from carto app

--- carto/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class DisplayOcsgeForm(forms.Form):
-    millesime = forms.ChoiceField(choices=[("2015", "2015"), ("2018", "2018")])
-    color = forms.ChoiceField(
-        choices=[("usage", "Usage des sols"), ("couverture", "Couverture des sols")]
-    )
