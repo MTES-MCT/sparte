ff0c3966ca647f49217b71ac752e2656281710ea
Swann <swann.bouviermuller@gmail.com>
2022-09-01 14:15:42 +0200

    fix dl report breadcrumbs

--- utils/views_mixins.py
+++ utils/views_mixins.py
@@ -16,9 +16,7 @@ class GetObjectMixin:
 
 class BreadCrumbMixin:
     def get_context_breadcrumbs(self):
-        return [
-            {"href": reverse_lazy("home:home"), "title": "Accueil"},
-        ]
+        return [{"href": reverse_lazy("home:home"), "title": "Accueil"}]
 
     def get_context_data(self, **kwargs):
         breadcrumbs = self.get_context_breadcrumbs()
