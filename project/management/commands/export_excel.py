3595169e7139c573aba44a20f8bd856400adfbb6
Swann <swann.bouviermuller@gmail.com>
2022-09-22 16:09:10 +0200

    simple export

---
+++ project/management/commands/export_excel.py
@@ -0,0 +1,57 @@
+import calendar
+from datetime import datetime, date
+import logging
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from project.models.export import export_dl_diag
+from project.storages import ExportStorage
+
+
+logger = logging.getLogger("management.commands")
+
+
+class Command(BaseCommand):
+    help = ""
+
+    def add_arguments(self, parser):
+        parser.add_argument("--local", action="store_true")
+        parser.add_argument(
+            "--start",
+            type=lambda s: date.fromisoformat(s),
+        )
+        parser.add_argument(
+            "--end",
+            type=lambda s: date.fromisoformat(s),
+        )
+
+    def handle(self, *args, **options):
+        logger.info("Start export stats")
+        if options["start"]:
+            start = options["start"]
+        else:
+            n = date.today()
+            start = date(day=1, month=n.month, year=n.year)
+        if options["end"]:
+            end = options["end"]
+        else:
+            _, last_day = calendar.monthrange(year=start.year, month=start.month)
+            end = date(day=last_day, month=start.month, year=start.year)
+
+        if isinstance(start, datetime):
+            start = timezone.make_aware(start)
+        if isinstance(end, datetime):
+            end = timezone.make_aware(end)
+        logger.info("Start: %s", start)
+        logger.info("End: %s", end)
+
+        excel_file = export_dl_diag(start, end)
+        filename = f"stats_{start.strftime('%d%m%Y')}_{end.strftime('%d%m%Y')}.xlsx"
+        if options["local"]:
+            with open(filename, "wb") as f:
+                f.write(excel_file.read())
+        else:
+            storage = ExportStorage()
+            path = storage.save_xlsx(filename, excel_file)
+            print(f"Url to file: {storage.url(path)}")
