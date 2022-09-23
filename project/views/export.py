98a4003d5560247beb09cf2c85a8907c975283ff
Swann <swann.bouviermuller@gmail.com>
2022-09-23 10:49:11 +0200

    improve layout

--- project/views/export.py
+++ project/views/export.py
@@ -1,3 +1,6 @@
+import re
+from datetime import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
@@ -9,7 +12,16 @@ class ExportListView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         storage = ExportStorage()
-        kwargs["excel_file_list"] = (
-            {"name": f, "url": storage.url(f)} for f in storage.list_excel()
-        )
+        find_date = re.compile(r"(\d{8,8})_(\d{8,8})")
+        kwargs["excel_file_list"] = []
+        for f in storage.list_excel():
+            if f.startswith("diag_downloaded"):
+                dates = find_date.search(f).groups()
+                kwargs["excel_file_list"].append(
+                    {
+                        "start": datetime.strptime(dates[0], "%d%m%Y").date(),
+                        "end": datetime.strptime(dates[1], "%d%m%Y").date(),
+                        "url": storage.url(f),
+                    }
+                )
         return super().get_context_data(**kwargs)
