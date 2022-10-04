8605afcdbbcf1792f64aa7b44f4e0f08ea9cd12e
Swann <swann.bouviermuller@gmail.com>
2022-10-04 11:13:30 +0200

    more tests

--- carto/views.py
+++ carto/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from public_data.models import CouvertureSol
+
 
 class VectorView(LoginRequiredMixin, TemplateView):
     template_name = "carto/vector.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["couv_sol_list"] = CouvertureSol.objects.all()
+        return super().get_context_data(**kwargs)
