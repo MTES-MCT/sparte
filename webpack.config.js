66f7dc708f1faa0951c5a542564b05695bb5c4c9
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-01-04 18:14:17 +0100

    Add dsfr npm package and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,55 @@ const common = {
     entry: "./assets/scripts/index.js",
     output: {
         "path": path.resolve(__dirname, "static"),
-        "filename": "bundle.js"
+        "filename": "assets/scripts/bundle.js",
+    },
+    performance: { 
+        hints: false
     },
     module: {
         rules: [
             {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[hash][ext][query]'
+                }
+            },
+            {
+                "test": /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[hash][ext][query]'
+                }
+            },
+            {
                 test: /\.css$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "svg-loader",
                     "css-loader"
-                ]
-            }
+                ],
+            },
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "[name].css",
+            filename: "assets/styles/[name].css",
             chunkFilename: "[id].css"
         })
-    ],
+    ]
 }
 
 const development = {
     ...common,
     mode: "development",
-    devtool: "cheap-source-map",
+    // devtool: "cheap-source-map",
     plugins: [
         ...common.plugins,
         new BundleAnalyzerPlugin({
-            analyzerPort: "8989"
+            analyzerPort: "8989",
+            openAnalyzer: false
         })
-    ],
+    ]
 }
 
 const production = {
@@ -57,7 +74,7 @@ const production = {
     },
     plugins: [
         ...common.plugins
-    ],
+    ]
 }
 
 module.exports = (env, argv) => {
