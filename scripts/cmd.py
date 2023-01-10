af1e3f81898aaa6ff19d751dea363bb55764f7f0
Swann <swann.bouviermuller@gmail.com>
2023-01-10 12:29:47 +0100

    fix cmd name

--- scripts/cmd.py
+++ scripts/cmd.py
@@ -261,7 +261,7 @@ def mep_210(ctx):
 
 @cli.command()
 @click.pass_context
-def mep_220(ctx):
+def mep_240(ctx):
     """Trigger all data transformation to successful MEP release 2.4"""
     click.secho("Start migration v2.4", fg="cyan")
     connecter = ScalingoInterface(ctx.obj)
