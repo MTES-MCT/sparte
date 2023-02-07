364bb23ec2cffa413ca52b12f3f14450978b20c8
Swann <swann.bouviermuller@gmail.com>
2023-02-07 12:31:55 +0100

    move redirecturlmixin

--- utils/views_mixins.py
+++ utils/views_mixins.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import resolve_url
 from django.urls import reverse_lazy
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 class GetObjectMixin:
@@ -23,3 +26,26 @@ class BreadCrumbMixin:
         breadcrumbs[-1]["is_active"] = True
         kwargs.update({"breadcrumbs": breadcrumbs})
         return super().get_context_data(**kwargs)
+
+
+class RedirectURLMixin:
+    """En attendant Django 4"""
+
+    next_page = None
+    redirect_field_name = "next"
+
+    def get_success_url(self):
+        return self.get_redirect_url() or self.get_default_redirect_url()
+
+    def get_redirect_url(self):
+        """Return the user-originating redirect URL if it's safe."""
+        redirect_to = self.request.POST.get(
+            self.redirect_field_name, self.request.GET.get(self.redirect_field_name)
+        )
+        return redirect_to if redirect_to else ""
+
+    def get_default_redirect_url(self):
+        """Return the default redirect URL."""
+        if self.next_page:
+            return resolve_url(self.next_page)
+        raise ImproperlyConfigured("No URL to redirect to. Provide a next_page.")
