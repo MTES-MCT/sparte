04764e9afa489692fb806c616d5d8a237c1769f6
Swann <swann.bouviermuller@gmail.com>
2023-02-21 11:53:27 +0100

    disable debug toolbar independantly and add types

--- config/settings.py
+++ config/settings.py
@@ -290,6 +290,21 @@ if DEBUG:
             "debug_toolbar.middleware.DebugToolbarMiddleware",
         ]
 
+        DEBUG_TOOLBAR_PANELS = [
+            "debug_toolbar.panels.versions.VersionsPanel",
+            "debug_toolbar.panels.timer.TimerPanel",
+            "debug_toolbar.panels.settings.SettingsPanel",
+            "debug_toolbar.panels.profiling.ProfilingPanel",
+            "debug_toolbar.panels.headers.HeadersPanel",
+            "debug_toolbar.panels.request.RequestPanel",
+            "debug_toolbar.panels.sql.SQLPanel",
+            "debug_toolbar.panels.templates.TemplatesPanel",
+            "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+            "debug_toolbar.panels.cache.CachePanel",
+            "debug_toolbar.panels.signals.SignalsPanel",
+            "debug_toolbar.panels.redirects.RedirectsPanel",
+        ]
+
         # bypass check of internal IPs
         def show_toolbar(request):
             return True
@@ -301,22 +316,6 @@ if DEBUG:
     except ImportError:
         pass
 
-
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "djt_csp.panel.SecurityPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-]
 
 
 # DJANGO DOCX TEMPLATES
