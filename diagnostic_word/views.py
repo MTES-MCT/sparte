57dd5a837e4eaa826b07b0a98d5f02a7fb49c47d
Swann <swann.bouviermuller@gmail.com>
2023-02-28 15:34:40 +0100

    make e-mail working for a long time

--- diagnostic_word/views.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import messages
-from django.views.generic import UpdateView
-
-from diagnostic_word.models import WordTemplate
--- project/views/report.py
+++ project/views/report.py
@@ -2,10 +2,8 @@ from decimal import InvalidOperation
 
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.templatetags.static import static
-from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
-from django_app_parameter import app_parameter
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView, TemplateView
 
 from project import charts, tasks
 from project.models import Project, ProjectCommune, Request
@@ -513,3 +511,21 @@ class ProjectReportTarget2031View(ProjectReportBaseView):
         )
 
         return super().get_context_data(**kwargs)
+
+
+class DownloadWordView(TemplateView):
+    template_name = "project/error_download_word.html"
+
+    def get(self, request, *args, **kwargs):
+        """Redirect vers le fichier word du diagnostic si: le diagnostic est public ou est associé à l'utilisateur
+        connecté. Sinon, affiche une page d'erreur."""
+        req = Request.objects.get(pk=self.kwargs["request_id"])
+        if req.project and req.project.is_public:  # le diagnostic est public
+            return HttpResponseRedirect(req.sent_file.url)
+        # le diagnostic n'est pas public, on vérifie que l'utilisateur connecté est bien le propriétaire du diagnostic
+        if request.user.is_anonymous:
+            messages.info(request, "Vous essayez d'accéder à un document privé. Veuillez vous connecter.")
+            return HttpResponseRedirect(f"{reverse('users:signin')}?next={request.path}")
+        if request.user.id == req.user_id:
+            return HttpResponseRedirect(req.sent_file.url)
+        return super().get(request, *args, **kwargs)
