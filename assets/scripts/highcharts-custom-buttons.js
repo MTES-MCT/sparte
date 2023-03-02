d8ef493fc9c572a934787dad570abda9cf68b8e7
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-03-02 14:41:02 +0100

    Refactoring chart options buttons

---
+++ assets/scripts/highcharts-custom-buttons.js
@@ -0,0 +1,29 @@
+window.htmx = require('htmx.org');
+
+window.htmx.onLoad(function() {
+    // Trigger full screen chart
+    document.querySelectorAll('.fullscreen-chart').forEach( button => {
+        button.onclick = function () {
+            const target = button.dataset.chartTarget;
+            const chartDom = document.getElementById(target);
+            const chart = Highcharts.charts[Highcharts.attr(chartDom, 'data-highcharts-chart')];
+        
+            chart.fullscreen.toggle();
+        }
+    });
+
+    // Export chart
+    document.querySelectorAll('.export-chart').forEach( button => {
+        button.onclick = function (e) {
+            const target = button.dataset.chartTarget;
+            const chartDom = document.getElementById(target);
+            const chart = Highcharts.charts[Highcharts.attr(chartDom, 'data-highcharts-chart')];
+            
+            e.preventDefault();
+
+            chart.exportChart({
+                type: button.dataset.type
+            });
+        }
+    });
+});
