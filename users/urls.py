3d986585ed05728ff751bdb16b041821a423d167
Swann <swann.bouviermuller@gmail.com>
2023-03-24 17:13:05 +0100

    remove unused import and code formatting

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 from django.contrib.auth.views import (
-    PasswordResetView, 
-    PasswordResetDoneView, 
+    PasswordResetView,
+    PasswordResetDoneView,
     PasswordResetConfirmView,
-    PasswordResetCompleteView
+    PasswordResetCompleteView,
 )
 
 app_name = "users"
@@ -16,9 +16,29 @@ urlpatterns = [
     path("signin/", views.SigninView.as_view(), name="signin"),
     path("profile/", views.ProfilFormView.as_view(), name="profile"),
     path("password/", views.UpdatePwFormView.as_view(), name="password"),
-    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
-    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
     path("unsubscribe/", views.UserDeleteView.as_view(), name="unsubscribe"),
 ]
