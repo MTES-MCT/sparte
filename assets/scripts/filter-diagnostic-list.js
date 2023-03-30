f55078831fb35075f25e8d6c44dfa38ea01bb9bd
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-03-30 23:39:06 +0200

    Add filter no result message

--- assets/scripts/filter-diagnostic-list.js
+++ assets/scripts/filter-diagnostic-list.js
@@ -2,16 +2,29 @@ const filterDiagnosticListInput = document.getElementById('filter-diagnostic-lis
 
 filterDiagnosticListInput.onkeyup = function () {
     const keyword = filterDiagnosticListInput.value.toLowerCase();
-    let elements = document.querySelectorAll('.project-list-item');
+    let elements = document.querySelectorAll('.diagnostic-list-item');
     elements = Array.from(elements);
 
     elements.map((element) => {
-        const name = element.querySelector('.project-list-item-key').dataset.key.toLowerCase();
+        const name = element.querySelector('.diagnostic-list-item-key').dataset.key.toLowerCase();
         if (name.indexOf(keyword) != -1) {
             element.removeAttribute('hidden');
+            element.style.visibility = 'visible';
         }
         else {
             element.setAttribute('hidden', true);
+            element.style.visibility = 'hidden';
         }
-    })
-}
+    });
+
+    // Get hidden elements count
+    const emptyMessage = document.getElementById('diagnostic-list-empty-message');
+    if (elements.length === elements.filter((obj) => obj.hidden).length) {
+        emptyMessage.removeAttribute('hidden');
+        emptyMessage.style.visibility = 'visible';
+    }
+    else {
+        emptyMessage.setAttribute('hidden', true);
+        emptyMessage.style.visibility = 'hidden';
+    }
+};
