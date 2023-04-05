0352e59f1a9c31fa3c653cb767efe5877482dded
Swann <swann.bouviermuller@gmail.com>
2023-04-05 10:10:48 +0200

    add trajectory models

--- trajectory/models.py
+++ trajectory/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from project.models import Project
+
+
+class Trajectory(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    start = models.IntegerField("Année de début", validators=[MinValueValidator(2000), MaxValueValidator(2075)])
+    end = models.IntegerField("Année de fin", validators=[MinValueValidator(2000), MaxValueValidator(2075)])
+    data = models.JSONField("Données")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name', 'start', 'end']
+
+    def __str__(self):
+        return self.name
