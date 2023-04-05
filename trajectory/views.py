4e804932460dc52fb5e596a351b7fcec61013f4e
Swann <swann.bouviermuller@gmail.com>
2023-04-05 18:51:00 +0200

    Initial load

--- trajectory/views.py
+++ trajectory/views.py
@@ -8,23 +8,38 @@ from project import charts
 from project.models import Project
 from project.views import ProjectReportBaseView
 
-from trajectory.forms import SelectYearPeriodForm, UpdateProjectTrajectoryForm
+from trajectory.forms import SelectYearPeriodForm, UpdateProjectTrajectoryForm, UpdateTrajectoryForm
+from trajectory.models import Trajectory
 
 
 class ProjectReportTrajectoryView(ProjectReportBaseView):
     template_name = "trajectory/report_trajectory.html"
     breadcrumbs_title = "Rapport trajectoires"
 
-    def get_context_data(self, **kwargs):
+    def get_select_year_form(self, trajectory: Trajectory | None = None) -> SelectYearPeriodForm:
+        if trajectory:
+            initial = {
+                "start": trajectory.start,
+                "end": trajectory.end,
+            }
+        else:
+            initial = {}
+        return SelectYearPeriodForm(initial=initial)
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
         diagnostic = self.get_object()
+        trajectory = diagnostic.trajectory_set.all().first()
         kwargs.update(
             {
                 "diagnostic": diagnostic,
                 "active_page": "trajectory",
-                "form_period": SelectYearPeriodForm(),
+                "form_period": self.get_select_year_form(trajectory),
             }
         )
-
+        if trajectory:
+            kwargs["form_year"] = UpdateProjectTrajectoryForm(
+                diagnostic, trajectory.start, trajectory.end
+            )
         return super().get_context_data(**kwargs)
 
 
@@ -52,19 +67,29 @@ class ProjectReportTrajectoryPeriodView(FormView):
 
     def form_valid(self, form: SelectYearPeriodForm) -> HttpResponse:
         context = self.get_context_data(form=form)
+        trajectory = self.diagnostic.trajectory_set.all().first()
+        if trajectory:
+            trajectory.start = form.cleaned_data["start"]
+            trajectory.end = form.cleaned_data["end"]
+            trajectory.save()
+        else:
+            trajectory = self.diagnostic.trajectory_set.create(
+                name="Trajectoire 1",
+                start=form.cleaned_data["start"],
+                end=form.cleaned_data["end"],
+                data={},
+            )
         context |= {
             "start": form.cleaned_data["start"],
             "end": form.cleaned_data["end"],
-            "form_consumption": UpdateProjectTrajectoryForm(
-                project=self.diagnostic, start=form.cleaned_data["start"], end=form.cleaned_data["end"]
-            ),
+            "form_consumption": UpdateTrajectoryForm(trajectory),
         }
         return self.render_to_response(context)
 
 
 class ProjectReportTrajectoryConsumptionView(FormView):
     template_name = "trajectory/partials/set_year_consumption.html"
-    form_class = UpdateProjectTrajectoryForm
+    form_class = UpdateTrajectoryForm
 
     @cached_property
     def diagnostic(self):
@@ -72,21 +97,15 @@ class ProjectReportTrajectoryConsumptionView(FormView):
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs() | {
-            "project": self.diagnostic,
-            "start": self.kwargs["start"],
-            "end": self.kwargs["end"],
+            "trajectory": self.diagnostic.trajectory_set.all().first(),
         }
         return kwargs
 
     def get_context_data(self, **kwargs):
-        kwargs |= {
-            "project": self.diagnostic,
-            "start": self.kwargs["start"],
-            "end": self.kwargs["end"],
-        }
+        kwargs |= {"project": self.diagnostic}
         return super().get_context_data(**kwargs)
 
-    def form_valid(self, form: SelectYearPeriodForm) -> HttpResponse:
+    def form_valid(self, form: UpdateTrajectoryForm) -> HttpResponse:
         form.save()
         context = self.get_context_data(form=form) | {
             "trajectory_chart": charts.ObjectiveChart(self.diagnostic),
