0f1096ceb0c880dc5a41fa4a7a728d2e0471d099
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-05-05 21:32:59 +0200

    Config base map

--- assets/scripts/map_v2/index.js
+++ assets/scripts/map_v2/index.js
@@ -6,6 +6,8 @@ export default class SparteMap {
 
         this.targetElement = _options.targetElement
         this.debug = _options.debug
+        this.map_center = _options.map_center
+        this.default_zoom = _options.default_zoom
 
         if (!this.targetElement) {
             console.warn('Missing \'targetElement\' property')
@@ -13,24 +15,81 @@ export default class SparteMap {
         }
 
         this.setConfig()
+        this.setMap()
+        this.setDatasPanel()
+
         if (this.debug)
             this.setDebug()
-        this.setMap()
-    }
-
-    setConfig() {
-        this.config = {}
     }
 
     setDebug() {
         this.debugPanel = document.createElement('div')
         this.debugPanel.className = "mapV2_debug-panel"
-        this.debugPanel.innerHTML = "Mode Debug"
+        this.debugPanel.innerHTML = "<strong>Mode Debug activé</strong>"
         this.targetElement.appendChild(this.debugPanel)
+
+        // Display size
+        this.debugPanelSize = document.createElement('div')
+        this.debugPanelSize.innerHTML = `<strong>Size:</strong> w ${this.config.width} x h ${this.config.height}`
+        this.debugPanel.appendChild(this.debugPanelSize)
+
+        // Display bounds
+        this.debugPanelBounding = document.createElement('div')
+        this.debugPanel.appendChild(this.debugPanelBounding)
+        this.map.on('moveend', () => {
+            this.debugPanelBounding.innerHTML = `<strong>Bounds:</strong> SW ${this.map.getBounds().getSouthWest().toString()}, NE ${this.map.getBounds().getNorthEast().toString()}`
+        });
+
+        // Display zoom level
+        this.debugPanelZoom = document.createElement('div')
+        this.debugPanel.appendChild(this.debugPanelZoom)
+        this.map.on('zoomend', () => {
+            this.debugPanelZoom.innerHTML = `<strong>Zoom level:</strong> ${this.map.getZoom()}`
+        });
+    }
+
+    setConfig() {
+        this.config = {}
+
+        // Width and height
+        const boundings = this.targetElement.getBoundingClientRect()
+        this.config.width = boundings.width
+        this.config.height = boundings.height || window.innerHeight
     }
 
     setMap() {
-        this.map = L.map(this.targetElement.id).setView([51.505, -0.09], 13)
+        this.map = L.map(this.targetElement.id, {
+            center: this.map_center,
+            zoom: this.default_zoom,
+            minZoom: 6,
+        })
+
+        // Set max bounds
+        let southWest = L.latLng(41.650542, -6.855469),
+            northEast = L.latLng(51.830852, 13.359375),
+            bounds = L.latLngBounds(southWest, northEast);
+        this.map.setMaxBounds(bounds);
+
+        // Get IGN tiles
+        L.tileLayer(
+            'https://wxs.ign.fr/{ignApiKey}/geoportail/wmts?' +
+            '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM' +
+            '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}' +
+            '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}', {
+                attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+                ignApiKey: 'ortho',
+                ignLayer: 'ORTHOIMAGERY.ORTHOPHOTOS',
+                style: 'normal',
+                format: 'image/jpeg',
+                service: 'WMTS'
+            }
+        ).addTo(this.map)
+    }
+
+    setDatasPanel() {
+        this.datasPanel = document.createElement('div')
+        this.datasPanel.className = "mapV2_datas-panel"
+        this.targetElement.appendChild(this.datasPanel)
     }
 }
 
