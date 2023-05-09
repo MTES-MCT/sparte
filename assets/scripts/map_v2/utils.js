89fc43e31db4adde3fe47bf826b208ccd915d9a7
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-05-09 11:21:51 +0200

    Add geo layers style and async selector

--- assets/scripts/map_v2/utils.js
+++ assets/scripts/map_v2/utils.js
@@ -1,21 +1,3 @@
-export const getColor = () => {
-    let letters = '0123456789ABCDEF'
-    let color = '#'
-    for (let i = 0; i < 6; i++)
-        color += letters[Math.floor(Math.random() * 16)]
-    return color
-}
-
-export const polyStyle = (feature) => {
-    return {
-        fillColor: getColor(feature),
-        fillOpacity: 0.5,
-        weight: 1,
-        opacity: 0.1,
-        color: 'white',
-    }
-}
-
 export const slugify = str =>
   str
     .toLowerCase()
