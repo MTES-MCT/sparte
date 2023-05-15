e3427202745c7d1f867fde0e3768cc5b0c44364d
Swann <swann.bouviermuller@gmail.com>
2023-05-15 17:59:05 +0200

    export all departements

--- public_data/management/commands/export_gpu.py
+++ public_data/management/commands/export_gpu.py
@@ -14,6 +14,25 @@ from public_data.storages import DataStorage
 logger = logging.getLogger("management.commands")
 
 
+FIELD_NAMES = [
+    "gid",
+    "libelle",
+    "libelong",
+    "typezone",
+    "insee",
+    "idurba",
+    "idzone",
+    "lib_idzone",
+    "partition",
+    "destdomi",
+    "nomfic",
+    "urlfic",
+    "datappro",
+    "datvalid",
+    "mpoly",
+]
+
+
 def to_b64_utf8(words: str) -> str:
     if words:
         words = str(words)
@@ -34,50 +53,43 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Export GPU to csv on s3")
 
+        self.storage = DataStorage()
+        self.storage.file_overwrite = True
+
+        qs = Departement.objects.all().order_by("name")
+
         dept_param = options["dept"]
-        qs = Departement.objects.filter(Q(source_id=dept_param) | Q(name=dept_param))
-        if not dept_param or not qs.exists():
-            raise ValueError("You must provide a valid departement")
-        dept = qs.first()
-        logger.info("Departement selected: %s", dept.name)
+        if dept_param:
+            qs = Departement.objects.filter(Q(source_id=dept_param) | Q(name=dept_param))
+            if not qs.exists():
+                raise ValueError(f"{dept_param} is not a valid departement")
+
+        logger.info("Total departement to process: %d", qs.count())
+
+        for dept in qs:
+            self.process_one(dept)
+
+        logger.info("End exporting GPU")
+
+    def process_one(self, dept: Departement) -> None:
+        logger.info("Departement processed: %s", dept.name)
 
         bcontent = io.BytesIO()
         StreamWriter = codecs.getwriter('utf-8')
         stream = StreamWriter(bcontent)
         writer = csv.writer(stream, delimiter=";", quotechar='"', quoting=csv.QUOTE_ALL)
 
-        field_names = [
-            "gid",
-            "libelle",
-            "libelong",
-            "typezone",
-            "insee",
-            "idurba",
-            "idzone",
-            "lib_idzone",
-            "partition",
-            "destdomi",
-            "nomfic",
-            "urlfic",
-            "datappro",
-            "datvalid",
-            "mpoly",
-        ]
-        writer.writerow(field_names)
+        writer.writerow(FIELD_NAMES)
 
         qs = ZoneUrba.objects.intersect(dept.mpoly)
         total = qs.count()
         logger.info("Exporting %d zones", total)
-        for row in qs.values_list(*field_names):
+        for row in qs.values_list(*FIELD_NAMES):
             writer.writerow(map(to_b64_utf8, row))
 
         filename = f"GPU/{dept.source_id}_{dept.name}.csv"
-        # data = stream.getvalue().encode("utf-8")
         bcontent.seek(0)
         logger.info("Writing file to S3")
-        storage = DataStorage()
-        storage.file_overwrite = True
-        final_name = storage.save(filename, bcontent)
-        logger.info("File created: %s", final_name)
 
-        logger.info("End exporting GPU")
+        final_name = self.storage.save(filename, bcontent)
+        logger.info("File created: %s", final_name)
