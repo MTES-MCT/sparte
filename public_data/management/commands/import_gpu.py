9a4f9fcfb3d6144228aa61eb7acd652b4d774dcc
Swann <swann.bouviermuller@gmail.com>
2023-05-15 18:11:48 +0200

    Import all departements

--- public_data/management/commands/import_gpu.py
+++ public_data/management/commands/import_gpu.py
@@ -12,10 +12,33 @@ from public_data.storages import DataStorage
 logger = logging.getLogger("management.commands")
 
 
+FIELD_NAMES = [
+    "gid",
+    "libelle",
+    "libelong",
+    "typezone",
+    "insee",
+    "idurba",
+    "idzone",
+    "lib_idzone",
+    "partition",
+    "destdomi",
+    "nomfic",
+    "urlfic",
+    "datappro",
+    "datvalid",
+    # "mpoly",
+]
+
+
 def from_b64_to_str(base64_unicode):
     base64_bytes = base64_unicode.encode("utf-8")
     str_bytes = base64.b64decode(base64_bytes)
     return str_bytes.decode("utf-8")
+
+
+class MissingFile(Exception):
+    pass
 
 
 class Command(BaseCommand):
@@ -31,47 +54,44 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Import GPU from S3")
 
+        self.storage = DataStorage()
+
+        qs = Departement.objects.all().order_by("name")
+
         dept_param = options["dept"]
-        qs = Departement.objects.filter(Q(source_id=dept_param) | Q(name=dept_param))
-        if not dept_param or not qs.exists():
-            raise ValueError("You must provide a valid departement")
-        dept = qs.first()
-        logger.info("Departement selected: %s", dept.name)
+        if dept_param:
+            qs = qs.filter(Q(source_id=dept_param) | Q(name=dept_param))
+            if not qs.exists():
+                raise ValueError(f"{dept_param} is not a valid departement")
+
+        for dept in qs:
+            try:
+                self.process_one(dept)
+            except MissingFile:
+                logger.warning("Missing file for departement %s", dept.name)
+
+        logger.info("End importing GPU")
+
+    def process_one(self, dept: Departement) -> None:
+        logger.info("Departement processed: %s", dept.name)
 
         filename = f"GPU/{dept.source_id}_{dept.name}.csv"
-        storage = DataStorage()
-        if not storage.exists(filename):
-            raise ValueError("File does not exist, have you exported it first ?")
+        if not self.storage.exists(filename):
+            raise MissingFile("File does not exist, have you exported it first ?")
 
         logger.info("Delete previous data from this departement")
         ZoneUrba.objects.intersect(dept.mpoly).delete()
 
         logger.info("Read data from file")
-        s3_file = storage.open(filename, "r")
-        field_names = [
-            "gid",
-            "libelle",
-            "libelong",
-            "typezone",
-            "insee",
-            "idurba",
-            "idzone",
-            "lib_idzone",
-            "partition",
-            "destdomi",
-            "nomfic",
-            "urlfic",
-            "datappro",
-            "datvalid",
-            "mpoly",
-        ]
+        s3_file = self.storage.open(filename, "r")
+
         zones = []
         for i, line in enumerate(s3_file):
             if i == 0:
                 continue
             line_b64 = line.strip()[1:-1].split('";"')
             line_data = list(map(from_b64_to_str, line_b64))
-            line_dict = dict(zip(field_names[:-1], line_data[:-1]))
+            line_dict = dict(zip(FIELD_NAMES, line_data[:-1]))
             try:
                 line_dict["mpoly"] = GEOSGeometry(line_data[-1])
             except ValueError as exc:
@@ -88,5 +108,3 @@ class Command(BaseCommand):
                 zones = []
         ZoneUrba.objects.bulk_create(zones)
         logger.info("Imported %i lines", i)
-
-        logger.info("End importing GPU")
