7a9ea70935151600df48157f73783a5b945c8e0d
Swann <swann.bouviermuller@gmail.com>
2023-06-05 17:57:00 +0200

    upgrade models

--- public_data/models/gpu.py
+++ public_data/models/gpu.py
@@ -2,6 +2,8 @@
 Les zones urbaines fournies par le Géoportail de l'urbanisme
 """
 from django.contrib.gis.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import UniqueConstraint
 
 from utils.db import IntersectMixin
 
@@ -14,7 +16,7 @@ class ZoneUrba(models.Model):
     gid = models.CharField("gid", max_length=80, blank=True, null=True)
     libelle = models.CharField("libelle", max_length=80, blank=True, null=True)
     libelong = models.CharField("libelong", max_length=254, blank=True, null=True)
-    typezone = models.CharField("typezone", max_length=80, blank=True, null=True)
+    origin_typezone = models.CharField("typezone", max_length=80, blank=True, null=True)
     origin_insee = models.CharField("insee", max_length=80, blank=True, null=True)
     idurba = models.CharField("idurba", max_length=80, blank=True, null=True)
     idzone = models.CharField("idzone", max_length=80, blank=True, null=True)
@@ -31,6 +33,26 @@ class ZoneUrba(models.Model):
     # calulated fields
     insee = models.CharField("insee", max_length=10, blank=True, null=True)
     area = models.DecimalField("area", max_digits=15, decimal_places=4, blank=True, null=True)
-    artificial_area = models.DecimalField("artificial_area", max_digits=15, decimal_places=4, blank=True, null=True)
+    typezone = models.CharField("typezone", max_length=3, blank=True, null=True)
 
     objects = ZoneUrbaManager()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["insee"]),
+            models.Index(fields=["typezone"]),
+        ]
+
+
+class ArtifAreaZoneUrba(models.Model):
+    zone_urba = models.ForeignKey(ZoneUrba, on_delete=models.CASCADE)
+    year = models.IntegerField("Millésime", validators=[MinValueValidator(2000), MaxValueValidator(2050)])
+    area = models.DecimalField("Surface artificielle", max_digits=15, decimal_places=4)
+
+    class Meta:
+        constraints = [UniqueConstraint(fields=["zone_urba", "year"], name="unique_zone_year")]
+        indexes = [
+            models.Index(fields=["zone_urba"]),
+            models.Index(fields=["year"]),
+            models.Index(fields=["zone_urba", "year"]),
+        ]
