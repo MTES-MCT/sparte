05db8a5e108dd8874b231c1d2d83bdc5b6bca9c7
Swann <swann.bouviermuller@gmail.com>
2023-07-17 10:17:32 +0200

    fix unit test

--- public_data/test_administration.py
+++ public_data/test_administration.py
@@ -2,7 +2,7 @@ import pytest
 # from django.contrib.gis.geos import MultiPolygon
 from django.contrib.gis.geos import GEOSGeometry
 
-from public_data.models.administration import Departement, Region
+from public_data.models.administration import Departement, Epci, Region
 
 from .models import Commune, CommunePop
 
@@ -17,21 +17,27 @@ SQUARE = GEOSGeometry('MULTIPOLYGON(((2.2 4.2, 5.2 4.2, 5.2 6.6, 2.2 6.6, 2.2 4.
 @pytest.fixture
 def setup_administration_level():
     region = Region.objects.create(
+        source_id="TestRegion",
         name="TestRegion",
-        insee="1",
         mpoly=SQUARE,
     )
     dept = Departement.objects.create(
+        source_id="TestDepartement",
         name="TestDepartement",
-        insee="12",
         mpoly=SQUARE,
         region=region,
+    )
+    epci = Epci.objects.create(
+        source_id="TestEpci",
+        name="TestEpci",
+        mpoly=SQUARE,
     )
     commune = Commune.objects.create(
         name="TestCommune",
         insee="12345",
         mpoly=SQUARE,
         departement=dept,
+        epci=epci,
     )
     return commune
 
@@ -39,7 +45,7 @@ def setup_administration_level():
 @pytest.fixture
 def setup_commune_pop(setup_administration_level):
     commune_pop = CommunePop(
-        commune=setup_administration_level,
+        city=setup_administration_level,
         year=2020,
         pop=1000,
         pop_change=100,
@@ -52,8 +58,8 @@ def setup_commune_pop(setup_administration_level):
 
 @pytest.mark.django_db
 class TestCommunePop:
-    def test_setup_commune_pop(setup_commune, setup_commune_pop):
-        assert setup_commune_pop.commune_id == setup_commune.id
+    def test_setup_commune_pop(self, setup_administration_level, setup_commune_pop):
+        assert setup_commune_pop.city_id == setup_administration_level.id
         assert all(
             [
                 setup_commune_pop.year == 2020,
@@ -63,27 +69,3 @@ class TestCommunePop:
                 setup_commune_pop.household_change == 50,
             ]
         )
-
-    # def test_commune_pop_year_validator_max():
-    #     with pytest.raises(ValueError):
-    #         CommunePop(year=3000)
-
-    # def test_commune_pop_year_validator_min():
-    #     with pytest.raises(ValueError):
-    #         CommunePop(year=1999)
-
-    # def test_commune_pop_pop_can_be_null():
-    #     commune_pop = CommunePop(pop=None)
-    #     assert commune_pop.pop is None
-
-    # def test_commune_pop_pop_change_can_be_null():
-    #     commune_pop = CommunePop(pop_change=None)
-    #     assert commune_pop.pop_change is None
-
-    # def test_commune_pop_household_can_be_null():
-    #     commune_pop = CommunePop(household=None)
-    #     assert commune_pop.household is None
-
-    # def test_commune_pop_household_change_can_be_null():
-    #     commune_pop = CommunePop(household_change=None)
-    #     assert commune_pop.household_change is None
