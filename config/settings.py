071a30cc0569fbb8596d6d5a021d846d2049babe
Swann <swann.bouviermuller@gmail.com>
2023-08-02 17:45:20 +0200

    use dummy cache when local dev

--- config/settings.py
+++ config/settings.py
@@ -244,17 +244,24 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CACHES
 # https://docs.djangoproject.com/en/4.2/topics/cache/
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env.str("REDIS_URL"),
-        "TIMEOUT": 60 * 60 * 9,  # 9 heures
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "MAX_ENTRIES": 1000,
+if ENVIRONMENT in ["local"]:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
         }
     }
-}
+else:
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": env.str("REDIS_URL"),
+            "TIMEOUT": 60 * 60 * 9,  # 9 heures
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                "MAX_ENTRIES": 1000,
+            }
+        }
+    }
 
 
 # SESSION
