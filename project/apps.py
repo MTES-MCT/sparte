6ecb35929135059023cc255948253eea62a66288
Swann <swann.bouviermuller@gmail.com>
2023-08-04 16:57:52 +0200

    add new map field and bool fields

--- project/apps.py
+++ project/apps.py
@@ -6,6 +6,7 @@ def pre_create_historical_record_callback(sender, **kwargs):
     project_history = kwargs['history_instance']
     if project_history.async_theme_map_gpu_done is None:
         project_history.async_theme_map_gpu_done = False
+        project_history.async_thme_map_fill_gpu_done = False
 
 
 class ProjectConfig(AppConfig):
