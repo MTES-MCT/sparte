ac49bbc506edf2f1d756e40cbcb7774218f220fb
Swann <swann.bouviermuller@gmail.com>
2023-08-08 14:51:50 +0200

    fix import order

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from .create import create_from_public_key, trigger_async_tasks
 from .project_base import Emprise, Project, ProjectCommune
 from .request import ErrorTracking, Request
 from .utils import user_directory_path
+from .create import create_from_public_key, trigger_async_tasks
