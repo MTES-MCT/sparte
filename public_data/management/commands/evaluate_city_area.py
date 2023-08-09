f5396aae66f3f003d1cbaf23f82334965c1a100d
Swann <swann.bouviermuller@gmail.com>
2023-08-09 13:20:15 +0200

    add area to city

---
+++ public_data/management/commands/evaluate_city_area.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.contrib.gis.db.models.functions import Area, Transform
+from django.core.management.base import BaseCommand
+
+from public_data.models import Commune
+
+logger = logging.getLogger("management.commands")
+
+
+class Command(BaseCommand):
+    help = "Evaluate city area"
+
+    def handle(self, *args, **options):
+        logger.info("Start evaluation of city area")
+        Commune.objects.all().update(area=Area(Transform("mpoly", 2154)))
+        logger.info("End evaluation of city area")
