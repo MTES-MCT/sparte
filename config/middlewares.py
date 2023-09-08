8c57c744e4f1e1bf37d20b1a9782110d3e022a68
Swann <swann.bouviermuller@gmail.com>
2023-09-08 12:49:51 +0200

    add maintenance state

--- config/middlewares.py
+++ config/middlewares.py
@@ -1,6 +1,11 @@
 import logging
 import time
 
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,3 +24,24 @@ class LogIncomingRequest:
         microseconds = int((duration - seconds - milliseconds / 1000) * 1000000)
         logger.info(f"Request took {seconds}s {milliseconds}ms {microseconds}µs to process")
         return response
+
+
+class MaintenanceModeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Vérifiez si MAINTENANCE_MODE est activé
+        is_maintenance_mode = getattr(settings, "MAINTENANCE_MODE", False)
+
+        maintenance_path = reverse("home:maintenance_mode")
+
+        # Évitez une redirection infinie en autorisant l'accès à la vue de maintenance et à l'admin
+        if (
+            is_maintenance_mode
+            and not request.path.startswith("/admin/")
+            and request.path != maintenance_path
+        ):
+            return HttpResponseRedirect(maintenance_path)
+
+        return self.get_response(request)
