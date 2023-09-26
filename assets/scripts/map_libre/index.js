1b3df214c5653fc88bcf64b6e59e68e23a980358
home_user <sofian.ab@hotmail.fr>
2023-09-26 17:26:02 +0200

    Add ocsge and loading optimization

--- assets/scripts/map_libre/index.js
+++ assets/scripts/map_libre/index.js
@@ -24,6 +24,9 @@ export default class MapLibre {
         this.setConfig()
         this.setTabs()
         this.setMap()
+
+        if (this.debug)
+            this.setDebug()
     }
 
     setConfig() {
@@ -63,11 +66,21 @@ export default class MapLibre {
         })
 
         this.map.on('load', () => {
-            this.map.addControl(new maplibregl.AttributionControl({ compact: true }))
+            // Set sources
+            this.setSources()
 
-            this.map.on('moveend', debounce(() => this.update(), 1000))
-
+            // Set sources
             this.setLayers()
+
+            // Set Filters
+            this.setFilters()
+
+            // Events
+            this.map.on('moveend', debounce(() => this.update(), 1000))
+            this.map.on('sourcedata', (_obj) => this.sourceData(_obj))
+
+            // Controls
+            this.map.addControl(new maplibregl.NavigationControl(), 'top-left')
         })
     }
 
@@ -84,31 +97,87 @@ export default class MapLibre {
             }
         ]
 
+        if (this.debug)
+            tabList.push(
+                {
+                    id: 'debug',
+                    iconClass: 'bi-bug',
+                }
+            )
+
         this.tabs = new Tabs({
             tabList: tabList,
         })
     }
 
-    setLayers() {
+    setSources() {
         this.sources = []
 
         this.layerList.map((_obj) => {
             // Create sources
-            this.sources.push(new Source(_obj.source, _obj.source_query_strings))
+            this.sources.push(new Source(_obj.source, _obj.source_custom_options))
+        })
+    }
 
-            // Create associated layers
-            if (_obj.layers?.length > 0)
-                _obj.layers.map((__obj) => new Layer(__obj))
+    setLayers() {
+        this.layerList.map((_obj) => {
+            // Create layers
+            _obj.layers.map((__obj) => new Layer(__obj))
+        })
+    }
 
-            // Create associated filters
-            if (_obj.filters?.length > 0)
-                new FilterGroup(_obj.name, _obj.filters)
+    setFilters() {
+        this.filters = []
+
+        this.layerList.map((_obj) => {
+            // Create filters
+            if (_obj.filters?.length > 0) {
+                this.filters.push(new FilterGroup(_obj.name, _obj.source.key, _obj.filters))
+            }
+        })
+    }
+
+    setDebug() {
+        this.debugPanel = document.getElementById("debug-tab")
+        this.debugPanel.innerHTML = '<div class="filter-group tab-item"><strong>Mode Debug activé</strong></div>'
+
+        // Display size
+        this.debugPanelSize = document.createElement('div')
+        this.debugPanelSize.classList.add('filter-group', 'tab-item')
+        this.debugPanelSize.innerHTML = `<strong>Size:</strong> w ${this.config.width} x h ${this.config.height}`
+        this.debugPanel.appendChild(this.debugPanelSize)
+
+        // Display bounds
+        this.debugPanelBounding = document.createElement('div')
+        this.debugPanelBounding.classList.add('filter-group', 'tab-item')
+        this.debugPanelBounding.innerHTML = `<strong>Bounds:</strong> SW ${this.map.getBounds().getSouthWest().toString()}, NE ${this.map.getBounds().getNorthEast().toString()}`
+        this.debugPanel.appendChild(this.debugPanelBounding)
+
+        // Display zoom level
+        this.debugPanelZoom = document.createElement('div')
+        this.debugPanelZoom.classList.add('filter-group', 'tab-item')
+        this.debugPanelZoom.innerHTML = `<strong>Zoom level:</strong> ${Math.floor(this.map.getZoom())}`
+        this.debugPanel.appendChild(this.debugPanelZoom)
+
+        // Debug events tracking
+        this.map.on('moveend', () => {
+            this.debugPanelZoom.innerHTML = `<strong>Zoom level:</strong> ${Math.floor(this.map.getZoom())}`
+            this.debugPanelBounding.innerHTML = `<strong>Bounds:</strong> SW ${this.map.getBounds().getSouthWest().toString()}, NE ${this.map.getBounds().getNorthEast().toString()}`
         })
     }
 
     update() {
         if (this.sources)
             this.sources.map((_obj) => _obj.update())
+    }
+
+    sourceData(_source) {
+        if (_source.source.type != 'geojson') // Display loader only for geojson 
+            return
+
+        const filter = this.filters.find((_obj) => _obj.source === _source.sourceId)
+        if (filter)
+            filter.sourceData(_source.isSourceLoaded)
     }
 }
 
