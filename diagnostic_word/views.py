09bbf3313d2371c75037e424eb269f6fd912fc66
Swann <swann.bouviermuller@gmail.com>
2023-10-25 21:42:36 +0200

    remove unused view

--- diagnostic_word/views.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from django.views.generic import TemplateView
-
-
-class DicoView(TemplateView):
-    template_name = "diagnostic_word/dico.html"
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        return super().get_context_data(**kwargs)
