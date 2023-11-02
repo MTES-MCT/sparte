b5147f478f2f85f445df43292663730435c30d83
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-11-02 18:19:00 +0100

    Fix lint

--- documentation/views.py
+++ documentation/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 
 from utils.views_mixins import BreadCrumbMixin
 
+
 class FAQView(BreadCrumbMixin, TemplateView):
     template_name = "documentation/faq.html"
 
