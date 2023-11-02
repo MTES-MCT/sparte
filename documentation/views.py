e8411059cf5025e376efd3a1acaa95e85822a3fb
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2023-11-02 17:08:14 +0100

    Add breadcrumb

--- documentation/views.py
+++ documentation/views.py
@@ -4,3 +4,10 @@ from utils.views_mixins import BreadCrumbMixin
 
 class FAQView(BreadCrumbMixin, TemplateView):
     template_name = "documentation/faq.html"
+
+    def get_context_breadcrumbs(self):
+        breadcrumbs = super().get_context_breadcrumbs()
+        breadcrumbs.append(
+            {"title": "FAQ"},
+        )
+        return breadcrumbs
