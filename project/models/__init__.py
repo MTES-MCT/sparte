8c5e70b573d95cd6d96aad774a88636381fc9741
Alexis A <alexis.athlani@beta.gouv.fr>
2023-11-02 15:27:36 +0100

    Fix erreur d'import circulaire (#68)
    * fix(create_project): remove circular import error
    
    * fix __init__ of project.models
    
    ---------
    
    Co-authored-by: Swann <swann.bouviermuller@gmail.com>

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -9,7 +9,11 @@ __all__ = [
     "user_directory_path",
 ]
 
-from .create import create_from_public_key, trigger_async_tasks
+
 from .project_base import Emprise, Project, ProjectCommune
 from .request import ErrorTracking, Request
 from .utils import user_directory_path
+
+# isort: split
+
+from .create import create_from_public_key, trigger_async_tasks
