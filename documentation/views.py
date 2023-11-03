757c7dd79d066c5cfde099cfb71e7ece51ecf5a5
Sofian Thibaut <sofian.ab@hotmail.fr>
2023-11-03 09:47:21 +0100

    Merge pull request #74 from MTES-MCT/feature/documentation-faq
    documentation-faq

--- documentation/views.py
+++ documentation/views.py
@@ -12,14 +12,3 @@ class FAQView(BreadCrumbMixin, TemplateView):
             {"title": "FAQ"},
         )
         return breadcrumbs
-
-
-class TutorielView(BreadCrumbMixin, TemplateView):
-    template_name = "documentation/tutoriel.html"
-
-    def get_context_breadcrumbs(self):
-        breadcrumbs = super().get_context_breadcrumbs()
-        breadcrumbs.append(
-            {"title": "Tutoriel"},
-        )
-        return breadcrumbs
