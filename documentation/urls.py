7337f5557ffff56b7c938852e9dbcf19ba4b7dff
Sofian Thibaut <sofian.ab@hotmail.fr>
2023-11-03 10:56:49 +0100

    Merge pull request #75 from MTES-MCT/feature/documentation-tutorial
    documentation-tutorial

--- documentation/urls.py
+++ documentation/urls.py
@@ -8,6 +8,7 @@ app_name = "documentation"
 
 urlpatterns = [
     path("faq", views.FAQView.as_view(), name="faq"),
+    path("tutoriel", views.TutorielView.as_view(), name="tutoriel"),
 ]
 
 router = routers.DefaultRouter()
