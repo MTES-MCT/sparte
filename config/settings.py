ce620dc1151599304705e7c99ea328ba1e9f4f9f
Alexis Athlani <alexis.athlani@gmail.com>
2023-11-29 12:15:10 +0100

    fix(cache): remove version option as we already flush it before release

--- config/settings.py
+++ config/settings.py
@@ -268,7 +268,6 @@ else:
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
                 "MAX_ENTRIES": 1000,
-                "VERSION": 1,
             },
         }
     }
