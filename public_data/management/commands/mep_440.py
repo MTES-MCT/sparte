a9eee49f87b80a9892a36c74e61a7a9b347fd75c
Alexis Athlani <alexis.athlani@gmail.com>
2023-12-11 14:32:19 +0100

    fix(4.4): change deploy command

--- public_data/management/commands/mep_440.py
+++ public_data/management/commands/mep_440.py
@@ -11,8 +11,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info("Start mep_ocsge_440")
+
         call_command("remove_ocsge_data_gironde")
         call_command("set_gers_departement_to_ocsge_objects")
+
         ocsge_items = [
             # Essonne ####
             "EssonneOcsge2018",
@@ -39,6 +41,9 @@ class Command(BaseCommand):
 
         call_command("import_gpu", dept="91")
         call_command("import_gpu", dept="77")
+
+        call_command("build_project_ocsge_status")
+        call_command("reset_first_last")
 
         call_command("load_insee")
 
