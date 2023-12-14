91786b3ea4a6a3e5291f990b48536e49504440b2
Alexis A <alexis.athlani@beta.gouv.fr>
2023-12-14 17:21:54 +0100

    Multiple fix 4.4 (#102)
    * fix(*): multiple fix 4.4
    
    * feat(build_commune_data): simplify availability and first and last millesimes methods
    
    * fix(build_artif_area): remplace create by bulk_create
    
    * feat(build_commune_data): move some fields to setup_dept

--- public_data/management/commands/setup_dept.py
+++ public_data/management/commands/setup_dept.py
@@ -2,14 +2,14 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from public_data.models import Departement
+from public_data.models import Commune, Departement
 
 logger = logging.getLogger("management.commands")
 
 config = {
-    "Gers": ["2016", "2019"],
-    "Essonne": ["2018", "2021"],
-    "Seine-et-Marne": ["2017", "2021"],
+    "Gers": [2016, 2019],
+    "Essonne": [2018, 2021],
+    "Seine-et-Marne": [2017, 2021],
 }
 
 
@@ -22,16 +22,29 @@ class Command(BaseCommand):
             is_artif_ready=False,
             ocsge_millesimes=None,
         )
+        Commune.objects.all().update(
+            first_millesime=None,
+            last_millesime=None,
+            ocsge_available=False,
+        )
         logger.info("Done reset, start config")
+
         for name, millesimes in config.items():
             dept = Departement.objects.get(name=name)
             dept.is_artif_ready = True
             dept.ocsge_millesimes = millesimes
             dept.save()
+
+            Commune.objects.filter(departement=dept).update(
+                first_millesime=min(millesimes),
+                last_millesime=max(millesimes),
+                ocsge_available=True,
+            )
+
             logger.info(f"Done {name}: {millesimes}")
+
         qte = Departement.objects.filter(is_artif_ready=True).count()
         if qte == len(config):
             logger.info("%d departement is artif ready", qte)
         else:
             logger.error("%d departement with artif ready instead of %d", qte, len(config))
-        logger.info("End setup departement OCSGE")
