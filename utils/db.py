ac96215915775d3f9d91358d2995295384cc87bb
Alexis Athlani <alexis.athlani@gmail.com>
2023-12-19 10:12:11 +0100

    feat(autoloadmixin): add make_mpoly_valid to allow hauts de seine to load

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,12 @@
-from django.contrib.gis.db.models import Func
 from django.contrib.gis.db.models.functions import Area, Intersection, Transform
-from django.contrib.gis.geos import MultiPolygon, Polygon
+from django.contrib.gis.geos import (
+    GeometryCollection,
+    LineString,
+    MultiPoint,
+    MultiPolygon,
+    Point,
+    Polygon,
+)
 from django.db.models import DecimalField, Manager, QuerySet, Sum
 from django.db.models.functions import Cast, Coalesce
 
@@ -40,27 +46,66 @@ class IntersectManager(IntersectMixin, Manager):
     pass
 
 
-def fix_poly(field):
+def fix_poly(field) -> MultiPolygon:
     if isinstance(field, Polygon):
         return MultiPolygon(field)
     elif isinstance(field, MultiPolygon):
         return field
     else:
-        raise TypeError("Field should be Polygon or MultiPolygon")
+        raise TypeError(f"Field should be Polygon or MultiPolygon. Found: {field.geom_type}")
 
 
-class Buffer(Func):
-    """Make a buffer arround a mpoly.
-
-    Add an outputfield to ensure the result is a MultiPolygon.
-    >>> Buffer("intersection", buffer_width, output_field=MultiPolygonField())
-
-    To get meters when srid=4326, use:
-    >>> buffer_width = 5 / 40000000.0 * 360.0  # 5 meters
+def __buffer_geometry_and_make_it_valid(
+    geom: LineString | Point | MultiPoint,
+) -> Polygon | MultiPolygon | GeometryCollection:
     """
+    NOTE: please delete me with the make_multipolygon_valid function below
+    as soon as possible.
+    """
+    buffered = geom.buffer(0.0000000001).make_valid()
 
-    function = "ST_Buffer"
-    arity = 2
+    if (
+        not isinstance(buffered, Polygon)
+        and not isinstance(buffered, MultiPolygon)
+        and not isinstance(buffered, GeometryCollection)
+    ):
+        raise Exception(f"Unexpected geometry type while make geometry valid: {geom.geom_type}")
 
-    def __init__(self, expression, radius, **extra):
-        super().__init__(expression, radius, **extra)
+    return buffered
+
+
+def make_multipolygon_valid(
+    geom: MultiPolygon | Polygon | LineString | MultiPoint | Point | GeometryCollection,
+) -> MultiPolygon:
+    """
+    This command will recursively make a geom into a valid multipolygon
+    from a geometry of various type.
+
+    Multiparts geometries (GeometryCollection, Multipoint etc ...) will be
+    converted to polygons, adn assembled into a multipolygon.
+
+    Already valid multipolygon will be returned as is.
+
+    NOTE: The function will not erase all topological errors, the output
+    should always checked manually before being used. Delete this function
+    when the data is fixed upstream and loadable. I repeat, this function
+    is a hack, it is not a future proof fix.
+    """
+    ouput_geom = None
+    geom = geom.make_valid()
+
+    if isinstance(geom, MultiPolygon):
+        ouput_geom = geom
+    elif isinstance(geom, Polygon):
+        ouput_geom = MultiPolygon(geom)
+    elif isinstance(geom, LineString) or isinstance(geom, MultiPoint) or isinstance(geom, Point):
+        ouput_geom = make_multipolygon_valid(__buffer_geometry_and_make_it_valid(geom))
+    elif isinstance(geom, GeometryCollection):
+        ouput_geom = MultiPolygon()
+
+        for geom_part in geom:
+            ouput_geom.extend(make_multipolygon_valid(geom_part))
+    else:
+        raise Exception(f"Unexpected input geometry type while making a valid multipolygon : {geom.geom_type}")
+
+    return ouput_geom
