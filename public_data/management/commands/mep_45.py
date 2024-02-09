2cdf665f839b813a8a1143d888e6766a69ff6584
Swann <swann.bouviermuller@gmail.com>
2024-02-09 17:45:40 +0100

    revert to 4.5 mep command

--- public_data/management/commands/mep_45.py
+++ public_data/management/commands/mep_45.py
@@ -1,11 +1,9 @@
-# 4.4.1 migration
-# TODO: remove this file when 4.4.1 is deployed on staging and prod
-
 import logging
 
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+from project.models import Project
 from public_data.models import Departement
 
 logger = logging.getLogger("management.commands")
@@ -16,7 +14,6 @@ class Command(BaseCommand):
 
     def load_departement(self, departement: Departement):
         call_command("load_ocsge", departement=departement.name)
-        call_command("setup_departements")
         call_command("build_commune_data", departement=departement.name, verbose=True)
         call_command("build_artificial_area", departement=departement.name, verbose=True)
         call_command("import_gpu", departement=departement.name)
@@ -26,22 +23,29 @@ class Command(BaseCommand):
 
         call_command("maintenance", on=True)
 
+        logger.info("Initialize data sources")
         call_command("loaddata", "public_data/models/data_source_fixture.json")
 
-        call_command("load_cerema", departement="971")  # Guadeloupe
-        call_command("load_cerema", departement="972")  # Martinique
-        call_command("load_cerema", departement="973")  # Guyane française
-        call_command("load_cerema", departement="974")  # La Réunion
-
-        call_command("build_administrative_layers")
-        call_command("load_scot")
+        logger.info("Load DROM-COM")
+        # Guadeloupe, Martinique, Guyane française, La Réunion
+        dept_codes = ["971", "972", "973", "974"]
+        call_command("load_cerema", official_land_ids=dept_codes)
+        call_command("build_administrative_layers", departements=dept_codes)
         call_command("load_insee")
 
-        self.load_departement(departement=Departement.objects.get(name="Hauts-de-Seine"))
-        self.load_departement(departement=Departement.objects.get(name="Landes"))
+        logger.info("Load new OCS GE")
+        call_command("setup_departements")
 
-        call_command("reset_first_last")
-        call_command("build_project_ocsge_status")
+        haut_de_seine = Departement.objects.get(source_id="92")
+        self.load_departement(departement=haut_de_seine)
+
+        landes = Departement.objects.get(source_id="40")
+        self.load_departement(departement=landes)
+
+        # fix very old projets
+        Project.objects.filter(async_add_city_done=False).update(async_add_city_done=True)
+
+        call_command("update_project_ocsge", departements=["92", "40"])
 
         call_command("maintenance", off=True)
         logger.info("End mep_45")
