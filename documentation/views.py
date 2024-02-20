1be6fbd52e8e864fd05da3d6ed141e1478418f7c
Alexis A. <alexis.athlani@gmail.com>
2024-02-20 12:58:16 +0100

    feat(subdomain): change to mondiagartif.beta.gouv.fr

--- documentation/views.py
+++ documentation/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django_app_parameter import app_parameter
 
 from utils.views_mixins import BreadCrumbMixin
 
@@ -12,6 +13,10 @@ class FAQView(BreadCrumbMixin, TemplateView):
             {"title": "FAQ"},
         )
         return breadcrumbs
+
+    def get_context_data(self, **kwargs):
+        kwargs["team_email"] = app_parameter.TEAM_EMAIL
+        return super().get_context_data(**kwargs)
 
 
 class TutorielView(BreadCrumbMixin, TemplateView):
