520c105091678715f1183a56488e92441c90fbc9
Alexis A. <alexis.athlani@gmail.com>
2024-03-01 15:52:23 +0100

    feat(ocsge): add 35 and 83 o list of data sources

--- public_data/management/commands/mep_46.py
+++ public_data/management/commands/mep_46.py
@@ -10,42 +10,33 @@ logger = logging.getLogger("management.commands")
 
 
 class Command(BaseCommand):
-    help = "Dedicated to load data for 4.5 deployment"
+    help = "Dedicated to load data for 4.6 deployment"
 
     def load_departement(self, departement: Departement):
         call_command("load_ocsge", departement=departement.name)
         call_command("build_commune_data", departement=departement.name, verbose=True)
         call_command("build_artificial_area", departement=departement.name, verbose=True)
         call_command("import_gpu", departement=departement.name)
+        call_command("update_project_ocsge", departements=[departement.source_id])
 
     def handle(self, *args, **options):
-        logger.info("Start mep_45")
+        logger.info("Start mep_46")
+
+        Project.objects.filter(async_add_city_done=False).update(async_add_city_done=True)
 
         call_command("maintenance", on=True)
 
         logger.info("Initialize data sources")
         call_command("loaddata", "public_data/models/data_source_fixture.json")
 
-        logger.info("Load DROM-COM")
-        # Guadeloupe, Martinique, Guyane française, La Réunion
-        dept_codes = ["971", "972", "973", "974"]
-        call_command("load_cerema", official_land_ids=dept_codes)
-        call_command("build_administrative_layers", departements=dept_codes)
-        call_command("load_insee")
-
         logger.info("Load new OCS GE")
         call_command("setup_departements")
 
-        haut_de_seine = Departement.objects.get(source_id="92")
-        self.load_departement(departement=haut_de_seine)
+        departements_source_ids = ["35", "83"]
 
-        landes = Departement.objects.get(source_id="40")
-        self.load_departement(departement=landes)
-
-        # fix very old projets
-        Project.objects.filter(async_add_city_done=False).update(async_add_city_done=True)
-
-        call_command("update_project_ocsge", departements=["92", "40"])
+        for source_id in departements_source_ids:
+            departement = Departement.objects.get(source_id=source_id)
+            self.load_departement(departement)
 
         call_command("maintenance", off=True)
-        logger.info("End mep_45")
+        logger.info("End mep_46")
