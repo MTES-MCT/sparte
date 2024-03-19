3a9305c48e5a3383174951dc7539bcaf70385a01
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-03-19 16:56:40 +0100

    Install react dom test 1

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,12 @@ const common = {
                     'css-loader'
                 ],
             },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
+            },
         ]
     },
     plugins: [
