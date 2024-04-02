092e3b7c582c3292eb9644d9cc23ef23d62af71d
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-04-02 16:40:15 +0200

    Link Crisp FAQ

--- documentation/views.py
+++ documentation/views.py
@@ -1,22 +1,6 @@
 from django.views.generic import TemplateView
-from django_app_parameter import app_parameter
 
 from utils.views_mixins import BreadCrumbMixin
-
-
-class FAQView(BreadCrumbMixin, TemplateView):
-    template_name = "documentation/faq.html"
-
-    def get_context_breadcrumbs(self):
-        breadcrumbs = super().get_context_breadcrumbs()
-        breadcrumbs.append(
-            {"title": "FAQ"},
-        )
-        return breadcrumbs
-
-    def get_context_data(self, **kwargs):
-        kwargs["team_email"] = app_parameter.TEAM_EMAIL
-        return super().get_context_data(**kwargs)
 
 
 class TutorielView(BreadCrumbMixin, TemplateView):
