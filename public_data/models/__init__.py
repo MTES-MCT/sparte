34fd39c38cd7f4be9ce7e3e7d3d65c900b3dd2aa
Alexis A. <alexis.athlani@gmail.com>
2024-04-17 12:41:34 +0200

    fix(public_data): reorder import in init

--- public_data/models/__init__.py
+++ public_data/models/__init__.py
@@ -4,5 +4,5 @@ from .data_source import *  # noqa: F401, F403
 from .deprecated import *  # noqa: F401, F403
 from .gpu import *  # noqa: F401, F403
 from .mixins import *  # noqa: F401, F403
-from .sudocuh import Sudocuh  # noqa: F401
 from .ocsge import ArtificialArea, Ocsge, OcsgeDiff, ZoneConstruite  # noqa: F401, F403
+from .sudocuh import Sudocuh  # noqa: F401
