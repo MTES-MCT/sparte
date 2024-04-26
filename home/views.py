ba5e50928601658bc8d6b35fae4e13135b4e753e
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-04-26 17:33:42 +0200

    Create rapport local landing page

--- home/views.py
+++ home/views.py
@@ -34,6 +34,14 @@ class HomeView(BreadCrumbMixin, TemplateView):
         return super().get_context_data(**kwargs)
 
 
+class HomeRapportLocalView(BreadCrumbMixin, TemplateView):
+    template_name = "home/home_rapport_local.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["form"] = NewsletterForm()
+        return super().get_context_data(**kwargs)
+
+
 class AccessView(BreadCrumbMixin, TemplateView):
     template_name = "home/accessibilite.html"
 
