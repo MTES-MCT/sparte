bbf75f6e0ada9d46df199db958eae32ead21861a
Alexis A. <alexis.athlani@gmail.com>
2024-04-30 15:20:46 +0200

    feat(AnnualTotalConsoChart): add graph

---
+++ project/charts/AnnualTotalConsoChart.py
@@ -0,0 +1,56 @@
+from project.charts.base_project_chart import ProjectChart
+from project.charts.constants import (
+    CEREMA_CREDITS,
+    DEFAULT_HEADER_FORMAT,
+    DEFAULT_POINT_FORMAT,
+    DEFAULT_VALUE_DECIMALS,
+)
+from public_data.models import AdminRef
+
+
+class AnnualTotalConsoChart(ProjectChart):
+    name = "conso communes"
+
+    param = {
+        "chart": {"type": "column"},
+        "title": {"text": ""},
+        "yAxis": {"title": {"text": "Consommé (ha)"}},
+        "xAxis": {"type": "category"},
+        "tooltip": {
+            "headerFormat": DEFAULT_HEADER_FORMAT,
+            "pointFormat": DEFAULT_POINT_FORMAT,
+            "valueDecimals": DEFAULT_VALUE_DECIMALS,
+        },
+        "legend": {"enabled": False},
+        "series": [],
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.level = kwargs.pop("level", AdminRef.COMMUNE)
+        super().__init__(*args, **kwargs)
+
+    def get_series(self):
+        return {"Consommation": self.project.get_conso_per_year()}
+
+
+class AnnualTotalConsoChartExport(AnnualTotalConsoChart):
+    @property
+    def param(self):
+        return super().param | {
+            "title": {
+                "text": (
+                    f"Consommation d'espace à {self.project.territory_name} "
+                    f"entre {self.project.analyse_start_date} et {self.project.analyse_end_date} (en ha)"
+                )
+            },
+            "credits": CEREMA_CREDITS,
+            "plotOptions": {
+                "column": {
+                    "dataLabels": {
+                        "enabled": True,
+                        "format": "{point.y:,.1f}",
+                        "allowOverlap": True,
+                    },
+                }
+            },
+        }
