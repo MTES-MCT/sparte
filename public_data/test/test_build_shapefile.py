4d2df9e0cb079bd2092f5c13dd7f9dd769f6e0d7
Alexis A. <alexis.athlani@gmail.com>
2024-05-08 11:14:35 +0200

    fix(tests): remove checksums from tests

--- public_data/test/test_build_shapefile.py
+++ public_data/test/test_build_shapefile.py
@@ -1,4 +1,3 @@
-from hashlib import md5
 from pathlib import Path
 
 from django.core.management import call_command
@@ -13,15 +12,15 @@ class TestBuildOcsge(TestCase):
         call_command("loaddata", "public_data/models/data_source_fixture.json")
 
     def test_build_ocsge(self):
-        expected_files = {
-            "OCSGE_DIFFERENCE_94_2018_2021_MDA.shp.zip": "bc88efabde67772612e9b3fd69b5842b",
-            "OCSGE_OCCUPATION_DU_SOL_94_2018_MDA.shp.zip": "73db57d6273508860d9a122feffc1e6d",
-            "OCSGE_OCCUPATION_DU_SOL_94_2021_MDA.shp.zip": "daaf29a4582e3ba2321d0f69c69b3dc5",
-            "OCSGE_ZONE_ARTIFICIELLE_94_2018_MDA.shp.zip": "d3752cf997f6246deb267f6af6056566",
-            "OCSGE_ZONE_ARTIFICIELLE_94_2021_MDA.shp.zip": "bf3a8863604dd3cee6a5a0461589236b",
-            "OCSGE_ZONE_CONSTRUITE_94_2018_MDA.shp.zip": "46e023d2da74b5da5326710fee52098d",
-            "OCSGE_ZONE_CONSTRUITE_94_2021_MDA.shp.zip": "a15ca3c91e6532d379a64f5f1df878e9",
-        }
+        expected_files = [
+            "OCSGE_DIFFERENCE_94_2018_2021_MDA.shp.zip",
+            "OCSGE_OCCUPATION_DU_SOL_94_2018_MDA.shp.zip",
+            "OCSGE_OCCUPATION_DU_SOL_94_2021_MDA.shp.zip",
+            "OCSGE_ZONE_ARTIFICIELLE_94_2018_MDA.shp.zip",
+            "OCSGE_ZONE_ARTIFICIELLE_94_2021_MDA.shp.zip",
+            "OCSGE_ZONE_CONSTRUITE_94_2018_MDA.shp.zip",
+            "OCSGE_ZONE_CONSTRUITE_94_2021_MDA.shp.zip",
+        ]
 
         call_command(
             command_name="build_shapefile",
@@ -31,9 +30,5 @@ class TestBuildOcsge(TestCase):
             land_id="94",
         )
 
-        for file, checksum in expected_files.items():
+        for file in expected_files:
             self.assertTrue(Path(file).exists())
-            self.assertEqual(
-                md5(open(file, "rb").read()).hexdigest(),
-                checksum,
-            )
--- public_data/test/test_repack_ocsge.py
+++ public_data/test/test_repack_ocsge.py
@@ -1,4 +1,3 @@
-from hashlib import md5
 from pathlib import Path
 
 from django.core.management import call_command
@@ -9,13 +8,13 @@ from public_data.models import DataSource
 
 class TestRepackOcsge(TestCase):
     def setUp(self) -> None:
-        self.expected_files = {
-            "94_DIFFERENCE_2018_2021_IGN_REPACKED.shp.zip": "ef0f7f02ca9e7e9ca4a27ab6634a08c7",
-            "94_OCCUPATION_DU_SOL_2018_IGN_REPACKED.shp.zip": "779f9ffdb39489a34839d61a40037720",
-            "94_OCCUPATION_DU_SOL_2021_IGN_REPACKED.shp.zip": "8d161c65aac9ce01efb29251ee8f6924",
-            "94_ZONE_CONSTRUITE_2018_IGN_REPACKED.shp.zip": "b65e61ebfd7c7c68f779ec39b59bea28",
-            "94_ZONE_CONSTRUITE_2021_IGN_REPACKED.shp.zip": "4cf92e7475a65936304d7ac8743781b6",
-        }
+        self.expected_files = [
+            "94_DIFFERENCE_2018_2021_IGN_REPACKED.shp.zip",
+            "94_OCCUPATION_DU_SOL_2018_IGN_REPACKED.shp.zip",
+            "94_OCCUPATION_DU_SOL_2021_IGN_REPACKED.shp.zip",
+            "94_ZONE_CONSTRUITE_2018_IGN_REPACKED.shp.zip",
+            "94_ZONE_CONSTRUITE_2021_IGN_REPACKED.shp.zip",
+        ]
 
     def tearDown(self) -> None:
         for file in self.expected_files:
@@ -69,12 +68,8 @@ class TestRepackOcsge(TestCase):
 
         call_command(command_name="repack_ocsge", urls=urls)
 
-        for file, checksum in self.expected_files.items():
+        for file in self.expected_files:
             self.assertTrue(Path(file).exists())
-            self.assertEqual(
-                md5(open(file, "rb").read()).hexdigest(),
-                checksum,
-            )
 
         for data_source in expected_data_sources:
             self.assertTrue(
