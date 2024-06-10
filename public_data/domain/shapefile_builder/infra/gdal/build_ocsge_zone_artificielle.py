cbfbb44b3c3cd4f767c569e6f1d997d87290dc13
Alexis A. <alexis.athlani@gmail.com>
2024-06-10 18:53:22 +0200

    feat(artif): temp calcul artif departement
    temp

--- public_data/domain/shapefile_builder/infra/gdal/build_ocsge_zone_artificielle.py
+++ public_data/domain/shapefile_builder/infra/gdal/build_ocsge_zone_artificielle.py
@@ -1,10 +1,13 @@
 import subprocess
 from pathlib import Path
 
+from django.conf import settings
+from django.db import connection
+
 from public_data.models import DataSource
 from public_data.shapefile import ShapefileFromSource
 
-from .utils import multiline_string_to_single_line
+from .is_artif_case import is_artif_case
 
 
 def build_ocsge_zone_artificielle(source: DataSource) -> tuple[DataSource, Path]:
@@ -21,45 +24,127 @@ def build_ocsge_zone_artificielle(source: DataSource) -> tuple[DataSource, Path]
         + ".shp.zip"
     )
 
+    fields = {
+        "couverture": "CODE_CS",
+        "usage": "CODE_US",
+    }
+
+    temp_table = "occupation_sol"
+    temp_table_artif = "artif_table"
+
+    db = settings.DATABASES["default"]
+
     with ShapefileFromSource(source=source) as shapefile_path:
+        command_occupation_du_sol = f'ogr2ogr -f "PostgreSQL" -overwrite "PG:dbname={db["NAME"]} host={db["HOST"]} port={db["PORT"]} user={db["USER"]} password={db["PASSWORD"]}" {shapefile_path.absolute()} -nln {temp_table} -a_srs EPSG:{source.srid} -nlt MULTIPOLYGON -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=mpoly -lco PRECISION=NO --config PG_USE_COPY YES'  # noqa: E501
+        subprocess.run(args=command_occupation_du_sol, check=True, shell=True)
+
         sql = f"""
+        DROP TABLE IF EXISTS {temp_table_artif};
+        CREATE TABLE {temp_table_artif} AS
+        WITH ocsge_classified AS ( /* assign is_artificial value to each object */
             SELECT
-                'NO_ID' AS ID,
-                '{source.millesimes[0]}' AS YEAR,
-                '{source.millesimes[0]}' AS MILLESIME,
-                '{source.srid}' AS SRID,
-                '{source.official_land_id}' AS DPT,
-                round(ST_Area(GEOMETRY), 4) AS SURFACE,
-                GEOMETRY AS MPOLY
+            *,
+            {is_artif_case(
+                code_cs=fields['couverture'],
+                code_us=fields['usage'],
+                true_value='TRUE',
+                false_value='FALSE',
+            )} AS is_artificial
             FROM
-                {Path(source.shapefile_name).stem}
+            occupation_sol
+        ),
+        clustered_ocsge AS ( /* group artificial and non_artficial objects by their proximity  */
+            SELECT
+                is_artificial,
+                ST_UnaryUnion(
+                    unnest(
+                        ST_ClusterIntersecting(mpoly)
+                    )
+                ) AS mpoly
+            FROM
+                ocsge_classified
+            GROUP BY
+                is_artificial
+        ),
+        artif_nat_by_surface AS ( /* invert is_articicial value if the surface is inferior to 2500m2 */
+            SELECT
+                CASE
+                    WHEN ST_Area(mpoly) < 2500 THEN NOT is_artificial
+                    ELSE is_artificial
+                END AS is_artificial,
+                mpoly
+            FROM
+                clustered_ocsge
+        ),
+        small_built AS ( /* retrieve small built surfaces that are enclaved in natural surfaces */
+            SELECT
+                is_artificial,
+                mpoly
+            FROM
+                ocsge_classified
+            WHERE
+                code_cs = 'CS1.1.1.1'
+                AND ST_Area(mpoly) < 2500
+                AND EXISTS (
+                    SELECT
+                        mpoly
+                    FROM
+                        artif_nat_by_surface
+                    WHERE
+                        ST_Intersects(mpoly, ocsge_classified.mpoly) AND
+                        is_artificial = FALSE
+                )
+        ), artificial_union AS (
+            SELECT
+                is_artificial,
+                mpoly
+            FROM
+                artif_nat_by_surface
+            WHERE
+                is_artificial = TRUE
+            UNION ALL
+            SELECT
+                is_artificial,
+                mpoly
+            FROM
+                small_built
+        ), artificial_geom_union AS (
+            SELECT
+                ST_Union(mpoly) AS mpoly,
+                is_artificial
+            FROM
+                artificial_union
+            GROUP BY
+                is_artificial
+        )
+        SELECT
+            (ST_Dump(mpoly)).geom AS mpoly
+        FROM
+            artificial_geom_union;
         """
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql=sql)
+            cursor.connection.commit()
+
         command = [
             "ogr2ogr",
-            "-dialect SQLITE",
-            '-f "ESRI Shapefile"',
+            "-f",
+            '"ESRI Shapefile"',
             f'"{build_name}"',
-            str(shapefile_path.absolute()),
+            f'"PG:dbname={db["NAME"]} host={db["HOST"]} port={db["PORT"]} user={db["USER"]} password={db["PASSWORD"]}"',  # noqa: E501
             "-nlt MULTIPOLYGON",
             "-nlt PROMOTE_TO_MULTI",
-            f"-nln {source.name}",
+            "-nln",
+            '"ZONE_ARTIFICIELLE"',
             f"-a_srs EPSG:{source.srid}",
             "-sql",
-            f'"{multiline_string_to_single_line(sql)}"',
+            f'"SELECT * FROM {temp_table_artif}"',
+            "-progress",
         ]
-        subprocess.run(args=" ".join(command), shell=True, check=True)
+        with open("error.log", "w") as f:
+            subprocess.run(args=" ".join(command), shell=True, check=True, stdout=f, stderr=f)
 
-    output_source, _ = DataSource.objects.update_or_create(
-        productor=source.ProductorChoices.MDA,
-        dataset=source.dataset,
-        name=source.name,
-        millesimes=source.millesimes,
-        official_land_id=source.official_land_id,
-        defaults={
-            "mapping": None,
-            "path": build_name,
-            "shapefile_name": source.name + ".shp",
-            "srid": source.srid,
-        },
-    )
-    return output_source, Path(build_name)
+        with connection.cursor() as cursor:
+            cursor.execute(sql=f"DROP TABLE IF EXISTS {temp_table};")
+            cursor.execute(sql=f"DROP TABLE IF EXISTS {temp_table_artif};")
--- public_data/domain/shapefile_builder/infra/gdal/is_artif_case.py
+++ public_data/domain/shapefile_builder/infra/gdal/is_artif_case.py
@@ -1,41 +1,51 @@
-def is_artif_case(code_cs: str, code_us: str) -> str:
+def is_artif_case(
+    code_cs: str,
+    code_us: str,
+    true_value=1,
+    false_value=0,
+) -> str:
+    """
+    true_value and false_value are optional parameters
+    The default values are for working with sqlite
+    """
+
     return f""" CASE
         /* CS 1.1 */
-        WHEN {code_cs} = 'CS1.1.1.1' THEN 1
-        WHEN {code_cs} = 'CS1.1.1.2' THEN 1
-        WHEN {code_cs} = 'CS1.1.1.1' AND {code_us} != 'US1.3' THEN 1
-        WHEN {code_cs} = 'CS1.1.2.1' THEN 1
-        WHEN {code_cs} = 'CS1.1.2.2' THEN 1
+        WHEN {code_cs} = 'CS1.1.1.1' THEN {true_value}
+        WHEN {code_cs} = 'CS1.1.1.2' THEN {true_value}
+        WHEN {code_cs} = 'CS1.1.1.1' AND {code_us} != 'US1.3' THEN {true_value}
+        WHEN {code_cs} = 'CS1.1.2.1' THEN {true_value}
+        WHEN {code_cs} = 'CS1.1.2.2' THEN {true_value}
 
         /* CS 2.2 */
             /* CS 2.2.1 */
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US2' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US3' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US5' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US235' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.1' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.2' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.3' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.4' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.5' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.2' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.3' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US6.1' THEN 1
-            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US6.2' THEN 1
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US5' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US235' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.1' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.4' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.1.5' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US4.3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US6.1' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.1' AND {code_us} = 'US6.2' THEN {true_value}
 
             /* CS 2.2.2 */
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US2' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US3' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US5' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US235' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.1' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.2' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.3' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.4' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.5' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.2' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.3' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US6.1' THEN 1
-            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US6.2' THEN 1
-        ELSE 0
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US5' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US235' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.1' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.4' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.1.5' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.2' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US4.3' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US6.1' THEN {true_value}
+            WHEN {code_cs} = 'CS2.2.2' AND {code_us} = 'US6.2' THEN {true_value}
+        ELSE {false_value}
     END"""
