41c9fab3828dd8945e728cab0d7984f3d21a2554
Alexis A. <alexis.athlani@gmail.com>
2024-06-12 11:57:29 +0200

    feat(artificialArea): make it calculated during diag creation

--- public_data/domain/artificialisation/use_case/CalculateCommuneArtificialAreas.py
+++ public_data/domain/artificialisation/use_case/CalculateCommuneArtificialAreas.py
@@ -1,20 +1,25 @@
+from logging import getLogger
+
 from django.contrib.gis.db.models import Union
 from django.contrib.gis.db.models.functions import Area, Intersection, MakeValid
 from django.contrib.gis.geos import Polygon
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
+from django.db.models.query import QuerySet
 
 from public_data.models.administration import Commune
 from public_data.models.enums import SRID
 from public_data.models.ocsge import ArtificialArea, ZoneArtificielle
 from utils.db import DynamicSRIDTransform, fix_poly
 
+logger = getLogger(__name__)
+
 Zero = Area(Polygon(((0, 0), (0, 0), (0, 0), (0, 0)), srid=SRID.LAMBERT_93))
 
 
-class CalculateCommuneArtificialArea:
+class CalculateCommuneArtificialAreas:
     @staticmethod
-    def execute(commune: Commune) -> list[ArtificialArea]:
+    def execute(commune: Commune) -> QuerySet[ArtificialArea]:
         """
         This method calculates the artificial area objects of a commune by intersecting the commune
         with the ZoneArticielle.
@@ -44,13 +49,26 @@ class CalculateCommuneArtificialArea:
             )
         )
 
-        return [
-            ArtificialArea.objects.create(
-                mpoly=fix_poly(artif_area["mpoly"]),
+        created_artif_areas = []
+        count_newly_created = 0
+
+        for artif_area in qs:
+            create_artif_area, is_new = ArtificialArea.objects.update_or_create(
                 year=artif_area["year"],
                 city=commune.official_id,
-                surface=artif_area["surface"].sq_m / 10000,
-                departement=commune.departement.source_id,
+                defaults={
+                    "mpoly": fix_poly(artif_area["mpoly"]),
+                    "surface": artif_area["surface"].sq_m / 10000,
+                    "departement": commune.departement.source_id,
+                },
             )
-            for artif_area in qs
-        ]
+            created_artif_areas.append(create_artif_area)
+            if is_new:
+                count_newly_created += 1
+
+        logger.info(
+            f"Calculated {len(created_artif_areas)} artificial areas for {commune.official_id}. "
+            f"{count_newly_created} were newly created."
+        )
+
+        return ArtificialArea.objects.filter(id__in=[artif_area.id for artif_area in created_artif_areas])
--- public_data/tasks.py
+++ public_data/tasks.py
@@ -1,20 +1,16 @@
 import logging
-from pydoc import locate
 
 from celery import shared_task
 
-from public_data.models.mixins import AutoLoadMixin
+from public_data.domain.artificialisation.use_case.RetrieveFreshestCommnuneArtificialAreas import (
+    RetrieveFreshestCommuneArtificialAreas,
+)
+from public_data.models import Commune
 
 logging.basicConfig(level=logging.INFO)
 
 
-@shared_task
-def load_data(class_name, verbose=False):
-    my_class: AutoLoadMixin = locate(class_name)
-
-    if not my_class:
-        raise ModuleNotFoundError(class_name)
-
-    logging.info("load data of %s (verbose=%s)", my_class, verbose)
-
-    my_class.load(verbose=verbose)
+@shared_task(bind=True)
+def create_artificial_area_for_city(self, city_id: str):
+    commune = Commune.objects.get(insee=city_id)
+    RetrieveFreshestCommuneArtificialAreas.execute(commune)
