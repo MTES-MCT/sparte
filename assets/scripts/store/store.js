059cc16a95c8c94b9ec7ab52ae8c0ade50009146
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-06-19 16:35:28 +0200

    Remove unused api service

--- assets/scripts/store/store.js
+++ assets/scripts/store/store.js
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { djangoApi, publicApi } from '../services/api.js'
+import { djangoApi } from '../services/api.js'
 
 const store = configureStore({
   reducer: {
     [djangoApi.reducerPath]: djangoApi.reducer,
-    [publicApi.reducerPath]: publicApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .concat(djangoApi.middleware)
-    .concat(publicApi.middleware),
+    .concat(djangoApi.middleware),
 })
 
 setupListeners(store.dispatch)
