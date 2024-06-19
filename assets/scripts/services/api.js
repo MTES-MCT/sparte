766c1e010f96ef61b1a1f92c8142d5cdb708b75b
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-06-19 16:35:28 +0200

    Remove unused api service

--- assets/scripts/services/api.js
+++ assets/scripts/services/api.js
@@ -2,28 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const djangoApi = createApi({
   reducerPath: 'djangoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/v2/', credentials: 'include' }),
-  endpoints: (builder) => ({
-    getProject: builder.query({
-      query: (id) => `projects/${id}/`,
-    }),
-  }),
-})
-
-export const publicApi = createApi({
-  reducerPath: 'publicApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/public/' }),
+  baseQuery: fetchBaseQuery({ credentials: 'include' }),
   endpoints: (builder) => ({
     getDepartementList: builder.query({
-      query: () => 'departements/',
+      query: () => '/public/departements/',
     }),
   }),
 })
-
-export const {
-  useGetProjectQuery,
-} = djangoApi
 
 export const {
   useGetDepartementListQuery,
-} = publicApi
+} = djangoApi
