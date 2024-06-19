bae07d029dc28923aa07850625b160d2bb2a214d
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-06-19 16:59:01 +0200

    Fix urls config

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -40,7 +41,6 @@ urlpatterns = [
     path("fancy-cache", include("fancy_cache.urls")),
     path("documentation/", include("documentation.urls")),
     path("crisp/", include("crisp.urls")),
-    path("api/v2/", include("project.urlsv2")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
