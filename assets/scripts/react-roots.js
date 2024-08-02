ae5b4d530b7eccbfc784021629eb1ec8b2cbf868
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-08-02 15:58:56 +0200

    Dashboard layout

--- assets/scripts/react-roots.js
+++ assets/scripts/react-roots.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import HighchartsMapOcsge from './components/HighchartsMapOcsge.tsx'
+import DashboardSidebar from './components/DashboardSidebar.tsx'
 import store from './store/store.js'
 
 const highchartsMapOcsge = document.getElementById('react-highcharts-ocsge')
@@ -10,6 +11,18 @@ if (highchartsMapOcsge)
   createRoot(highchartsMapOcsge).render(
     <Provider store={store}>
         <HighchartsMapOcsge />
+    </Provider>,
+  )
+}
+
+const dashboardSidebar = document.getElementById('react-dashboard-sidebar')
+if (dashboardSidebar)
+{
+  const { menuItems, currentUrl } = JSON.parse(document.getElementById('sidebar-data').textContent)
+
+  createRoot(dashboardSidebar).render(
+    <Provider store={store}>
+        <DashboardSidebar menuItems={menuItems} currentUrl={currentUrl} />
     </Provider>,
   )
 }
