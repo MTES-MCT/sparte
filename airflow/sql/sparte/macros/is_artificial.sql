65d03d619fdf93516351c48b8a0330caf666d22f
Alexis A. <alexis.athlani@gmail.com>
2024-08-06 11:40:56 +0200

    fix(is_artificial): change style of comments

--- airflow/sql/sparte/macros/is_artificial.sql
+++ airflow/sql/sparte/macros/is_artificial.sql
@@ -1,14 +1,14 @@
 
 {% macro is_artificial(code_cs, code_us) %}
     (CASE
-        /* CS 1.1 */
+        -- CS 1.1
         WHEN {{ code_cs }} = 'CS1.1.1.1' THEN true
         WHEN {{ code_cs }} = 'CS1.1.1.2' THEN true
         WHEN {{ code_cs }} = 'CS1.1.2.1' AND {{ code_us }} != 'US1.3' THEN true
         WHEN {{ code_cs }} = 'CS1.1.2.2' THEN true
 
-        /* CS 2.2 */
-            /* CS 2.2.1 */
+        -- CS 2.2
+            -- CS 2.2.1
             WHEN {{ code_cs }} = 'CS2.2.1' AND {{ code_us }} = 'US2' THEN true
             WHEN {{ code_cs }} = 'CS2.2.1' AND {{ code_us }} = 'US3' THEN true
             WHEN {{ code_cs }} = 'CS2.2.1' AND {{ code_us }} = 'US5' THEN true
@@ -23,7 +23,7 @@
             WHEN {{ code_cs }} = 'CS2.2.1' AND {{ code_us }} = 'US6.1' THEN true
             WHEN {{ code_cs }} = 'CS2.2.1' AND {{ code_us }} = 'US6.2' THEN true
 
-            /* CS 2.2.2 */
+            -- CS 2.2.2
             WHEN {{ code_cs }} = 'CS2.2.2' AND {{ code_us }} = 'US2' THEN true
             WHEN {{ code_cs }} = 'CS2.2.2' AND {{ code_us }} = 'US3' THEN true
             WHEN {{ code_cs }} = 'CS2.2.2' AND {{ code_us }} = 'US5' THEN true
