d94526838d80b81102fa9c19ae6c5726e046bf0d
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-08-12 14:14:38 +0200

    Replace htmx with react router

--- assets/scripts/react-roots.js
+++ assets/scripts/react-roots.js
@@ -1,28 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import HighchartsMapOcsge from './components/HighchartsMapOcsge.tsx'
-import DashboardSidebar from './components/DashboardSidebar.tsx'
+import Dashboard from './components/Dashboard.tsx'
 import store from './store/store.js'
 
-const highchartsMapOcsge = document.getElementById('react-highcharts-ocsge')
-if (highchartsMapOcsge)
+const dashboard = document.getElementById('react-root')
+if (dashboard)
 {
-  createRoot(highchartsMapOcsge).render(
+  createRoot(dashboard).render(
     <Provider store={store}>
-        <HighchartsMapOcsge />
-    </Provider>,
-  )
-}
-
-const dashboardSidebar = document.getElementById('react-dashboard-sidebar')
-if (dashboardSidebar)
-{
-  const { menuItems, currentUrl } = JSON.parse(document.getElementById('sidebar-data').textContent)
-
-  createRoot(dashboardSidebar).render(
-    <Provider store={store}>
-        <DashboardSidebar menuItems={menuItems} currentUrl={currentUrl} />
+        <Dashboard />
     </Provider>,
   )
 }
