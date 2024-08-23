275bbb45e0747cc956a7c063873dd034c2b1f1cf
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-08-23 09:56:04 +0200

    Make charts working and improvements

--- assets/scripts/components/layout/Dashboard.tsx
+++ assets/scripts/components/layout/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useGetProjectQuery } from '../../services/api';
 import { setProjectData } from '../../store/projectSlice';
+import styled from 'styled-components';
 import Navbar from '@components/ui/Navbar';
 import Overview from '@components/widgets/Overview';
 import Synthese from '@components/pages/Synthese';
@@ -13,10 +14,29 @@ import Gpu from '@components/pages/Gpu';
 import Ocsge from '@components/pages/Ocsge';
 import Trajectoires from '@components/pages/Trajectoires';
 import RapportLocal from '@components/pages/RapportLocal';
+import Loader from '@components/ui/Loader';
 
 interface DashboardProps {
-    projectId: String;
+    projectId: string;
 }
+
+const DashboardWrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    background: #f4f7fe;
+`;
+
+const ContentWrapper = styled.main`
+    margin-left: 280px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+`;
+
+const ContentContainer = styled.section`
+    overflow-y: auto;
+    height: 100%;
+`;
 
 const Dashboard: React.FC<DashboardProps> = ({ projectId }) => {
     const dispatch = useDispatch();
@@ -28,53 +48,53 @@ const Dashboard: React.FC<DashboardProps> = ({ projectId }) => {
       }
     }, [data, dispatch]);
   
-    if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <Loader />;
     if (error) return <div>Error loading project data.</div>;
     
     return (
         <Router>
-            <div className="dashboard-wrapper">
-                <div className="dashboard-sidebar">
-                    <Navbar />
-                </div>
-                <div className="dashboard-content">
+            <DashboardWrapper>
+                <Navbar />
+                <ContentWrapper>
                     <Overview />
-                    <Routes>
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/synthesis"
-                            element={<Synthese />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/consommation"
-                            element={<Consommation />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/trajectoires"
-                            element={<Trajectoires />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/découvrir-l-ocsge"
-                            element={<Ocsge />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/artificialisation"
-                            element={<Artificialisation />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/impermeabilisation"
-                            element={<Impermeabilisation />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/zonages-d-urbanisme"
-                            element={<Gpu />}
-                        />
-                        <Route
-                            path="/project/:projectId/tableau-de-bord/rapport-local"
-                            element={<RapportLocal />}
-                        />
-                    </Routes>
-                </div>
-            </div>
+                    <ContentContainer>
+                        <Routes>
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/synthesis"
+                                element={<Synthese />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/consommation"
+                                element={<Consommation />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/trajectoires"
+                                element={<Trajectoires />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/découvrir-l-ocsge"
+                                element={<Ocsge />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/artificialisation"
+                                element={<Artificialisation />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/impermeabilisation"
+                                element={<Impermeabilisation />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/zonages-d-urbanisme"
+                                element={<Gpu />}
+                            />
+                            <Route
+                                path="/project/:projectId/tableau-de-bord/rapport-local"
+                                element={<RapportLocal />}
+                            />
+                        </Routes>
+                    </ContentContainer>
+                </ContentWrapper>
+            </DashboardWrapper>
         </Router>
     );
 };
--- assets/scripts/components/pages/Trajectoires.tsx
+++ assets/scripts/components/pages/Trajectoires.tsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useHtmlLoader } from '@hooks/useHtmlLoader';
+import useHtmx from '@hooks/useHtmx';
+import useHighcharts from '@hooks/useHighcharts';
+import Loader from '@components/ui/Loader';
 
-const Synthese: React.FC = () => {
+const Trajectoires: React.FC = () => {
     const { projectId } = useParams<{ projectId: string }>();
     const endpoint = `/project/${projectId}/tableau-de-bord/trajectoires`;
-    const { content, loading, error } = useHtmlLoader(endpoint);
 
-    if (loading) return <div>Chargement...</div>;
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    const { content, loading, error } = useHtmlLoader(endpoint + `?refreshKey=${refreshKey}`);
+    const htmxRef = useHtmx([loading]);
+
+    useHighcharts([
+        'target_2031_chart'
+    ], loading);
+
+    useEffect(() => {
+        const handleLoadGraphic = () => {
+            setTimeout(() => {
+                // Ferme la modal Bootstrap
+                const modalElement = document.getElementById("setTarget");
+                if (modalElement) {
+                    const modalInstance = window.bootstrap.Modal.getInstance(modalElement);
+                    if (modalInstance) {
+                        modalInstance.hide();
+                    }
+                }
+                // Rafraîchit la clé après avoir fermé la modal pour recharger le contenu
+                setRefreshKey(prevKey => prevKey + 1);
+            }, 1800);
+        };
+
+        document.addEventListener('load-graphic', handleLoadGraphic);
+
+        return () => {
+            document.removeEventListener('load-graphic', handleLoadGraphic);
+        };
+    }, []);
+
+    if (loading) return <Loader />;
     if (error) return <div>Erreur : {error}</div>;
 
     return (
-        <div>
-            {/* Autres composants React */}
-            <div dangerouslySetInnerHTML={{ __html: content }} />
+        <div className="fr-container--fluid fr-p-3w" ref={htmxRef}>
+            <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                    <div dangerouslySetInnerHTML={{ __html: content }} />
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Synthese;
+export default Trajectoires;
