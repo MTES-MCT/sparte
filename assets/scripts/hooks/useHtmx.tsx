f93c8a910515ca9fe69a805db141d0d7bd248ab3
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-08-30 18:51:32 +0200

    Improve react htmx hook

--- assets/scripts/hooks/useHtmx.tsx
+++ assets/scripts/hooks/useHtmx.tsx
@@ -4,10 +4,34 @@ import htmx from 'htmx.org';
 const useHtmx = (dependencies: React.DependencyList = []) => {
     const ref = useRef<HTMLDivElement | null>(null); 
 
-    useEffect(() => {        
+    useEffect(() => {
+        // Configurer HTMX et définir l'extension disable-element
+        htmx.config.includeIndicatorStyles = false;
+
+        htmx.defineExtension('disable-element', {
+            onEvent(name, evt) {
+                if (name === 'htmx:beforeRequest' || name === 'htmx:afterRequest') {
+                    const { elt } = evt.detail;
+                    const target = elt.getAttribute('hx-disable-element');
+                    const targetElement = (target === 'self') ? elt : document.querySelector(target);
+
+                    if (name === 'htmx:beforeRequest' && targetElement) {
+                        targetElement.disabled = true;
+                    } else if (name === 'htmx:afterRequest' && targetElement) {
+                        targetElement.disabled = false;
+                    }
+                }
+            },
+        });
+
         if (ref.current) {            
             htmx.process(ref.current);
         }
+
+        // Nettoyer l'extension et la configuration à la désinstallation du composant
+        return () => {
+            delete window.htmx; // Supprimer HTMX si nécessaire
+        };
     }, dependencies);
 
     return ref;
