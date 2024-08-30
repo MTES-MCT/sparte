5f691bef722bc72076ee2b077c242b8abd19e580
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-08-30 18:52:01 +0200

    Date format utils

--- assets/scripts/utils/formatUtils.js
+++ assets/scripts/utils/formatUtils.js
@@ -4,4 +4,25 @@ const formatNumber = (number, decimals = 0, useGrouping = true) => number.toLoca
   useGrouping,
 })
 
-export default formatNumber
+const formatDateTime = (date, options = {}) =>
+{
+  if (!(date instanceof Date) || Number.isNaN(date.getTime()) || !Number.isFinite(date.getTime()))
+  {
+    return 'Date invalide'
+  }
+
+  const defaultOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: undefined,
+  }
+
+  const finalOptions = { ...defaultOptions, ...options }
+
+  return new Intl.DateTimeFormat('fr-FR', finalOptions).format(date)
+}
+
+export { formatNumber, formatDateTime }
