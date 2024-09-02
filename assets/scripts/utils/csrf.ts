38d2bb80543f69dca13c46227ab3739ce328cd04
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-02 22:24:18 +0200

    React search bar component

---
+++ assets/scripts/utils/csrf.ts
@@ -0,0 +1,13 @@
+const getCsrfToken = (): string | null => {
+  const name = 'csrftoken';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return decodeURIComponent(cookie.substring(name.length + 1));
+    }
+  }
+  return null;
+};
+
+export default getCsrfToken;
