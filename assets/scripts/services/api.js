d14023eea1c41c45cece415c4333a8126515eef6
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-02 22:24:18 +0200

    React search bar component

--- assets/scripts/services/api.js
+++ assets/scripts/services/api.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import getCsrfToken from '../utils/csrf.ts'
 
 export const djangoApi = createApi({
   reducerPath: 'djangoApi',
@@ -10,10 +11,23 @@ export const djangoApi = createApi({
     getProject: builder.query({
       query: (id) => `/project/${id}/detail`,
     }),
+    searchTerritory: builder.query({
+      query: (needle) =>
+      {
+        const csrfToken = getCsrfToken()
+        return {
+          url: '/public/search-land',
+          method: 'POST',
+          body: { needle },
+          headers: csrfToken ? { 'X-CSRFToken': csrfToken } : {},
+        }
+      },
+    }),
   }),
 })
 
 export const {
   useGetDepartementListQuery,
   useGetProjectQuery,
+  useSearchTerritoryQuery,
 } = djangoApi
