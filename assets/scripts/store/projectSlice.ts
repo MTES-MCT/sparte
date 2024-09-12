b22b349edf711e7f163e734e1aa8911d2338906d
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-12 12:33:27 +0200

    Cleaning

--- assets/scripts/store/projectSlice.ts
+++ assets/scripts/store/projectSlice.ts
@@ -1,25 +1,21 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface ProjectState {
-  projectData: any | null;
-}
-
-const initialState: ProjectState = {
-  projectData: null,
-};
+import { createSlice } from '@reduxjs/toolkit'
 
 const projectSlice = createSlice({
   name: 'project',
-  initialState,
+  initialState: {
+    projectData: null,
+  },
   reducers: {
-    setProjectData: (state, action: PayloadAction<any>) => {
-      state.projectData = action.payload;
+    setProjectData: (state, action) =>
+    {
+      state.projectData = action.payload
     },
-    clearProjectData: (state) => {
-      state.projectData = null;
+    clearProjectData: (state) =>
+    {
+      state.projectData = null
     },
   },
-});
+})
 
-export const { setProjectData, clearProjectData } = projectSlice.actions;
-export default projectSlice.reducer;
+export const { setProjectData, clearProjectData } = projectSlice.actions
+export default projectSlice.reducer
