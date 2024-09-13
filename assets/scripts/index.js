1f9281033eaec45cc944fbc4c3526a5665a1fc75
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-13 15:02:01 +0200

    Remove unused scripts

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,30 +7,5 @@ import '@gouvfr/dsfr/dist/dsfr.module.min.js'
 // Import map
 import './map_libre/index.js'
 
+// React
 import './react-roots.tsx'
-
-// Import HTMX and inject it into the window scope
-window.htmx = require('htmx.org')
-// Fix CSP inline style
-window.htmx.config.includeIndicatorStyles = false
-// Disable Submit Button
-window.htmx.defineExtension('disable-element', {
-  onEvent(name, evt)
-  {
-    if (name === 'htmx:beforeRequest' || name === 'htmx:afterRequest')
-    {
-      const { elt } = evt.detail
-      const target = elt.getAttribute('hx-disable-element')
-      const targetElement = (target === 'self') ? elt : document.querySelector(target)
-
-      if (name === 'htmx:beforeRequest' && targetElement)
-      {
-        targetElement.disabled = true
-      }
-      else if (name === 'htmx:afterRequest' && targetElement)
-      {
-        targetElement.disabled = false
-      }
-    }
-  },
-})
