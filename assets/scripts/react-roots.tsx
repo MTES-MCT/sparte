6646c0327ce31c140670f9b467da2b683faf94f5
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-16 14:13:26 +0200

    Fix display home ocsge map

--- assets/scripts/react-roots.tsx
+++ assets/scripts/react-roots.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store/store';
-import ErrorBoundary from './components/ui/ErrorBoundary';
-import Dashboard from './components/layout/Dashboard';
+import store from '@store/store';
+import ErrorBoundary from '@components/ui/ErrorBoundary';
+import HighchartsMapOcsge from '@components/charts/HighchartsMapOcsge'
+import Dashboard from '@components/layout/Dashboard';
+
+const highchartsMapOcsge = document.getElementById('react-highcharts-ocsge')
+if (highchartsMapOcsge)
+{
+  createRoot(highchartsMapOcsge).render(
+    <Provider store={store}>
+        <HighchartsMapOcsge />
+    </Provider>,
+  )
+}
 
 const dashboard = document.getElementById('react-root');
-
 if (dashboard) {
   const projectId = dashboard.dataset.projectId;
   
