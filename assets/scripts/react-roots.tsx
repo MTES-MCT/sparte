52cb8b320ed8d5ba3dba507952376def53d6fb34
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-09-19 17:04:16 +0200

    Replace old search with react search

--- assets/scripts/react-roots.tsx
+++ assets/scripts/react-roots.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from '@store/store';
 import ErrorBoundary from '@components/ui/ErrorBoundary';
-import HighchartsMapOcsge from '@components/charts/HighchartsMapOcsge'
 import Dashboard from '@components/layout/Dashboard';
+import HighchartsMapOcsge from '@components/charts/HighchartsMapOcsge'
+import SearchBar from '@components/widgets/SearchBar'
+
+const searchBar = document.getElementById('react-search-bar')
+if (searchBar)
+{
+  createRoot(searchBar).render(
+    <Provider store={store}>
+      <SearchBar createUrl=""/>
+    </Provider>,
+  )
+}
+
 
 const highchartsMapOcsge = document.getElementById('react-highcharts-ocsge')
 if (highchartsMapOcsge)
