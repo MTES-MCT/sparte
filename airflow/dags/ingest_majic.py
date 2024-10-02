f5d29b8fa41d33528022429a500a5b550c216dcb
Alexis A. <alexis.athlani@gmail.com>
2024-10-02 20:25:32 +0200

    feat(ingest_majic): code cleanup

--- airflow/dags/ingest_majic.py
+++ airflow/dags/ingest_majic.py
@@ -1,5 +1,3 @@
-import json
-import os
 import shutil
 import subprocess
 from typing import List
@@ -8,17 +6,19 @@ from zipfile import ZipFile
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from include.container import Container
+from include.majic.sources import sources
 from include.pools import DBT_POOL
+from include.utils import (
+    get_dbt_command_from_directory,
+    get_first_shapefile_path_in_dir,
+)
 from pendulum import datetime
-
-with open("include/majic/sources.json", "r") as f:
-    sources = json.load(f)
 
 BUCKET_NAME = "airflow-staging"
 TMP_PATH = "/tmp/majic"
 
 
-def ogr_load_to_postgres(source: dict):
+def load_shapefile_to_postgres(source: dict):
     shapefile_on_s3 = source["shapefile_on_s3"]
     srid = source["srid"]
     table_name = source["table_name"]
@@ -28,32 +28,29 @@ def ogr_load_to_postgres(source: dict):
         zip_file = ZipFile(f)
         tmp_path = f"{TMP_PATH}/{table_name}"
         zip_file.extractall(tmp_path)
-        for dirpath, _, filenames in os.walk(tmp_path):
-            for filename in filenames:
-                if filename.endswith(".shp"):
-                    path = os.path.abspath(os.path.join(dirpath, filename))
-                    cmd = [
-                        "ogr2ogr",
-                        "-f",
-                        '"PostgreSQL"',
-                        f'"{Container().gdal_dbt_conn().encode()}"',
-                        "-overwrite",
-                        "-lco",
-                        "GEOMETRY_NAME=geom",
-                        "-a_srs",
-                        f"EPSG:{srid}",
-                        "-nlt",
-                        "MULTIPOLYGON",
-                        "-nlt",
-                        "PROMOTE_TO_MULTI",
-                        "-nln",
-                        table_name,
-                        path,
-                        "--config",
-                        "PG_USE_COPY",
-                        "YES",
-                    ]
-                    subprocess.run(" ".join(cmd), shell=True, check=True)
+        shapefile_path = get_first_shapefile_path_in_dir(tmp_path)
+        cmd = [
+            "ogr2ogr",
+            "-f",
+            '"PostgreSQL"',
+            f'"{Container().gdal_dbt_conn().encode()}"',
+            "-overwrite",
+            "-lco",
+            "GEOMETRY_NAME=geom",
+            "-a_srs",
+            f"EPSG:{srid}",
+            "-nlt",
+            "MULTIPOLYGON",
+            "-nlt",
+            "PROMOTE_TO_MULTI",
+            "-nln",
+            table_name,
+            shapefile_path,
+            "--config",
+            "PG_USE_COPY",
+            "YES",
+        ]
+        subprocess.run(" ".join(cmd), shell=True, check=True)
 
     shutil.rmtree(TMP_PATH)
 
@@ -76,26 +73,27 @@ def ingest_majic():
     for source in sources:
         ingest_task = PythonOperator(
             task_id=f"ingest_{source['table_name']}",
-            python_callable=ogr_load_to_postgres,
+            python_callable=load_shapefile_to_postgres,
             op_kwargs={"source": source},
         )
         ingest_tasks.append(ingest_task)
 
     @task.bash(pool=DBT_POOL)
     def dbt_build() -> str:
-        return 'cd "${AIRFLOW_HOME}/include/sql/sparte" && dbt build -s +consommation.sql+'
+        return get_dbt_command_from_directory(cmd="dbt build -s +consommation.sql+")
 
     @task.bash()
     def cleanup() -> str:
         return f"rm -rf {TMP_PATH}"
 
-    # run ingest tasks sequentially
     for i in range(len(ingest_tasks) - 1):
         ingest_tasks[i] >> ingest_tasks[i + 1]
 
     build = dbt_build()
     cleanup_task = cleanup()
 
+    # Build the models and clean up the temporary files
+    # after all the ingest tasks have completed
     ingest_tasks[-1].set_downstream(build)
     build.set_downstream(cleanup_task)
 
--- airflow/include/majic/sources.py
+++ airflow/include/majic/sources.py
@@ -1,32 +1,32 @@
-[
+sources = [
     {
         "name": "France Métropolitaine",
         "shapefile_on_s3": "majic/obs_artif_conso_com_2009_2023.zip",
         "srid": 2154,
-        "table_name": "majic_france_metropolitaine"
+        "table_name": "majic_france_metropolitaine",
     },
     {
         "name": "Guadeloupe",
         "shapefile_on_s3": "majic/obs_artif_conso_com_2009_2023_971.zip",
         "srid": 32620,
-        "table_name": "majic_guadeloupe"
+        "table_name": "majic_guadeloupe",
     },
     {
         "name": "Martinique",
         "shapefile_on_s3": "majic/obs_artif_conso_com_2009_2023_972.zip",
         "srid": 32620,
-        "table_name": "majic_martinique"
+        "table_name": "majic_martinique",
     },
     {
         "name": "Guyane",
         "shapefile_on_s3": "majic/obs_artif_conso_com_2009_2023_973.zip",
         "srid": 2972,
-        "table_name": "majic_guyane"
+        "table_name": "majic_guyane",
     },
     {
         "name": "La Réunion",
         "shapefile_on_s3": "majic/obs_artif_conso_com_2009_2023_974.zip",
         "srid": 2975,
-        "table_name": "majic_la_reunion"
-    }
+        "table_name": "majic_la_reunion",
+    },
 ]
