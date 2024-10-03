f52081c047c2c278f02241509e47a6d431a6bbaa
Alexis A. <alexis.athlani@gmail.com>
2024-10-03 09:29:49 +0200

    feat(epci): load epcis with airflow

---
+++ airflow/include/sql/sparte/models/admin_express/epci.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='table',
+        indexes=[
+            {'columns': ['id'], 'type': 'btree'},
+            {'columns': ['code'], 'type': 'btree'},
+            {'columns': ['name'], 'type': 'btree'},
+            {'columns': ['geom'], 'type': 'gist'}
+        ])
+}}
+
+SELECT
+    *,
+    32620 AS srid_source
+FROM {{ ref('epci_guadeloupe') }}
+UNION ALL
+SELECT
+    *,
+    32620 AS srid_source
+FROM {{ ref('epci_martinique') }}
+UNION ALL
+SELECT
+    *,
+    2972 AS srid_source
+FROM {{ ref('epci_guyane') }}
+UNION ALL
+SELECT
+    *,
+    2975 AS srid_source
+FROM {{ ref('epci_reunion') }}
+UNION ALL
+SELECT
+    *,
+    2154 AS srid_source
+FROM {{ ref('epci_metropole') }}
