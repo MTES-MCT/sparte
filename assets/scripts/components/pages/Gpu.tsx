9a610247684b41c440b7bc303fea1ae84188068a
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-10-04 17:46:20 +0200

    Simplify OCSGE/GPU status check and improve conditional rendering logic

--- assets/scripts/components/pages/Gpu.tsx
+++ assets/scripts/components/pages/Gpu.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { useHtmlLoader } from '@hooks/useHtmlLoader';
-import { isOcsgeAvailable } from '@utils/project';
 import Loader from '@components/ui/Loader';
-import PageTitle from '@components/widgets/PageTitle';
-import OcsgeStatus, { OcsgeStatusProps } from '@components/widgets/OcsgeStatus';
-import GpuStatus from '@components/widgets/GpuStatus';
 
 /*
 Ce composant est un composant hybride qui permet de récupérer du contenu côté serveur via Django et de l'intégrer directement dans l'interface React.
@@ -16,33 +12,16 @@ Cela est nécessaire pour rendre du contenu HTML généré côté serveur, mais 
 Dans ce cas, les données provenant de Django sont considérées comme fiables.
 */
 
-const Gpu: React.FC<{ endpoint: string; ocsgeStatus: OcsgeStatusProps['status']; hasGpu: boolean }> = ({ endpoint, ocsgeStatus, hasGpu }) => {
-    const pageTitle = "Artificialisation des zonages d'urbanisme";
-
-    if (!isOcsgeAvailable(ocsgeStatus) || !hasGpu) {
-        return (
-            <div className="fr-container--fluid fr-p-3w w-100">
-                <div className="fr-grid-row fr-grid-row--gutters">
-                    <div className="fr-col-12">
-                        <PageTitle title={pageTitle} />
-                        {!isOcsgeAvailable(ocsgeStatus) && <OcsgeStatus status={ocsgeStatus} />}
-                        {!hasGpu && <GpuStatus />}
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
+const Gpu: React.FC<{ endpoint: string }> = ({ endpoint }) => {
     const { content, isLoading, error } = useHtmlLoader(endpoint);
 
     if (isLoading) return <Loader />;
     if (error) return <div>Erreur : {error}</div>;
 
     return (
-        <div className="fr-container--fluid fr-p-3w w-100">
-            <div className="fr-grid-row fr-grid-row--gutters">
+        <div className="fr-container--fluid fr-p-3w">
+            <div className="fr-grid-row">
                 <div className="fr-col-12">
-                    <PageTitle title={pageTitle} />
                     <div dangerouslySetInnerHTML={{ __html: content }} />
                 </div>
             </div>
--- assets/scripts/components/pages/Synthese.tsx
+++ assets/scripts/components/pages/Synthese.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useHtmlLoader } from '@hooks/useHtmlLoader';
 import Loader from '@components/ui/Loader';
-import PageTitle from '@components/widgets/PageTitle';
 
 /*
 Ce composant est un composant hybride qui permet de récupérer du contenu côté serveur via Django et de l'intégrer directement dans l'interface React.
@@ -20,10 +19,9 @@ const Synthese: React.FC<{ endpoint: string }> = ({ endpoint }) => {
     if (error) return <div>Erreur : {error}</div>;
 
     return (
-        <div className="fr-container--fluid fr-p-3w w-100">
-            <div className="fr-grid-row fr-grid-row--gutters">
+        <div className="fr-container--fluid fr-p-3w">
+            <div className="fr-grid-row">
                 <div className="fr-col-12">
-                    <PageTitle title="Synthèse" />
                     <div dangerouslySetInnerHTML={{ __html: content }} />
                 </div>
             </div>
