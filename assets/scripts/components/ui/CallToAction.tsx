f83d1f3b5001bf346d8acdcb495bd91539f1ffdc
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2024-10-11 15:01:15 +0200

    Refactoring download buttons and local report ui

---
+++ assets/scripts/components/ui/CallToAction.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 6px;
+  background-image:
+    radial-gradient(
+      farthest-corner circle at top left,
+      #95a4ff 0%, 63%, #9094ff 0% 0%
+    )
+`;
+
+const Title = styled.h3`
+  font-size: 0.9rem;
+  color: #313178;
+  margin-bottom: 0.5rem;
+`;
+
+const Text = styled.p`
+  font-size: 0.8rem;
+  line-height: 1.2rem;
+  color: #313178;
+  margin-bottom: 1rem;
+`;
+
+
+interface CallToActionProps {
+  title: string;
+  text: string;
+  children: ReactNode;
+}
+
+const CallToAction: React.FC<CallToActionProps> = ({ title, text, children }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Text>{text}</Text>
+      {children}
+    </Container>
+  );
+};
+
+export default CallToAction;
