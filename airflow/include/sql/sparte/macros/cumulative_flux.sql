e736d3a55c5960fcf0637ca1e94f3bea813b3957
Alexis A. <alexis.athlani@gmail.com>
2024-11-14 16:38:42 +0100

    doc(cumulative_flux): add usage comment

--- airflow/include/sql/sparte/macros/cumulative_flux.sql
+++ airflow/include/sql/sparte/macros/cumulative_flux.sql
@@ -1,4 +1,29 @@
+{#
+
+Cette macro permet de générer une liste de combinaisons d'années
+pour calculer des flux cumulés.
+
+Usage :
+    {% call(start_year, end_year) cumulative_flux(
+        first_available_year=2009,
+        last_available_year=2012
+    ) %}
+        sum(population_{{ start_year }}_{{ end_year + 1 }})
+        as population_{{ start_year }}_{{ end_year + 1 }}
+    {% endcall %}
+
+Va créer les colonnes suivantes :
+    - population_2009_2010
+    - population_2010_2011
+    - population_2011_2012
+    - population_2009_2011
+    - population_2010_2012
+    - population_2009_2012
+A partir des sommes des colonnes de flux annuels.
+
+#}
 {% macro cumulative_flux(first_available_year, last_available_year) %}
+
     {% set ns = namespace(continued=false) %}
     {% for start_year in range(first_available_year, last_available_year + 1) %}
         {% for end_year in range(first_available_year, last_available_year + 1) -%}
