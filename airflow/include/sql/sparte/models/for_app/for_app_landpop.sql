f28f7b16a5b81ddc00aa1991b4e03a1919a06350
Alexis A. <alexis.athlani@gmail.com>
2024-11-18 15:51:19 +0100

    feat(landpop): add comparison level

--- airflow/include/sql/sparte/models/for_app/for_app_landpop.sql
+++ airflow/include/sql/sparte/models/for_app/for_app_landpop.sql
@@ -15,13 +15,20 @@
 SELECT
     code_commune as land_id,
     'COMMUNE' as land_type,
+    'EPCI' as comparison_level,
+    commune.epci as comparison_id,
     {{ fields_to_query }}
 FROM
-    {{ ref('flux_population_commune') }}
+    {{ ref('flux_population_commune') }} as flux_population_commune
+LEFT JOIN
+    {{ ref('commune') }} as commune
+    ON flux_population_commune.code_commune = commune.code
 UNION
 SELECT
     epci as land_id,
     'EPCI' as land_type,
+    'NATION' as comparison_level,
+    'NATION' as comparison_id,
     {{ fields_to_query }}
 FROM
     {{ ref('flux_population_epci') }}
@@ -29,13 +36,20 @@ UNION
 SELECT
     departement as land_id,
     'DEPARTEMENT' as land_type,
+    'REGION' as comparison_level,
+    departement.region as comparison_id,
     {{ fields_to_query }}
 FROM
-    {{ ref('flux_population_departement') }}
+    {{ ref('flux_population_departement') }} as flux_population_departement
+LEFT JOIN
+    {{ ref('departement') }} as departement
+    ON departement.code = flux_population_departement.departement
 UNION
 SELECT
     region as land_id,
     'REGION' as land_type,
+    'NATION' as comparison_level,
+    'NATION' as comparison_id,
     {{ fields_to_query }}
 FROM
     {{ ref('flux_population_region') }}
@@ -43,6 +57,8 @@ UNION
 SELECT
     scot as land_id,
     'SCOT' as land_type,
+    'NATION' as comparison_level,
+    'NATION' as comparison_id,
     {{ fields_to_query }}
 FROM
     {{ ref('flux_population_scot') }}
