0e69dba51efc0ad04e26e471d8c6b4d7c5160e81
Alexis A. <alexis.athlani@gmail.com>
2024-11-28 14:06:01 +0100

    fix(sql): fix division by zero in sum_percent.sql

--- airflow/include/sql/sparte/macros/utils/sum_percent.sql
+++ airflow/include/sql/sparte/macros/utils/sum_percent.sql
@@ -3,8 +3,8 @@
     sum({{ field }}) as {{ field }},
     ( -- prevents division by zero
         CASE
-            WHEN    sum({{ total_field }}) = 0 THEN sum({{ field }}) * 100 / 1
-            ELSE    sum({{ field }}) * 100 / sum({{ total_field }})
+            WHEN    max({{ total_field }}) = 0 THEN sum({{ field }}) * 100 / 1
+            ELSE    sum({{ field }}) * 100 / max({{ total_field }})
         END
     ) as {{ field }}_percent
 {% endmacro %}
