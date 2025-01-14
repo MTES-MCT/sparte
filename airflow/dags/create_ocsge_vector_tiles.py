0e72f0285df920f2febb2cf3902d562379fe1167
Alexis A. <alexis.athlani@gmail.com>
2025-01-14 22:07:34 +0100

    feat(vector tiles) make generation by year and departement to save time

--- airflow/dags/create_ocsge_vector_tiles.py
+++ airflow/dags/create_ocsge_vector_tiles.py
@@ -4,12 +4,12 @@ from airflow.models.param import Param
 from include.domain.container import Container
 
 
-def get_geojson_filename(year: int) -> str:
-    return f"occupation_du_sol_{year}.geojson"
+def get_geojson_filename(year: int, departement: str) -> str:
+    return f"occupation_du_sol_{year}_{departement}.geojson"
 
 
-def get_pmtiles_filename(year: int) -> str:
-    return f"occupation_du_sol_{year}.pmtiles"
+def get_pmtiles_filename(year: int, departement: str) -> str:
+    return f"occupation_du_sol_{year}_{departement}.pmtiles"
 
 
 @dag(
@@ -23,6 +23,7 @@ def get_pmtiles_filename(year: int) -> str:
     max_active_tasks=10,
     params={
         "year": Param(2018, type="integer"),
+        "departement": Param("75", type="string"),
     },
 )
 def create_ocsge_vector_tiles():
@@ -32,13 +33,14 @@ def create_ocsge_vector_tiles():
     @task.python()
     def postgis_to_geojson(params: dict):
         year = params.get("year")
-        filename = get_geojson_filename(year)
+        departement = params.get("departement")
+        filename = get_geojson_filename(year, departement)
 
         return (
             Container()
             .sql_to_geojsonseq_on_s3_handler()
             .export_sql_result_to_geojsonseq_on_s3(
-                sql=f"SELECT * FROM public_ocsge.occupation_du_sol WHERE year = {year}",
+                sql=f"SELECT * FROM public_ocsge.occupation_du_sol WHERE year = {year} and departement = '{departement}'",  # noqa: E501
                 s3_key=f"{vector_tiles_dir}/{filename}",
                 s3_bucket=bucket_name,
             )
@@ -47,8 +49,9 @@ def create_ocsge_vector_tiles():
     @task.bash(skip_on_exit_code=110)
     def geojson_to_pmtiles(params: dict):
         year = params.get("year")
-        geojson_filename = get_geojson_filename(year)
-        pmtiles_filename = get_pmtiles_filename(year)
+        departement = params.get("departement")
+        geojson_filename = get_geojson_filename(year, departement)
+        pmtiles_filename = get_pmtiles_filename(year, departement)
         local_input = f"/tmp/{geojson_filename}"
         local_output = f"/tmp/{pmtiles_filename}"
         Container().s3().get_file(f"{bucket_name}/{vector_tiles_dir}/{geojson_filename}", local_input)
@@ -62,8 +65,8 @@ def create_ocsge_vector_tiles():
             "--force",
             "--no-simplification-of-shared-nodes",
             "--no-tiny-polygon-reduction",
-            "--base-zoom=11",
             "--coalesce-densest-as-needed",
+            "--no-tile-size-limit",
             "-zg",
         ]
 
@@ -74,7 +77,8 @@ def create_ocsge_vector_tiles():
     @task.python(trigger_rule="all_done")
     def upload(params: dict):
         year = params.get("year")
-        pmtiles_filename = get_pmtiles_filename(year)
+        departement = params.get("departement")
+        pmtiles_filename = get_pmtiles_filename(year, departement)
         local_path = f"/tmp/{pmtiles_filename}"
         path_on_s3 = f"{bucket_name}/{vector_tiles_dir}/{pmtiles_filename}"
         Container().s3().put(local_path, path_on_s3)
@@ -82,13 +86,15 @@ def create_ocsge_vector_tiles():
     @task.bash()
     def delete_geojson_file(params: dict):
         year = params.get("year")
-        geojson_filename = get_geojson_filename(year)
+        departement = params.get("departement")
+        geojson_filename = get_geojson_filename(year, departement)
         return f"rm /tmp/{geojson_filename}"
 
     @task.bash()
     def delete_pmtiles_file(params: dict):
         year = params.get("year")
-        pmtiles_filename = get_pmtiles_filename(year)
+        departement = params.get("departement")
+        pmtiles_filename = get_pmtiles_filename(year, departement)
         return f"rm /tmp/{pmtiles_filename}"
 
     (postgis_to_geojson() >> geojson_to_pmtiles() >> upload() >> delete_geojson_file() >> delete_pmtiles_file())
