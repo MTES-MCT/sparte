e937d43528cd01d093e09af4258c57bda38c4397
Alexis A. <alexis.athlani@gmail.com>
2025-01-20 17:54:44 +0100

    fix(lovac-sitadel): ajout des pourcentages de chaque parc sur le parc general

--- airflow/include/sql/sparte/models/for_app/for_app_region.sql
+++ airflow/include/sql/sparte/models/for_app/for_app_region.sql
@@ -4,13 +4,27 @@
         docs={'node_color': 'purple'}
     )
 }}
-
+with autorisation_logement_summary as (
+    SELECT DISTINCT land_id
+    FROM {{ ref('for_app_autorisationlogement') }}
+    WHERE land_type = '{{ var('REGION') }}'
+), logement_vacants_summary as (
+    SELECT DISTINCT land_id
+    FROM {{ ref('for_app_logementvacant') }}
+    WHERE land_type = '{{ var('REGION') }}'
+)
 SELECT
     code AS source_id,
     name AS name,
-    ST_Transform(geom, 4326) AS mpoly,
+    code in (
+        SELECT land_id
+        FROM autorisation_logement_summary
+    ) as autorisation_logement_available,
+    code in (
+        SELECT land_id
+        FROM logement_vacants_summary
+    ) as logements_vacants_available,
     srid_source AS srid_source,
-    TRUE as autorisation_logement_available,
-    TRUE as logements_vacants_available
+    ST_Transform(geom, 4326) AS mpoly
 FROM
     {{ ref('region') }}
