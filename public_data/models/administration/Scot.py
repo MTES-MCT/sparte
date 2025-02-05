e231c11bebcde2b9135eec60060437d0ec633bdc
Alexis A. <alexis.athlani@gmail.com>
2025-02-05 18:30:59 +0100

    fix(get_ocsge_millesimes): add method to scot

--- public_data/models/administration/Scot.py
+++ public_data/models/administration/Scot.py
@@ -43,7 +43,8 @@ class Scot(LandMixin, GetDataFromCeremaMixin, models.Model):
     def get_ocsge_millesimes(self) -> set:
         millesimes = set()
         for dept in self.departements.all():
-            millesimes.update(dept.ocsge_millesimes)
+            if dept.ocsge_millesimes:
+                millesimes.update(dept.ocsge_millesimes)
         return millesimes
 
     objects = ScotManager()
