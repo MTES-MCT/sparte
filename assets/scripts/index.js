bc538a007e984d9327838f4b72bd51c070de230c
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2025-02-11 11:54:32 +0100

    Cleaning and migrate boostrap to webpack

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,10 @@
 // Import styles
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/index.css'
+
+// Import bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // Import dsfr
 import '@gouvfr/dsfr/dist/dsfr.module.min.js'
