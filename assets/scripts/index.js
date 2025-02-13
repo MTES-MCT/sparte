7f5d25b1fa16bfe38103ebb2d037581abb481679
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2025-02-13 13:15:33 +0100

    Remove bootstrap bundle js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,9 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/index.css'
 
-// Import bootstrap
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 // Import dsfr
 import '@gouvfr/dsfr/dist/dsfr.module.min.js'
 
