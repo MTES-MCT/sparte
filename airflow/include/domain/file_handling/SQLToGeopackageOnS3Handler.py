fab29a36a83f3e9834e334adbfdc9d4648279b4a
Alexis A. <alexis.athlani@gmail.com>
2025-02-25 16:42:13 +0100

    feat(data_gouv): add export of imper by commune

---
+++ airflow/include/domain/file_handling/SQLToGeopackageOnS3Handler.py
@@ -0,0 +1,75 @@
+import logging
+import os
+import subprocess
+
+from .BaseHTTPFileHandler import BaseHTTPFileHandler
+from .BaseS3Handler import BaseS3Handler
+from .BaseTmpPathGenerator import BaseTmpPathGenerator
+
+logger = logging.getLogger(__name__)
+
+
+class SQLToGeopackageOnS3Handler:
+    def __init__(
+        self,
+        http_file_handler: BaseHTTPFileHandler,
+        s3_handler: BaseS3Handler,
+        tmp_path_generator: BaseTmpPathGenerator,
+        db_connection: str,
+    ):
+        self.http_file_handler = http_file_handler
+        self.s3_handler = s3_handler
+        self.tmp_path_generator = tmp_path_generator
+        self.db_connection = db_connection
+
+    def export_sql_result_to_geopackage_on_s3(
+        self,
+        s3_key: str,
+        s3_bucket: str,
+        sql_to_layer_name_mapping: dict[str, str],
+    ) -> str:
+        tmp_file = f"{self.tmp_path_generator.get_tmp_path()}.gpkg"
+
+        first_layer = True
+
+        for layer_name, sql in sql_to_layer_name_mapping.items():
+            logger.info(f"Exporting SQL result to {tmp_file}")
+
+            cmd = [
+                "ogr2ogr",
+                "-progress",
+                "-f",
+                '"GPKG"',
+                tmp_file,
+                f'"{self.db_connection}"',
+                "public_ocsge.occupation_du_sol",
+                "-update" if not first_layer else "",
+                f'-sql "{sql}"',
+                "-nln",
+                layer_name,
+            ]
+            try:
+                subprocess.run(" ".join(cmd), shell=True, stderr=subprocess.STDOUT, check=True)
+            except subprocess.CalledProcessError as e:
+                logger.error(f"Error while exporting SQL results: {e.output}")
+                raise e
+            logger.info(f"SQL result exported to {tmp_file}")
+            first_layer = False
+
+        logger.info(f"Uploading {tmp_file} to s3://{s3_bucket}/{s3_key}")
+
+        upload_path = self.s3_handler.upload_file(
+            local_file_path=tmp_file,
+            s3_key=s3_key,
+            s3_bucket=s3_bucket,
+        )
+
+        logger.info(f"Uploaded {tmp_file} to {upload_path}")
+
+        logger.info(f"Deleting file {tmp_file}")
+
+        os.remove(tmp_file)
+
+        logger.info(f"Deleted file {tmp_file}")
+
+        return upload_path
--- airflow/include/domain/file_handling/__init__.py
+++ airflow/include/domain/file_handling/__init__.py
@@ -2,9 +2,12 @@ from .BaseCSVFileIngestor import BaseCSVFileIngestor
 from .BaseHTTPFileHandler import BaseHTTPFileHandler
 from .BaseS3Handler import BaseS3Handler
 from .BaseTmpPathGenerator import BaseTmpPathGenerator
+from .DataGouvHandler import DataGouvHandler
 from .RemoteToS3FileHandler import RemoteToS3FileHandler
 from .S3CSVFileToDBTableHandler import S3CSVFileToDBTableHandler
+from .S3ToDataGouvHandler import S3ToDataGouvHandler
 from .SQLToGeojsonSeqOnS3Handler import SQLToGeojsonSeqOnS3Handler
+from .SQLToGeopackageOnS3Handler import SQLToGeopackageOnS3Handler
 
 __all__ = [
     "BaseHTTPFileHandler",
@@ -14,4 +17,7 @@ __all__ = [
     "BaseCSVFileIngestor",
     "S3CSVFileToDBTableHandler",
     "SQLToGeojsonSeqOnS3Handler",
+    "SQLToGeopackageOnS3Handler",
+    "DataGouvHandler",
+    "S3ToDataGouvHandler",
 ]
