c83438fab8289139fd2ae6c84ebd140f3457397a
Alexis A. <alexis.athlani@gmail.com>
2025-02-26 14:10:32 +0100

    feat(imper): ajout de tables reprenant les données des graphiques du diag

---
+++ airflow/include/sql/sparte/models/ocsge/calculated/flux/imper/imper_flux_commune_by_usage.sql
@@ -0,0 +1,53 @@
+with
+    imper as (
+        select
+            commune_code,
+            commune_surface,
+            year_old,
+            year_new,
+            us_new as usage,
+            sum(surface) as surface_imper
+        from {{ ref("commune_flux_couverture_et_usage") }}
+        where new_is_impermeable
+        group by
+            commune_code,
+            commune_surface,
+            year_old,
+            year_new,
+            new_is_impermeable,
+            us_new
+    ),
+    desimper as (
+        select
+            commune_code,
+            commune_surface,
+            year_old,
+            year_new,
+            us_old as usage,
+            sum(surface) as surface_desimper
+        from {{ ref("commune_flux_couverture_et_usage") }}
+        where new_not_impermeable
+        group by
+            commune_code,
+            commune_surface,
+            year_old,
+            year_new,
+            new_is_impermeable,
+            us_old
+    )
+select
+    coalesce(imper.commune_code, desimper.commune_code) as commune_code,
+    coalesce(imper.commune_surface, desimper.commune_surface) as commune_surface,
+    coalesce(imper.year_old, desimper.year_old) as year_old,
+    coalesce(imper.year_new, desimper.year_new) as year_new,
+    coalesce(imper.usage, desimper.usage) as usage,
+    coalesce(imper.surface_imper, 0) as surface_imper,
+    coalesce(desimper.surface_desimper, 0) as surface_desimper
+from imper
+full outer join
+    desimper
+    on imper.commune_code = desimper.commune_code
+    and imper.usage = desimper.usage
+    and imper.year_old = desimper.year_old
+    and imper.year_new = desimper.year_new
+where coalesce(imper.commune_code, desimper.commune_code) = '56216'
---
+++ airflow/include/sql/sparte/models/ocsge/calculated/stock/imper/imper_commune_by_couverture.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized="table",
+        indexes=[{"columns": ["commune_code"], "type": "btree"}],
+    )
+}}
+
+with
+    without_percent as (
+        select
+            commune_code,
+            year,
+            sum(percent) as percent_of_commune,
+            sum(surface) as surface,
+            code_cs as couverture
+        from {{ ref("commune_couverture_et_usage") }}
+        where is_impermeable
+        group by commune_code, year, code_cs
+    )
+select
+    without_percent.*,
+    (without_percent.surface / imper_commune.surface) * 100 as percent_of_imper
+from without_percent
+left join
+    {{ ref("imper_commune") }}
+    on without_percent.commune_code = imper_commune.commune_code
+    and without_percent.year = imper_commune.year
