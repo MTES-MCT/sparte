ed083c4871b1201e974c099f0c30261653eb6bc9
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2025-03-13 23:57:48 +0100

    Config django 2fa

--- config/urls.py
+++ config/urls.py
@@ -18,15 +18,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from two_factor.admin import AdminSiteOTPRequired
+from two_factor.urls import urlpatterns as tf_urls
 
 from config.views import EnvironmentView
 
+
+class CustomAdminSite(AdminSiteOTPRequired, admin.AdminSite):
+    pass
+
+
+admin.site.__class__ = CustomAdminSite
+
 admin.site.site_header = f"Mon Diagnostic Artificialisation v{settings.OFFICIAL_VERSION}"
 
-
 urlpatterns = [
-    path("", include("home.urls")),
+    path("account/", include(tf_urls)),
     path("admin/", admin.site.urls),
+    path("", include("home.urls")),
     path("users/", include("users.urls")),
     path("public/", include("public_data.urls")),
     path("project/", include("project.urls")),
--- users/views.py
+++ users/views.py
@@ -24,6 +24,15 @@ class SigninView(BreadCrumbMixin, LoginView):
             {"href": reverse_lazy("users:signin"), "title": "Connexion"},
         ]
 
+    def get_success_url(self):
+        """Redirige les admins vers /admin/ et les autres utilisateurs vers /project/"""
+        user = self.request.user
+        if user.is_authenticated:
+            if user.is_staff or user.is_superuser:
+                return reverse_lazy("admin:index")
+            return reverse_lazy("project:list")
+        return super().get_success_url()
+
 
 class SignoutView(RedirectView):
     url = reverse_lazy("users:signin")
