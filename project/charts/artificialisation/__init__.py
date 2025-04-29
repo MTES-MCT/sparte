bfec450ba44c620709cc28b1b7180baa788f0cd5
Alexis A. <alexis.athlani@gmail.com>
2025-04-29 13:00:11 +0200

    f

--- project/charts/artificialisation/__init__.py
+++ project/charts/artificialisation/__init__.py
@@ -1,13 +1,11 @@
 from .ArtifByCouverturePieChart import ArtifByCouverturePieChart
 from .ArtifByUsagePieChart import ArtifByUsagePieChart
 from .ArtifMap import ArtifMap
-from .ArtifPercentRate import ArtifPercentRate
 from .ArtifStock import ArtifStock
 
 __all__ = [
     "ArtifByCouverturePieChart",
     "ArtifByUsagePieChart",
     "ArtifStock",
-    "ArtifPercentRate",
     "ArtifMap",
 ]
