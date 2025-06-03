281d3e169e712f930fd58f5e550532fb9ad69bfd
Sofian A. Thibaut <sofian.ab@hotmail.fr>
2025-06-03 18:14:52 +0200

    Adds hook to fetch land friche status

---
+++ assets/scripts/services/types/land_friches_statut.ts
@@ -0,0 +1,14 @@
+export interface LandFrichesStatut {
+    land_id: string;
+    land_type: string;
+    friche_statut: 'friche reconvertie' | 'friche avec projet' | 'friche sans projet' | 'friche potentielle';
+    friche_count: number;
+    friche_surface: number;
+}
+
+export type UseLandFrichesStatutType = (arg: { land_type: string; land_id: string }) => {
+    data?: LandFrichesStatut[];
+    isLoading: boolean;
+    error?: any;
+    refetch: () => void;
+};
