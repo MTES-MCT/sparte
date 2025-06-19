0ac714407aaad5e6a8f921e57c85fa5abec062ca
Alexis A. <alexis.athlani@gmail.com>
2025-06-19 10:34:35 +0200

    feat(satisfactionview): add basic wizard

--- home/views/SatisfactionView.py
+++ home/views/SatisfactionView.py
@@ -1,9 +1,18 @@
-from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from formtools.wizard.views import SessionWizardView
 
-from home.forms import SatisfactionForm
+from home.forms import SatisfactionFormNPS, SatisfactionFormSuggestedChange
+from home.models import SatisfactionFormEntry
 
 
-class SatisfactionView(FormView):
-    form_class = SatisfactionForm
+class SatisfactionView(SessionWizardView):
+    form_list = [SatisfactionFormNPS, SatisfactionFormSuggestedChange]
     template_name = "home/satisfaction.html"
-    success_url = "/"
+
+    def done(self, form_list, **kwargs):
+        form_dict = self.get_all_cleaned_data()
+        SatisfactionFormEntry.objects.create(
+            **form_dict,
+            user=self.request.user if self.request.user.is_authenticated else None,
+        )
+        return HttpResponseRedirect("/")
