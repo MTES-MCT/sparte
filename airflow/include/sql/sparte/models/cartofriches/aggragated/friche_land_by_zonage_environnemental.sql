4772ee28b2dc3aefdd2355d66d158ef71c407ac8
Alexis A. <alexis.athlani@gmail.com>
2025-06-19 15:01:16 +0200

    f

--- airflow/include/sql/sparte/models/cartofriches/aggragated/friche_land_by_zonage_environnemental.sql
+++ airflow/include/sql/sparte/models/cartofriches/aggragated/friche_land_by_zonage_environnemental.sql
@@ -2,13 +2,13 @@
     count_friche_by_value(
         'friche_zonage_environnemental',
         [
-            'proximite_zone (reserves_naturelles)',
-            'reserve_naturelle',
-            'natura_2000',
-            'hors zone',
-            'znieff',
-            'proximite_zone (znieff)',
-            'proximite_zone (natura_2000)'
+            "proche d''une réserve naturelle",
+            "réserve naturelle",
+            "Natura 2000",
+            "hors zone",
+            "ZNIEFF",
+            "proche d''une ZNIEFF",
+            "proche d''une zone Natura 2000"
         ]
     )
 }}
