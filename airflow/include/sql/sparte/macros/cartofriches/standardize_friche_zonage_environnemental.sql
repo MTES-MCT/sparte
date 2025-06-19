07d7ffe2f1492fe523a4e0a443ed6357735417bc
Alexis A. <alexis.athlani@gmail.com>
2025-06-19 14:32:11 +0200

    f

--- airflow/include/sql/sparte/macros/cartofriches/standardize_friche_zonage_environnemental.sql
+++ airflow/include/sql/sparte/macros/cartofriches/standardize_friche_zonage_environnemental.sql
@@ -1,12 +1,12 @@
 {% macro standardize_friche_zonage_environnemental(friche_zonage_environnemental) %}
     CASE
-        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (reserves_naturelles)' THEN 'Proche d''une réserve naturelle'
-        WHEN {{ friche_zonage_environnemental }} = 'reserve_naturelle' THEN 'Réserve naturelle'
+        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (reserves_naturelles)' THEN 'proche d''une réserve naturelle'
+        WHEN {{ friche_zonage_environnemental }} = 'reserve_naturelle' THEN 'réserve naturelle'
         WHEN {{ friche_zonage_environnemental }} = 'natura_2000' THEN 'Natura 2000'
-        WHEN {{ friche_zonage_environnemental }} = 'hors zone' THEN 'Hors zone'
+        WHEN {{ friche_zonage_environnemental }} = 'hors zone' THEN 'hors zone'
         WHEN {{ friche_zonage_environnemental }} = 'znieff' THEN 'ZNIEFF'
-        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (znieff)' THEN 'Proche d''une ZNIEFF'
-        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (natura_2000)' THEN 'Proche d''une zone Natura 2000'
+        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (znieff)' THEN 'proche d''une ZNIEFF'
+        WHEN {{ friche_zonage_environnemental }} = 'proximite_zone (natura_2000)' THEN 'proche d''une zone Natura 2000'
         ELSE {{ friche_zonage_environnemental }}
     END
 {% endmacro %}
