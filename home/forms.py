105532e631e126777680de11a4ee111c080683fb
Alexis A. <alexis.athlani@gmail.com>
2025-07-01 16:30:07 +0200

    feat(satisfaction): add image to form

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.templatetags.static import static
 
 from .models import Newsletter, SatisfactionFormEntry
 
@@ -15,9 +16,13 @@ class SatisfactionFormNPS(forms.ModelForm):
         fields = ("nps",)
         widgets = {"nps": forms.NumberInput(attrs={"class": "fr-input"})}
 
+    image = static("home/img/nps.svg")
+
 
 class SatisfactionFormSuggestedChange(forms.ModelForm):
     class Meta:
         model = SatisfactionFormEntry
         fields = ("suggested_change",)
         widgets = {"suggested_change": forms.Textarea(attrs={"class": "fr-input", "rows": 3})}
+
+    image = static("home/img/suggested_change.svg")
