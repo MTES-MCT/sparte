6df97e955dc6eafa5074fb13c2702752fafff22c
Sofian Thibaut <sofian.thibaut@gmail.com>
2025-07-11 11:46:44 +0200

    Remove unused stuff

--- assets/scripts/components/pages/Synthese.tsx
+++ assets/scripts/components/pages/Synthese.tsx
@@ -8,75 +8,13 @@ import { MillesimeDisplay } from "@components/features/ocsge/MillesimeDisplay";
 import { LandMillesimeTable } from "@components/features/ocsge/LandMillesimeTable";
 import { useArtificialisation } from "@hooks/useArtificialisation";
 import Guide from "@components/ui/Guide";
-import styled from 'styled-components';
 import { FricheOverview, FricheAbstract } from "@components/features/friches";
 import { LogementVacantOverview, LogementVacantAbstract } from "@components/features/logementVacant";
-
-const ChartSkeleton = styled.div`
-    width: 100%;
-    height: 250px;
-    background-color: #f0f0f0;
-    border-radius: 10px;
-`;
-
-const TryptiqueContainer = styled.div`
-    display: flex;
-    gap: 2rem;
-    justify-content: center;
-`;
-
-const TryptiqueCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-`;
-
-const TryptiqueIcon = styled.div`
-    font-size: 2em;
-    margin-bottom: 1rem;
-`;
-
-const TryptiqueText = styled.div`
-    font-weight: 600;
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-`;
 
 interface SyntheseProps {
 	projectData: ProjectDetailResultType;
 	landData: LandDetailResultType;
 }
-
-const tryptiqueConso = [
-    {
-        icon: 'bi-diagram-3',
-        text: "Consommation d’espaces par destinations"
-    },
-    {
-        icon: 'bi-people',
-        text: 'Indicateurs démographiques',
-    },
-    {
-        icon: 'bi-bar-chart-line',
-        text: 'Comparaison avec d’autres territoires',
-    }
-];
-
-const tryptiqueArtif = [
-    {
-        icon: 'bi-diagram-3',
-        text: "Evolution de l’artificialisation"
-    },
-    {
-        icon: 'bi-people',
-        text: 'Répartition par couverture et usage ',
-    },
-    {
-        icon: 'bi-bar-chart-line',
-        text: 'Cartographie des sols artificialisés',
-    }
-];
 
 const Synthese: React.FC<SyntheseProps> = ({ 
 	projectData,
@@ -112,32 +50,21 @@ const Synthese: React.FC<SyntheseProps> = ({
                     </Guide>
 				</div>
 			</div>
-            <h2 className="fr-mt-5w">Comprendre : les enjeux de la sobriété foncière</h2>
+            <h2 className="fr-mt-5w">Mesurer : les objectifs nationaux de sobriété foncière</h2>
             <div className="fr-grid-row fr-grid-row--gutters">
                 <div className="fr-col-12 fr-col-md-6 fr-grid-row">
-                    <div className="fr-callout bg-white w-100">
+                    <div className="bg-white fr-p-3w rounded w-100">
                         <div className="fr-mb-3w">
-                            <ChartSkeleton />
+                            graphique
                         </div>
                         <p>
                             Depuis 2011, la consommation d'espaces NAF a tendance à diminuer sur le territoire de Toulon.
                         </p>
-                        <h5 className="fr-mt-3w">Pour aller plus loin</h5>
-                        <TryptiqueContainer>
-                          {tryptiqueConso.map((item, idx) => (
-                            <TryptiqueCard key={idx}>
-                              <TryptiqueIcon>
-                                <i className={`bi ${item.icon}`} />
-                              </TryptiqueIcon>
-                              <TryptiqueText>{item.text}</TryptiqueText>
-                            </TryptiqueCard>
-                          ))}
-                        </TryptiqueContainer>
-                        <Link to={projectData.urls.consommation} className="fr-link fr-text--sm">Accéder au bilan de la consommation</Link>
+                        <Link to={projectData.urls.consommation} className="fr-btn fr-mt-3w fr-icon-arrow-right-line fr-btn--icon-right fr-text--sm">Accéder au bilan de la consommation</Link>
                     </div>
                 </div>
                 <div className="fr-col-12 fr-col-md-6 fr-grid-row">
-                    <div className="fr-callout bg-white w-100">
+                    <div className="bg-white fr-p-3w rounded w-100">
                         <h4 className="fr-mb-3w">Période 2021 - 2031 : Mesure de la consommation d'espaces NAF</h4>
                         <p className="fr-text--sm fr-mb-2w">
                             La première étape de loi Climat et Résilience consiste à réduire de <strong>50 %</strong> la consommation d'espaces entre 2021 et 2031, par rapport à la consommation entre 2011 et 2020 (période de référence). 
@@ -148,35 +75,24 @@ const Synthese: React.FC<SyntheseProps> = ({
                         <p className="fr-text--sm fr-mb-2w">
                             Depuis 2021, 10 ha ont déjà été consommés, soit 2,5 ha en moyenne annuelle. Si la consommation d'espaces de ce territoire se poursuivait au même rythme, l'objectif non-réglementaire de réduction à horizon 2031 (53%) <strong>serait dépassé en 2028.</strong>
                         </p>
-                        <Link to={projectData.urls.trajectoires} className="fr-link fr-text--sm">Visualiser la trajectoire de sobriété foncière</Link>
+                        <Link to={projectData.urls.trajectoires} className="fr-btn fr-mt-3w fr-icon-arrow-right-line fr-btn--icon-right fr-text--sm">Visualiser la trajectoire de sobriété foncière</Link>
                     </div>
                 </div>
             </div>
-            <div className="fr-grid-row fr-grid-row--gutters">
+            <div className="fr-grid-row fr-grid-row--gutters fr-mt-5w">
                 <div className="fr-col-12 fr-col-md-6 fr-grid-row">
-                    <div className="fr-callout bg-white w-100">
+                    <div className="bg-white fr-p-3w rounded w-100">
                         <div className="fr-mb-3w">
-                            <ChartSkeleton />
+                            graphique
                         </div>
                         <p>
                             Entre 2019 et 2021, la surface artificialisée a augmenté de 130 ha sur le territoire de Toulon.
                         </p>
-                        <h5 className="fr-mt-3w">Pour aller plus loin</h5>
-                        <TryptiqueContainer>
-                          {tryptiqueArtif.map((item, idx) => (
-                            <TryptiqueCard key={idx}>
-                              <TryptiqueIcon>
-                                <i className={`bi ${item.icon}`} />
-                              </TryptiqueIcon>
-                              <TryptiqueText>{item.text}</TryptiqueText>
-                            </TryptiqueCard>
-                          ))}
-                        </TryptiqueContainer>
-                        <Link to={projectData.urls.artificialisation} className="fr-link fr-text--sm">Accéder au bilan de l'artificialisation</Link>
+                        <Link to={projectData.urls.artificialisation} className="fr-btn fr-mt-3w fr-icon-arrow-right-line fr-btn--icon-right fr-text--sm">Accéder au bilan de l'artificialisation</Link>
                     </div>
                 </div>
                 <div className="fr-col-12 fr-col-md-6 fr-grid-row">
-                    <div className="fr-callout bg-white w-100">
+                    <div className="bg-white fr-p-3w rounded w-100">
                         <h4 className="fr-mb-3w">Période 2031 - 2050 : Mesure de l’artificialisation</h4>
                         <p className="fr-text--sm fr-mb-2w">
                             La deuxième étape de loi Climat et Résilience consiste à atteindre <strong>l'objectif de zéro artificialisation nette à horizon 2050</strong>, mesurée avec les données, non plus de consommation d'espaces, mais d'artificialisation du sol, reposant sur la donnée OCS GE.
@@ -203,15 +119,13 @@ const Synthese: React.FC<SyntheseProps> = ({
                                 number: data.flux_surface,
                             })} ha
                             ont été artificialisés.
-                            {" "}
-                            <strong>Cela signifie que l'artificialisation nette sur la période est de 25,9 ha.</strong>
                         </p>
                         <Link to={projectData.urls.trajectoires} className="fr-btn fr-mt-3w fr-icon-arrow-right-line fr-btn--icon-right fr-text--sm">Visualiser la trajectoire de sobriété foncière</Link>
                     </div>
                 </div>
             </div>
-            <h2 className="fr-mt-5w">Agir : les leviers de la sobriété foncière</h2>
-            <div className="fr-mt-7w">
+            <h2 className="fr-mt-10w">Agir : les leviers de la sobriété foncière</h2>
+            <div className="fr-mt-5w">
                 <h3>Vacance des Logements</h3>
                 <LogementVacantOverview 
                     logements_vacants_status_details={landData.logements_vacants_status_details} 
