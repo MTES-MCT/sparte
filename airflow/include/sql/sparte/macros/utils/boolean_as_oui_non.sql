653351e12e6afd56ebc94a7c5c0a564ac8fda83b
Alexis A. <alexis.athlani@gmail.com>
2025-07-31 09:57:31 +0200

    fix(brevo): change type of boolean export to OUI/NON

--- airflow/include/sql/sparte/macros/utils/boolean_as_oui_non.sql
+++ airflow/include/sql/sparte/macros/utils/boolean_as_oui_non.sql
@@ -1,7 +1,7 @@
-{% macro boolean_as_oui_non(field) %}
+{% macro boolean_as_oui_non(field, uppercase=false) %}
     CASE
-        WHEN {{ field }} IS TRUE THEN 'oui'
-        WHEN {{ field }} IS FALSE THEN 'non'
+        WHEN {{ field }} IS TRUE THEN '{{ 'oui' if not uppercase else 'OUI' }}'
+        WHEN {{ field }} IS FALSE THEN '{{ 'non' if not uppercase else 'NON' }}'
         ELSE NULL
     END
 {% endmacro %}
--- airflow/include/sql/sparte/models/for_brevo/for_brevo_user_info.sql
+++ airflow/include/sql/sparte/models/for_brevo/for_brevo_user_info.sql
@@ -18,16 +18,16 @@ SELECT
     user_online_diagnostic_count as "NB_DIAG_CREES",
     user_download_diagnostic_count as "NB_DIAG_TELECHARGES",
     nps as "NPS",
-    newsletter_fully_opted_in as "EST_INSCRIT_NEWSLETTER",
+    {{ boolean_as_oui_non('newsletter_fully_opted_in', true) }} as "EST_INSCRIT_NEWSLETTER",
     newsletter_opt_in_date as "DATE_INSCRIPTION_NEWSLETTER",
     newsletter_double_opt_in_date as "DATE_CONFIRMATION_INSCRIPTION_NEWSLETTER",
-    main_land_competence_planification "A_COMPETENCE_URBA",
+    {{ boolean_as_oui_non('main_land_competence_planification', true) }} as "A_COMPETENCE_URBA",
     id as "EXT_ID",
-    visited_page_trajectoires as "ARRIVE_BAS_PAGE_TRAJ",
-    visited_page_vacance_des_logements as "ARRIVE_BAS_PAGE_VACANCE",
-    user_target_2031_modified as "A_MODIFIE_OBJ_REDUC_CONSO",
-    visited_page_consommation as "ARRIVE_BAS_PAGE_CONSO",
-    visited_page_artificialisation as "ARRIVE_BAS_PAGE_ARTIF",
-    visited_page_friches as "ARRIVE_BAS_DE_PAGE_FRICHES"
+    {{ boolean_as_oui_non('visited_page_trajectoires', true) }} as "ARRIVE_BAS_PAGE_TRAJ",
+    {{ boolean_as_oui_non('visited_page_vacance_des_logements', true) }} as "ARRIVE_BAS_PAGE_VACANCE",
+    {{ boolean_as_oui_non('user_target_2031_modified', true) }} as "A_MODIFIE_OBJ_REDUC_CONSO",
+    {{ boolean_as_oui_non('visited_page_consommation', true) }} as "ARRIVE_BAS_PAGE_CONSO",
+    {{ boolean_as_oui_non('visited_page_artificialisation', true) }} as "ARRIVE_BAS_PAGE_ARTIF",
+    {{ boolean_as_oui_non('visited_page_friches', true) }} as "ARRIVE_BAS_DE_PAGE_FRICHES"
 FROM
     {{ ref('user_single_view')}}
