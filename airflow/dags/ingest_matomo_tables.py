08c87241a7db2043dfe830c2a9c08c41a291d942
Alexis A. <alexis.athlani@gmail.com>
2025-08-01 09:28:54 +0200

    feat(matomo): add dbt build at the end of dag

--- airflow/dags/ingest_matomo_tables.py
+++ airflow/dags/ingest_matomo_tables.py
@@ -1,5 +1,7 @@
 from gdaltools import ogr2ogr
 from include.container import InfraContainer as Container
+from include.pools import DBT_POOL
+from include.utils import get_dbt_command_from_directory
 from pendulum import datetime
 
 from airflow.decorators import dag, task
@@ -44,12 +46,19 @@ def ingest_matomo_tables():  # noqa: C901
     def ingest_log_action():
         return ingest_table("matomo.matomo_log_action", "matomo_log_action")
 
+    @task.bash(retries=0, trigger_rule="all_success", pool=DBT_POOL)
+    def dbt_run() -> str:
+        return get_dbt_command_from_directory(
+            "dbt build -s matomo+",
+        )
+
     (
         ingest_log_visit()
         >> ingest_log_link_visit_action()
         >> ingest_log_conversion()
         >> ingest_log_action()
         >> ingest_action_types()
+        >> dbt_run()
     )
 
 
