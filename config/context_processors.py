ad07e7c859a26ca562c2c7f0b4b970881228bac1
Sofian Thibaut <sofian.thibaut@gmail.com>
2025-08-19 12:03:00 +0200

    Fix expose GOOGLE_ADWORDS_ACTIVATE env variable

--- config/context_processors.py
+++ config/context_processors.py
@@ -2,4 +2,7 @@ from django.conf import settings
 
 
 def add_settings_to_context(request):
-    return {"debug": settings.DEBUG}
+    return {
+        "debug": settings.DEBUG,
+        "GOOGLE_ADWORDS_ACTIVATE": settings.GOOGLE_ADWORDS_ACTIVATE,
+    }
