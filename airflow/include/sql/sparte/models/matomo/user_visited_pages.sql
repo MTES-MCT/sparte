514615f23a0e250f6602ffebc8454292425f11e0
Alexis A. <alexis.athlani@gmail.com>
2025-10-07 11:21:27 +0200

    fix: simplifier la sélection de user_id dans les requêtes SQL

--- airflow/include/sql/sparte/models/matomo/user_visited_pages.sql
+++ airflow/include/sql/sparte/models/matomo/user_visited_pages.sql
@@ -10,14 +10,7 @@ from
     (
         select distinct
             visited_page,
-            coalesce(custom_dimension_1, user_id) as user_id,
-            case
-                when custom_dimension_1 is not null
-                then 'from_custom_dimension_1'
-                when user_id is not null
-                then 'from_user_id'
-                else 'unknown'
-            end as source_user_id_field
+            user_id
         from {{ ref('matomo_log_link_visit_action') }}
         where visited_page is not null
     )
