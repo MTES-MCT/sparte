cedcc1143ec9c4d0d79eadcf54baebac2baa5440
Alexis A. <alexis.athlani@gmail.com>
2025-10-13 23:41:17 +0200

    refactor(artif_difference): supprimer la configuration de matérialisation et les index inutilisés

--- airflow/include/sql/sparte/models/ocsge/artif_difference.sql
+++ airflow/include/sql/sparte/models/ocsge/artif_difference.sql
@@ -19,24 +19,7 @@
         ],
     )
 }}
-{{
-    config(
-        materialized="table",
-        indexes=[
-            {"columns": ["loaded_date"], "type": "btree"},
-            {"columns": ["year_old"], "type": "btree"},
-            {"columns": ["year_new"], "type": "btree"},
-            {"columns": ["departement"], "type": "btree"},
-            {"columns": ["uuid"], "type": "btree"},
-            {"columns": ["cs_old"], "type": "btree"},
-            {"columns": ["cs_new"], "type": "btree"},
-            {"columns": ["us_old"], "type": "btree"},
-            {"columns": ["us_new"], "type": "btree"},
-            {"columns": ["geom"], "type": "gist"},
-            {"columns": ["srid_source"], "type": "btree"},
-        ],
-    )
-}}
+
 
 select
     foo.year_old,
