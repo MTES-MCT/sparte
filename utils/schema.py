cb4c3ee383562b6254a03dd60e5828c51e5d8482
Alexis A. <alexis.athlani@gmail.com>
2025-10-19 23:25:19 +0200

    feat: Mettre à jour la fonction dinitialisation du schéma pour inclure tous les modèles nécessaires aux tests

--- utils/schema.py
+++ utils/schema.py
@@ -4,16 +4,51 @@ from django.db import connection
 from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 from public_data.models import (
+    ArtifZonage,
+    ArtifZonageIndex,
+    AutorisationLogement,
     Commune,
     Departement,
     Epci,
+    ImperZonage,
+    ImperZonageIndex,
+    LandArtifStock,
+    LandArtifStockCouvertureComposition,
+    LandArtifStockCouvertureCompositionIndex,
+    LandArtifStockIndex,
+    LandArtifStockUsageComposition,
+    LandArtifStockUsageCompositionIndex,
     LandConso,
     LandConsoComparison,
     LandConsoStats,
+    LandFriche,
+    LandFricheGeojson,
+    LandFrichePollution,
+    LandFricheStatut,
+    LandFricheSurfaceRank,
+    LandFricheType,
+    LandFricheZonageEnvironnementale,
+    LandFricheZonageType,
+    LandFricheZoneActivite,
+    LandImperFlux,
+    LandImperFluxCouvertureComposition,
+    LandImperFluxCouvertureCompositionIndex,
+    LandImperFluxIndex,
+    LandImperFluxUsageComposition,
+    LandImperFluxUsageCompositionIndex,
+    LandImperStock,
+    LandImperStockCouvertureComposition,
+    LandImperStockCouvertureCompositionIndex,
+    LandImperStockIndex,
+    LandImperStockUsageComposition,
+    LandImperStockUsageCompositionIndex,
+    LandModel,
     LandPop,
     LandPopStats,
+    LogementVacant,
     Region,
     Scot,
+    SimilarTerritories,
 )
 
 
@@ -42,13 +77,63 @@ def init_unmanaged_schema_for_tests() -> None:
         raise NotInTestEnvironmentError("Cette fonction ne doit être appelée que dans le cadre de tests unitaires")
 
     with connection.schema_editor() as schema_editor:
-        drop_and_create_model(Commune, schema_editor)
+        # Administration
         drop_and_create_model(Region, schema_editor)
         drop_and_create_model(Departement, schema_editor)
         drop_and_create_model(Epci, schema_editor)
         drop_and_create_model(Scot, schema_editor)
+        drop_and_create_model(Commune, schema_editor)
+        drop_and_create_model(LandModel, schema_editor)
+
+        # Consommation
         drop_and_create_model(LandConso, schema_editor)
         drop_and_create_model(LandConsoComparison, schema_editor)
         drop_and_create_model(LandConsoStats, schema_editor)
+
+        # Demography
         drop_and_create_model(LandPop, schema_editor)
         drop_and_create_model(LandPopStats, schema_editor)
+        drop_and_create_model(SimilarTerritories, schema_editor)
+
+        # Artificialisation
+        drop_and_create_model(ArtifZonage, schema_editor)
+        drop_and_create_model(ArtifZonageIndex, schema_editor)
+        drop_and_create_model(LandArtifStock, schema_editor)
+        drop_and_create_model(LandArtifStockIndex, schema_editor)
+        drop_and_create_model(LandArtifStockCouvertureComposition, schema_editor)
+        drop_and_create_model(LandArtifStockCouvertureCompositionIndex, schema_editor)
+        drop_and_create_model(LandArtifStockUsageComposition, schema_editor)
+        drop_and_create_model(LandArtifStockUsageCompositionIndex, schema_editor)
+
+        # Imperméabilisation - Stock
+        drop_and_create_model(ImperZonage, schema_editor)
+        drop_and_create_model(ImperZonageIndex, schema_editor)
+        drop_and_create_model(LandImperStock, schema_editor)
+        drop_and_create_model(LandImperStockIndex, schema_editor)
+        drop_and_create_model(LandImperStockCouvertureComposition, schema_editor)
+        drop_and_create_model(LandImperStockCouvertureCompositionIndex, schema_editor)
+        drop_and_create_model(LandImperStockUsageComposition, schema_editor)
+        drop_and_create_model(LandImperStockUsageCompositionIndex, schema_editor)
+
+        # Imperméabilisation - Flux
+        drop_and_create_model(LandImperFlux, schema_editor)
+        drop_and_create_model(LandImperFluxIndex, schema_editor)
+        drop_and_create_model(LandImperFluxCouvertureComposition, schema_editor)
+        drop_and_create_model(LandImperFluxCouvertureCompositionIndex, schema_editor)
+        drop_and_create_model(LandImperFluxUsageComposition, schema_editor)
+        drop_and_create_model(LandImperFluxUsageCompositionIndex, schema_editor)
+
+        # Urbanisme - Friches
+        drop_and_create_model(LandFriche, schema_editor)
+        drop_and_create_model(LandFricheGeojson, schema_editor)
+        drop_and_create_model(LandFrichePollution, schema_editor)
+        drop_and_create_model(LandFricheStatut, schema_editor)
+        drop_and_create_model(LandFricheSurfaceRank, schema_editor)
+        drop_and_create_model(LandFricheType, schema_editor)
+        drop_and_create_model(LandFricheZonageEnvironnementale, schema_editor)
+        drop_and_create_model(LandFricheZonageType, schema_editor)
+        drop_and_create_model(LandFricheZoneActivite, schema_editor)
+
+        # Urbanisme - Autres
+        drop_and_create_model(AutorisationLogement, schema_editor)
+        drop_and_create_model(LogementVacant, schema_editor)
