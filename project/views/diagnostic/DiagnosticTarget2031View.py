c445f0529b6508d98388cc4fe549692c1027b3ca
Alexis A. <alexis.athlani@gmail.com>
2025-10-20 12:21:02 +0200

    feat: Supprimer le modèle React des trajectoires dans DiagnosticTarget2031View

--- project/views/diagnostic/DiagnosticTarget2031View.py
+++ project/views/diagnostic/DiagnosticTarget2031View.py
@@ -2,5 +2,5 @@ from .DiagnosticBaseView import DiagnosticBaseView
 
 
 class DiagnosticTarget2031View(DiagnosticBaseView):
-    partial_template_name = "project/components/dashboard/trajectoires_react.html"
+    partial_template_name = None
     full_template_name = "project/pages/trajectoires.html"
