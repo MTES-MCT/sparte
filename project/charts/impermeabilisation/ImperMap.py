c3675056509436a5139197fa66230132e23c900c
Alexis A. <alexis.athlani@gmail.com>
2025-10-21 09:00:10 +0200

    feat: Renommer les titres et descriptions liés à limperméabilisation pour plus de clarté

--- project/charts/impermeabilisation/ImperMap.py
+++ project/charts/impermeabilisation/ImperMap.py
@@ -69,8 +69,8 @@ class ImperMap(DiagnosticChart):
             AdminRef.get_label(self.params.get("child_land_type")),
             f"Part imperméabilisée (%) - {self.year_or_index_after}",
             f"Surface imperméabilisée (ha) - {self.year_or_index_after}",
-            f"Flux d'imperméabilisation (ha) - {self.year_or_index_before} -> {self.year_or_index_after}",
-            f"Flux d'imperméabilisation (%) - {self.year_or_index_before} -> {self.year_or_index_after}",
+            f"Imperméabilisation (ha) - {self.year_or_index_before} -> {self.year_or_index_after}",
+            f"Imperméabilisation (%) - {self.year_or_index_before} -> {self.year_or_index_after}",
         ]
 
         return {
@@ -172,7 +172,7 @@ class ImperMap(DiagnosticChart):
                     },
                 },
                 {
-                    "name": "Flux d'imperméabilisation",
+                    "name": "Imperméabilisation",
                     "type": "mapbubble",
                     "joinBy": ["land_id"],
                     "showInLegend": True,
