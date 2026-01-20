a5abfa2daa88ab3e802598fad63b0338b9c54479
Alexis A. <alexis.athlani@gmail.com>
2026-01-20 13:15:46 +0100

    feat: Met à jour les migrations de TerritorialisationObjectif pour ajouter des champs et modifier les contraintes

--- public_data/migrations/0210_load_territorialisation_objectif_fixture.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("public_data", "0209_alter_document_comment_default"),
-    ]
-
-    operations = []
