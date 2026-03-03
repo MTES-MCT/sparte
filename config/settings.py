7ce483766a39301bbffe20e107c0d651b1a44526
Alexis A. <alexis.athlani@gmail.com>
2026-03-03 17:41:49 +0100

    feat: Nettoyer le code en supprimant les commentaires inutiles, les classes et les fichiers obsolètes

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
-hello world
 """
 
 import sys
