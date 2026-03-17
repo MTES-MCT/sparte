f1bd8ef171fff535c61f914a8c5b9ae79cb559cc
Sofian Thibaut <sofian.thibaut@gmail.com>
2026-03-17 15:32:27 +0100

    Add badge to friches KPIs

--- frontend/scripts/components/pages/Friches/components/FrichesKpiCards.tsx
+++ frontend/scripts/components/pages/Friches/components/FrichesKpiCards.tsx
@@ -22,106 +22,112 @@ export const FrichesKpiCards: React.FC = () => {
     friche_reconvertie_count,
   } = fricheStatusDetails;
 
-  const sansProjetFooter: [
-    { icon: string; label: string; value: string },
-    { icon: string; label: string; value: string }
-  ] = [
-    {
-      icon: "bi bi-buildings",
-      label: "Surfaces artificialisées",
-      value: `${formatNumber({ number: friche_sans_projet_surface_artif })} ha`,
-    },
-    {
-      icon: "bi bi-droplet",
-      label: "Surfaces imperméables",
-      value: `${formatNumber({ number: friche_sans_projet_surface_imper })} ha`,
-    },
-  ];
-
-  const avecProjetFooter: [
-    { icon: string; label: string; value: string },
-    { icon: string; label: string; value: string }
-  ] = [
-    {
-      icon: "bi bi-buildings",
-      label: "Surfaces artificialisées",
-      value: `${formatNumber({ number: friche_avec_projet_surface_artif })} ha`,
-    },
-    {
-      icon: "bi bi-droplet",
-      label: "Surfaces imperméables",
-      value: `${formatNumber({ number: friche_avec_projet_surface_imper })} ha`,
-    },
-  ];
-
-  const reconvertieFooter: [
-    { icon: string; label: string; value: string },
-    { icon: string; label: string; value: string }
-  ] = [
-    {
-      icon: "bi bi-buildings",
-      label: "Surfaces artificialisées",
-      value: `${formatNumber({ number: friche_reconvertie_surface_artif })} ha`,
-    },
-    {
-      icon: "bi bi-droplet",
-      label: "Surfaces imperméables",
-      value: `${formatNumber({ number: friche_reconvertie_surface_imper })} ha`,
-    },
-  ];
-
   return (
     <div className="fr-mb-5w">
       <div className="fr-grid-row fr-grid-row--gutters fr-mb-3w">
         <div className="fr-col-12 fr-col-xl-4">
           <Kpi
             icon="bi bi-building-x"
-            label={`${friche_sans_projet_count} ${pluralize(friche_sans_projet_count, "friche")} sans projet`}
             value={
               <>
-                {formatNumber({ number: friche_sans_projet_surface })}{" "}
-                <span>ha</span>
+                <div>
+                  {formatNumber({ number: friche_sans_projet_surface })}{" "}
+                  <span>ha</span>
+                </div>
+                <span className="fr-badge fr-badge--no-icon text-lowercase fr-badge--warning">
+                  <strong>
+                    {friche_sans_projet_count}{" "}
+                    {pluralize(friche_sans_projet_count, "friche")} sans projet
+                  </strong>
+                </span>
               </>
             }
             variant="default"
-            badge="Actionnable"
             footer={{
               type: "metric",
-              items: sansProjetFooter,
+              items: [
+                {
+                  icon: "bi bi-buildings",
+                  label: "Surfaces artificialisées",
+                  value: `${formatNumber({ number: friche_sans_projet_surface_artif })} ha`,
+                },
+                {
+                  icon: "bi bi-droplet",
+                  label: "Surfaces imperméables",
+                  value: `${formatNumber({ number: friche_sans_projet_surface_imper })} ha`,
+                },
+              ],
             }}
           />
         </div>
         <div className="fr-col-12 fr-col-xl-4">
           <Kpi
             icon="bi bi-building"
-            label={`${friche_avec_projet_count} ${pluralize(friche_avec_projet_count, "friche")} avec projet`}
             value={
               <>
-                {formatNumber({ number: friche_avec_projet_surface })}{" "}
-                <span>ha</span>
+                <div>
+                  {formatNumber({ number: friche_avec_projet_surface })}{" "}
+                  <span>ha</span>
+                </div>
+                <span className="fr-badge fr-badge--no-icon text-lowercase fr-badge--info">
+                  <strong>
+                    {friche_avec_projet_count}{" "}
+                    {pluralize(friche_avec_projet_count, "friche")} avec projet
+                  </strong>
+                </span>
               </>
             }
             variant="default"
             footer={{
               type: "metric",
-              items: avecProjetFooter,
+              items: [
+                {
+                  icon: "bi bi-buildings",
+                  label: "Surfaces artificialisées",
+                  value: `${formatNumber({ number: friche_avec_projet_surface_artif })} ha`,
+                },
+                {
+                  icon: "bi bi-droplet",
+                  label: "Surfaces imperméables",
+                  value: `${formatNumber({ number: friche_avec_projet_surface_imper })} ha`,
+                },
+              ],
             }}
           />
         </div>
         <div className="fr-col-12 fr-col-xl-4">
           <Kpi
             icon="bi bi-building-check"
-            label={`${friche_reconvertie_count} ${pluralize(friche_reconvertie_count, "friche")} reconvertie${friche_reconvertie_count > 1 ? "s" : ""}`}
             value={
               <>
-                {formatNumber({ number: friche_reconvertie_surface })}{" "}
-                <span>ha</span>
+                <div>
+                  {formatNumber({ number: friche_reconvertie_surface })}{" "}
+                  <span>ha</span>
+                </div>
+                <span className="fr-badge fr-badge--no-icon text-lowercase fr-badge--success">
+                  <strong>
+                    {friche_reconvertie_count}{" "}
+                    {pluralize(friche_reconvertie_count, "friche")} reconvertie
+                    {friche_reconvertie_count > 1 ? "s" : ""}
+                  </strong>
+                </span>
               </>
             }
             variant="default"
             footer={{
               type: "metric",
-              items: reconvertieFooter,
+              items: [
+                {
+                  icon: "bi bi-buildings",
+                  label: "Surfaces artificialisées",
+                  value: `${formatNumber({ number: friche_reconvertie_surface_artif })} ha`,
+                },
+                {
+                  icon: "bi bi-droplet",
+                  label: "Surfaces imperméables",
+                  value: `${formatNumber({ number: friche_reconvertie_surface_imper })} ha`,
+                },
+              ],
             }}
           />
         </div>
