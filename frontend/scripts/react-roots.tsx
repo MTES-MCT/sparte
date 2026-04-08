699e9b3b291bb8a907712068034252d5cba48b94
Sofian Thibaut <sofian.thibaut@gmail.com>
2026-04-08 09:34:12 +0200

    Add typewriter animation to search bar placeholder

--- frontend/scripts/react-roots.tsx
+++ frontend/scripts/react-roots.tsx
@@ -41,9 +41,10 @@ if (footerConsent) {
 const searchBar = document.getElementById('react-search-bar')
 if (searchBar)
 {
+  const isHomePage = searchBar.dataset.origin === 'home';
   createRoot(searchBar).render(
     <Provider store={store}>
-      <SearchBar />
+      <SearchBar animatedPlaceholder={isHomePage} />
     </Provider>,
   )
 }
