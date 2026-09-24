0eafd0dbedc3dac2c690142fea40903cf806a079
Benoit Pesty <benoit.pesty@ign.fr>
2026-09-24 16:42:38 +0200

    fix(frontend) : lint

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import sys
 from pathlib import Path
 from typing import Any, Dict, Literal
 
